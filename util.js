1aa35f06fe4dc4ffe70ce6905e82471db00f9239
zoodogood <52154209+zoodogood@users.noreply.github.com>
2022-08-31 18:55:59 +0300

    Method toLocaleDelevoperString

---
+++ util.js
@@ -0,0 +1,20 @@
+
+function toLocaleDelevoperString(value){
+  if (typeof value === "string"){
+    return `"${ value }"`;
+  }
+
+  if (typeof value === "number"){
+    return value;
+  }
+
+  if (typeof value === "object"){
+    const keys = Object.keys(value);
+    return `Объект(${ keys.length }) <${ value.constructor.name }>`;
+  }
+}
+
+
+module.exports = {
+  toLocaleDelevoperString
+};;
