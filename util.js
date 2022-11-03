60a5431b1a9fb0e08d914da4df60ec778655532b
Alexsandr1x <zoodogood@gmail.com>
2022-11-03 19:30:27 +0200

    Create CustomCollector

--- util.js
+++ util.js
@@ -1,3 +1,46 @@
+class CustomCollector {
+  #handler;
+  #callback;
+
+  constructor({target, event, filter, limit = 0}){
+    if ("on" in target === false){
+      throw new Error("Target must extends EventEmitter");
+    }
+
+    this.#handler = this.handleEvent.bind(this);
+    this.target = target;
+    this.event = event;
+    this.filter = filter;
+    this.handle();
+
+    if (limit){
+      this.setTimer(limit);
+    }
+  }
+
+  handleEvent(data){
+    if (!!this?.filter(data) === true){
+      this.#callback?.call(this, data);
+    }
+  }
+
+  setCallback(callback){
+    this.#callback = callback;
+  }
+
+  handle(){
+    this.target.on(this.event, this.#handler);
+  }
+
+  end(){
+    this.target.removeListener(this.event, this.#handler);
+  }
+
+  setTimer(ms){
+    const callback = this.end.bind(this);
+    setTimeout(callback, ms);
+  }
+}
 
 function toLocaleDelevoperString(value){
   if (typeof value === "string"){
@@ -16,5 +59,6 @@ function toLocaleDelevoperString(value){
 
 
 export {
-  toLocaleDelevoperString
+  toLocaleDelevoperString,
+  CustomCollector
 };
