b90f2677664638e4844907a4c46a40392d7c5612
Alexsandr1x <zoodogood@gmail.com>
2022-12-09 15:03:37 +0200

    Check

--- commands/restart.js
+++ commands/restart.js
@@ -6,7 +6,7 @@ import { exec } from 'child_process';
 class Command {
 
 	async onChatInput(msg, interaction){
-		const command = "git pull & pm2 restart blackghost";
+		const command = "pm2 restart blackghost";
 		
 		const embed = {
 			title: "Выполнение комманды <:emoji_50:753916145177722941>",
