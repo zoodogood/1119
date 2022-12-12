01d5d10fc6d33e7a907e93d37e142337b6b0c5c9
Alexsandr1x <zoodogood@gmail.com>
2022-12-12 12:08:48 +0200

    Fix

--- commands/charity.js
+++ commands/charity.js
@@ -54,7 +54,7 @@ class Command {
 
     let
      count   = countUsers || Util.random(11, 22),
-     members = [...msg.guild.members.cache.filter( e => !e.user.bot && e.user.id != msg.author.id ).random( count ).filter( e => e )];
+     members = [...msg.guild.members.cache.filter( e => !e.user.bot && e.user.id != msg.author.id ).random( count ).filter( e => e )],
      sum     = Math.floor(cash / members.length);
 
     members.forEach(e => e.user.data.coins += sum);
