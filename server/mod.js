bdb259522940023ec00057f54317aa4eb9be6469
Alexsandr1x <zoodogood@gmail.com>
2022-12-13 11:47:20 +0200

    Port forwarding

--- server/mod.js
+++ server/mod.js
@@ -1,23 +1,42 @@
 import 'dotenv/config';
 
+import config from '#src/config';
 import express from './express.js';
 import Router from './router.js';
+
+import { checkPort } from './util.js';
+
+const raiseServer = async (port) => {
+	while (true){
+		const isOpen = await checkPort(port);
+		if (isOpen){
+			break;
+		}
+
+		port++;
+	}
+	
+
+	return await new Promise(async (resolve) => {
+		const server = express.listen({ port }, async () => resolve(server));
+	})
+}
+
+function logger(server){
+	const {address, port} = server.address();
+	console.info(`Listen on http://${ address === "::" ? "localhost" : address }:${ port }/`);
+}
 
 export default async () => {
 	const router = await new Router().fetch();
 	router.bind(express);
 
 
-	const port = process.env.PORT ?? 8080;
-
-	express.listen({ port }, (err, address) => {
-	if (err) throw err;
-
+	const port = config.port ?? 8001;
+	const server = await raiseServer(port);
 	
-	// console.clear();
-	console.info(`Listen on http://${ process.env.INSTANCE_HOST || "localhost" }:${ port }/`);
-	console.info("Ready...\n\n");
-})
+	
+	logger(server);
 }
 
 
