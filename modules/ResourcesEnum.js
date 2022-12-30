1433c102ecd4640f4874c399d347ddeae71cd771
Alexsandr1x <zoodogood@gmail.com>
2022-12-30 17:03:13 +0200

    Working on a module

--- modules/ResourcesEnum.js
+++ modules/ResourcesEnum.js
@@ -1,5 +1,31 @@
-class ResourcesEnum {
+import * as Util from '#src/modules/util.js';
+import { Collection } from '@discordjs/collection';
 
+class ResourcesEnum {
+	static endingOf(resource, quantity){
+		const item = this.collection.get(resource);
+		if (!item){
+			return undefined;
+		}
+		return Util.ending(quantity, item.base, ...item.suffixes, {unite: (_, word) => word});
+	}
+
+	static emojiOf(resource){
+		const item = this.collection.get(resource);
+		if (!item){
+			return undefined;
+		}
+		return item.emoji;
+	}
+
+	static collection = new Collection(Object.entries({
+		coins: {
+			key: "coins",
+			emoji: "<:coin:637533074879414272>",
+			base: "коин",
+			suffixes: ["ов", "", "а"]
+		}
+	}));
 }
 
 export default ResourcesEnum;
