c44f7103ffc9830eeba376a54eab844b50677a62
Alexsandr1x <zoodogood@gmail.com>
2023-01-09 14:06:17 +0200

    Add bossEffectTimeoutEnd event

--- index.js
+++ index.js
@@ -1260,6 +1260,19 @@ const timeEvents = {
     }
 
     CurseManager.checkAvailable({user, curse});
+  },
+  bossEffectTimeoutEnd: function(isLost, userId, timestamp){
+    const user = client.users.cache.get(userId);
+    const effects = user.data.bossEffects;
+    
+    const compare = effect => effect.timestamp === timestamp;
+    const effect = effects.find(compare);
+
+    if (!effect){
+      return;
+    }
+
+    user.action(Actions.bossEffectTimeoutEnd, effect);
   }
 };
 
