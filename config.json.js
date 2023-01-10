8a3645003dbab50d2142828a2a3fffcc4e644a93
Alexsandr1x <zoodogood@gmail.com>
2023-01-10 14:50:08 +0200

    Fix

--- config.json.js
+++ config.json.js
@@ -1,5 +1,5 @@
 
-const { default: packageConfig } = await import("#src/config", {assert: {type: "json"}});
+const { default: packageConfig } = await import("#src/package.json", {assert: {type: "json"}});
 
 export default {
 	developers: ["416701743733145612", "469879141873745921", "500293566187307008", "535402224373989396", "921403577539387454", "711450675938197565"],
