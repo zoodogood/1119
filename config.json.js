fa0c3a6e670bf1476676f1330d053358f31d693c
zoodogood <zoodogood@gmail.com>
2023-01-10 13:39:00 +0200

    Add fields

--- config.json.js
+++ config.json.js
@@ -1,3 +1,10 @@
+
+const { default: packageConfig } = await import("#src/config", {assert: {type: "json"}});
+
 export default {
-	developers: ["416701743733145612", "469879141873745921", "500293566187307008", "535402224373989396", "921403577539387454", "711450675938197565"]
+	developers: ["416701743733145612", "469879141873745921", "500293566187307008", "535402224373989396", "921403577539387454", "711450675938197565"],
+	guild: {
+		url: "https://discord.gg/76hCg2h7r8"
+	},
+	version: packageConfig.version
 }
