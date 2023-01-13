589afbf72e8c66dd8d98deb633cc0afbfa52dbb9
zoodogood <zoodogood@gmail.com>
2023-01-13 10:26:45 +0200

    Create TimeEvents executer

---
+++ events/TimeEvents/mod.js
@@ -0,0 +1,23 @@
+import { BaseEvent } from '#src/modules/EventsManager.js';
+import TimeEventsManager from '#src/modules/TimeEventsManager.js';
+import EventsManager from '#src/modules/EventsManager.js';
+
+
+class Event extends BaseEvent {
+	constructor(){
+		const EVENT = "event";
+		super(TimeEventsManager.emitter, EVENT);
+	}
+
+	run(event){
+		const eventBase = EventsManager.collection.get(`TimeEvent/${ event.name }`);
+		const params = event.params ?? [];
+		eventBase.run(event.isLost, ...params);
+	}
+
+	options = {
+		name: "TimeEventManager-emit"
+	}
+}
+
+export default Event;
