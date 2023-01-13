2f74018b375d935695d0cdb7e3035e5b34990b8b
zoodogood <zoodogood@gmail.com>
2023-01-13 10:26:05 +0200

    Working

--- events/core/start.js
+++ events/core/start.js
@@ -1,6 +1,6 @@
 import { BaseEvent } from "#src/modules/EventsManager.js";
 import { assert } from 'console';
-import { DataManager, TimeEventsManager, CommandsManager, CounterManager, Util } from '#src/modules/mod.js';
+import { DataManager, TimeEventsManager, CommandsManager, CounterManager, Util, EventsManager } from '#src/modules/mod.js';
 import { client } from '#src/index.js';
 
 class Event extends BaseEvent {
@@ -10,23 +10,20 @@ class Event extends BaseEvent {
 	}
 
 	async run(){
+		EventsManager.listenAll();
+
 		await DataManager.file.load();
 		await TimeEventsManager.file.load();
 		// await ReactionsManager.loadReactionsFromFile();
 		await CounterManager.file.load();
 
-
+		
 
 
 		await CommandsManager.importCommands();
 		CommandsManager.createCallMap();
 
 
-
-		TimeEventsManager.emitter.on("event", (event) => {
-			const params = event.params ?? [];
-			timeEvents[event.name].call(null, event.isLost, ...params);
-		});
 
 		TimeEventsManager.handle();
 
@@ -37,7 +34,8 @@ class Event extends BaseEvent {
 		DataManager.data.users = DataManager.data.users.sort((a, b) => b.level - a.level);
 
 		if (DataManager.data.bot.dayDate !== Util.toDayDate( now )){
-			timeEvents["new_day"].call(null, true);
+			EventsManager.collection.get("TimeEvent/day-stats")
+				.run(true);
 		}
 
 		assert(DataManager.data.users);
