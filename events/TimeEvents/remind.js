c3bad514b40e460c75607b9764741e8b0584be48
zoodogood <zoodogood@gmail.com>
2023-01-28 10:55:42 +0200

    Restore remind event

---
+++ events/TimeEvents/remind.js
@@ -0,0 +1,24 @@
+import {TimeEventsManager, DataManager} from '#src/modules/mod.js';
+
+
+class Event {
+
+	run(isLost, authorId, channelId, phrase){
+		const channel = client.channels.cache.get(channelId);
+		const author  = client.users.cache.get(authorId);
+
+		const target = channel || author;
+
+		if (target !== author)
+			target.msg({content: author.toString(), mentions: [author.id]});
+			
+		target.msg({title: "Напоминание:", description: phrase, footer: isLost ? null : "Ваше напоминание не могло быть доставлено вовремя."});
+	}
+
+
+	options = {
+		name: "TimeEvent/remind"
+	}
+}
+
+export default Event;
