d759493b60a2afc8e25f09eb598654b482b14b27
zoodogood <zoodogood@gmail.com>
2023-01-29 21:09:22 +0200

    Working on a command

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,107 @@
+//@ts-check
 import * as Util from '#src/modules/util.js';
+import { MessageMentions } from 'discord.js';
+
 
 class Command {
+
+  removeNotAvailableRoles({guild, tieRoles}){
+    for (const controllerId in tieRoles){
+      const role = guild.roles.cache.get(controllerId);
+      if (!role){
+        delete tieRoles[controllerId];
+        continue;
+      }
+
+      const controllerList = tieRoles[controllerId];
+
+      for (const id of [...controllerList]){
+        const role = guild.roles.cache.get(id);
+        if (!role){
+          const index = controllerList.indexOf(id);
+          (~index) && controllerList.splice(index, 1);
+        }
+
+        if (!controllerList.length){
+          delete tieRoles[controllerId];
+        }
+        continue;
+      }
+    }
+    return;
+  }
+
+  async displayUserRolesInterface({user, interaction}){
+    const tieRoles = interaction.guild.data.tieRoles;
+
+    let roleId = interaction.params
+      .replace(MessageMentions.UsersPattern, "")
+      .trim()
+      .match(/\d{17,19}/)
+      ?.[1];
+
+    const authorRoles = interaction.member.roles;
+    const controledRoles = [...new Set(
+      Object.entries(tieRoles)
+        .filter(authorRoles.cache.has)
+        .map((id, roles) => roles)
+        .flat()
+    )];
+    
+
+    if (!controledRoles.length){
+      interaction.channel.msg({
+        title: "На этом сервере нет ролей, которыми вы могли бы управлять",
+        color: "#ff0000",
+        delete: 5000,
+        footer: {text: interaction.user.username, iconURL: interaction.user.avatarURL()}
+      });
+      return;
+    }
+
+    if (!roleId){
+      const numberReactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
+        .slice(0, controledRoles.length);
+
+      const message = await interaction.channel.msg({title: "Вы не указали айди роли", description: `Выберите доступную вам роль, чтобы снять или выдать её пользователю ${ user.toString() }\n${controledRoles.map((id, i) => `${numberReactions[i]} ${guildRoles[id]}`).join("\n")}`, color: "#00ff00"});
+      const react = await message.awaitReact({user: interaction.user, removeType: "all"}, ...numberReactions);
+      message.delete();
+      react && (roleId = controledRoles.at( numberReactions.indexOf(react) ));
+    }
+
+    if (!roleId){
+      return;
+    }
+
+    const role = interaction.guild.roles.cache.get(roleId);
+
+    if (!role){
+      interaction.channel.msg({
+        title: "Не удалось найти роль",
+        description: `На сервере не найдено роли со следующим ID: \`${ roleId }\``
+      });
+      return;
+    }
+
+    if (!controledRoles.includes(roleId)){
+      interaction.channel.msg({
+        title: "Отсуствуют связанные роли",
+        description: `Вы не можете выдавать ${ role.toString() }. У вас нет связанных с ней контролирующих ролей.\nИх могут создавать и редактировать администраторы сервера командой \`!role\``
+      });
+      return;
+    }
+
+    const member = interaction.guild.members.resolve(user);
+    const heAlreadyHas = member.roles.cache.has(roleId);
+    member.roles[heAlreadyHas ? "remove" : "add"](roleId);
+    interaction.channel.msg({
+      title: "Роли участника успешно изменены",
+      description: `${heAlreadyHas ? `У ${ member.toString() } отняли` : `${ member.toString() } получил`} роль ${ role.toString() }`,
+      delete: 5_000
+    });
+    return;
+  }
+
 
 	async onChatInput(msg, interaction){
     let heAccpet = await Util.awaitUserAccept({name: "tieRoles", message: "С помощью этой команды администраторы серверов могут дать своим модераторам возможность выдавать или снимать определенные роли, не давая создавать новые или управлять старыми", channel: msg.channel, userData: interaction.userData});
@@ -8,51 +109,17 @@ class Command {
       return;
     }
 
-    let tieRoles = msg.guild.data.tieRoles || (msg.guild.data.tieRoles = {});
+    const tieRoles = msg.guild.data.tieRoles || (msg.guild.data.tieRoles = {});
     const guildRoles = {};
 
-    // Фильтруем роли-контроллеры существуют ли они.
-    Object.keys(tieRoles).forEach((control) => {
-      controlRole = guildRoles[control] = msg.guild.roles.cache.get(control);
-      if (!controlRole) delete tieRoles[control];
-    });
-    // Фильтруем контролируемые
-    Object.entries(tieRoles).forEach(([control, roles]) => tieRoles[control] = roles.filter(e => {
-      return guildRoles[e] = guildRoles[e] || msg.guild.roles.cache.get(e);
-    }));
+    
+    this.removeNotAvailableRoles({guild: interaction.guild, tieRoles});
+    
 
 
     if (interaction.mention){
-      let memb = interaction.mention;
-      let myControled = interaction.mention.roles.cache.filter(e => Object.keys(tieRoles).includes(e.id)).map(e => e.id);
-      let [mention, id] = interaction.params.split(" ");
-
-      let controledRoles = new Set();
-      Object.entries(tieRoles).filter(([control, roles]) => myControled.includes(control)).map(([control, roles]) => roles).forEach(e => controledRoles.add(e));
-      controledRoles = [...controledRoles];
-
-      if (!controledRoles.length) {
-        msg.msg({title: "На этом сервере нет ролей, которыми вы могли бы управлять", color: "#ff0000", delete: 5000});
-        return;
-      }
-
-      if (!id) {
-        numberReactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"].slice(0, controledRoles.length);
-        let message = await msg.msg({title: "Вы не указали айди роли", description: `Выберите доступную вам роль, чтобы снять или выдать её пользователю ${memb}\n${controledRoles.map((id, i) => `${numberReactions[i]} ${guildRoles[id]}`).join("\n")}`, color: "#00ff00"});
-        let react = await message.awaitReact({user: msg.author, removeType: "all"}, ...numberReactions);
-        message.delete();
-        if (react) id = controledRoles[numberReactions.indexOf(react)];
-      }
-
-      if (!controledRoles.includes(id)) {
-        msg.msg({title: "Отсуствуют связанные роли", description: `Вы не можете выдавать ${guildRoles[id]}, так как у вас нет связанных с ней контролирующих ролей.\nИх могут создавать и редактировать администраторы сервера командой \`!role\``});
-        return;
-      }
-
-      memb = msg.guild.members.resolve(memb);
-      let heHas = memb.roles.cache.find(e => e.id == id);
-      memb.roles[heHas ? "remove" : "add"](id);
-      msg.msg({title: "Роли участника успешно изменены", description: `${heHas ? `У ${memb} отняли` : `${memb} получил`} роль ${guildRoles[id]}`, delete: 5000});
+      const user = interaction.mention;
+      this.displayUserRolesInterface({user, interaction});
       return;
     }
 
