b365d3af2e2d144da49d4d4addb82509078cb06c
zoodogood <zoodogood@gmail.com>
2023-01-30 17:49:17 +0200

    Fix

--- modules/CurseManager.js
+++ modules/CurseManager.js
@@ -226,7 +226,7 @@ class CurseManager {
  
 				 messages.push( now );
  
-				 const extraTimeForMobileUsers = 5_500 * ("mobile" in (user.presence.clientStatus || {}));
+				 const extraTimeForMobileUsers = 5_500 * ("mobile" in (user.presence?.clientStatus || {}));
 				 const TIMEOUT = 90_000 + extraTimeForMobileUsers;
  
 				 while (messages[0] + TIMEOUT < now){
