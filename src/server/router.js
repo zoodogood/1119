5fd0484b80720bdcdcbf620edfc0df5d38dd2f46
Alexsandr1x <zoodogood@gmail.com>
2023-02-12 17:30:36 +0200

    Add errors handler decorator & Refact

--- src/server/router.js
+++ src/server/router.js
@@ -21,9 +21,36 @@ class Router {
 
 	bind(express){
 		for (const {default: Route} of this.routes){
-			new Route(express);
+			const route = new Route(express);
+			this.handle(route, express);
 		} 
+	}
+
+	handle(route, express){
+		route.get && express.get(
+			route.prefix,
+			(...params) => this.decorate({params, route, callback: route.get})
+		);
+
+		route.post && express.post(
+			route.prefix,
+			(...params) => this.decorate({params, route, callback: route.post})
+		);
+	}
+
+	async decorate({params, route, callback}){
+		try {
+			await callback.apply(route, params);
+		} catch (error) {
+			const next = params.at(-1);
+			next(error);
+		}
 	}
 }
 
+class BaseRoute {
+
+}
+
 export default Router;
+export { Router, BaseRoute };
