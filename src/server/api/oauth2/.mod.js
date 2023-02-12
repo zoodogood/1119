87088030d46b9a390b114450da1716b63d6eefd8
Alexsandr1x <zoodogood@gmail.com>
2023-02-12 16:17:36 +0200

    Fix

--- src/server/api/oauth2/.mod.js
+++ src/server/api/oauth2/.mod.js
@@ -3,13 +3,12 @@ import OAuth from "@arthur.dev/discord-oauth2";
 
 /** @type {OAuth} */
 const oauth2 = new OAuth.default({
-	clientId: client.id,
+	clientId: client.user.id,
 	clientSecret: process.env.DISCORD_OAUTH2_TOKEN,
 
 	scope: ["identify", "guilds"],
 	callbackUrl: "http://localhost:8001/oauth2/callback",
 });
-
 
 export default oauth2;
 
