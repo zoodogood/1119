e0de986f471653de17b20fc0db865d0d077796af
Alexsandr1x <zoodogood@gmail.com>
2023-02-13 18:39:21 +0200

    Refact & Improve

--- folder/commands/restart.js
+++ folder/commands/restart.js
@@ -1,31 +1,42 @@
 
-import { exec } from 'child_process';
+import config from '#config';
+import get from '#lib/child-process-utils.js';
+const {run, info} = get({ root: process.cwd() });
+
+
 
 
 class Command {
 
 	async onChatInput(msg, interaction){
-		const COMMAND = "git pull && npm run build && pm2 restart --update-env blackghost";
+		const COMMANDS = [
+			"git pull",
+			"npm.cmd run build",
+			config.pm2.id ? `npm.cmd run pm2-please-restart ${ config.pm2.id }` : "echo pm2 not setted"
+		];
 		
 		const embed = {
 			title: "<:emoji_50:753916145177722941>",
 			color: "#2c2f33",
-			description: `> ${ COMMAND }`
+			description: `**RESTARTING...**`
 		};
 
 		const message = await msg.msg(embed);
 		embed.edit = true;
 
 		const updateDescription = (content) => {
-			embed.description += `\n${ content }`;
-			message.msg({...embed, description: `\`\`\`\n${ embed.description }\`\`\``});
-
+			embed.description += `\n\`\`\`ansi\n${ content }\`\`\``;
+			message.msg(embed);
 		}
-		const child = exec(COMMAND);	
 		
-		child.stdout.on("data", (data) => updateDescription(data));
-		child.stderr.on("data", (data) => updateDescription(`Error:\n${ data }`));
-		child.on("error", (error) => {throw error});
+		for (const string of COMMANDS) {
+			const [command, ...params] = string.split(" ");
+			embed.description += `\n> ${ string }`;
+			const result = await run(command, params)
+				.catch((error) => `Error: ${ error.message }`);
+
+			updateDescription(result);
+		}
   	}
 
 
