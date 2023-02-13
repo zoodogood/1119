f3f32e7800fa24a9d976ae38f120839ea88c8b9f
zoodogood <zoodogood@gmail.com>
2023-02-13 09:45:52 +0200

    Create build bat file

---
+++ folder/scripts/build.bat
@@ -0,0 +1,19 @@
+cd ../../
+
+echo "Node version:"
+call node -v || echo "ERROR: Need install Node.js" && exit /b
+
+
+echo "Install modules:"
+call npm install || echo "Unknow error" && exit /b
+
+
+echo "Check files:"
+call node ./folder/scripts/checkFiles.js || echo "Unknow error" && exit /b
+
+
+echo "Build bundle:"
+call rollup -c ./src/site/rollup.config.mjs || echo "Unknow error" && exit /b
+
+
+echo "Success!"
---
+++ folder/scripts/checkFiles.js
@@ -0,0 +1,58 @@
+import { execSync } from 'child_process';
+import FileSystem from 'fs/promises';
+import Path from 'path';
+
+const root = process.cwd();
+const resolve = (path) => Path.resolve(root, path);
+
+const FileExists = async (path) =>
+	!!(await FileSystem.stat(resolve(path)).catch(err => null));
+
+const Write = async (path, content) => 
+	await FileSystem.writeFile(resolve(path), content);
+
+const Paths = {
+	env: ".env",
+	envExample: ".env.example",
+	main: "folder/data/main.json",
+	counter: "folder/data/counter.json",
+	time: "folder/data/time.json",
+
+	firstBuildDocs: "static/special/first-build.html"
+}
+
+
+
+
+
+!FileExists(Path.env) && await 
+(async () => {
+	execSync(`start "" "file://${ resolve(Paths.firstBuildDocs) }"`);
+	const source = resolve(Paths.envExample);
+	const target = resolve(Paths.env);
+	await FileSystem.copyFile(source, target);
+})();
+
+
+!FileExists(Paths.main) && await 
+(async () => {
+	const DEFAULT_DATA = {bot: {}, users: [], guilds: []};
+	const content = JSON.stringify(DEFAULT_DATA);
+	await Write(Paths.main, content);
+})();
+
+!FileExists(Paths.counter) && await 
+(async () => {
+	const DEFAULT_DATA = [];
+	const content = JSON.stringify(DEFAULT_DATA);
+	await Write(Paths.counter, content);
+})();
+
+!FileExists(Paths.time) && await 
+(async () => {
+	const DEFAULT_DATA = {};
+	const content = JSON.stringify(DEFAULT_DATA);
+	await Write(Paths.time, content);
+})();
+
+
