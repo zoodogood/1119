d5b38dc70c074150cb7502b954797756278b5826
zoodogood <zoodogood@gmail.com>
2023-02-14 18:28:01 +0200

    Major changes

--- src/site/src/lib/Router.js
+++ src/site/src/lib/Router.js
@@ -1,25 +1,82 @@
-import PagesEnum from "#static/build/svelte-pages/enum[builded].mjs";
 import { relativeSiteRoot } from '#lib/safe-utils.js';
+import { Collection } from "@discordjs/collection";
+import config from '#config';
+import Path from 'path';
+import app from '#app';
 
-class Router {
-	static #pagesEnum = PagesEnum;
-	static #pathEnum = this.#resolvePathEnum(this.#pagesEnum);
+class PagesRouter {
 
-	static #resolvePathEnum(pagesEnum){
-		const pages = Object.keys(pagesEnum);
-		const toPath = page => page.replaceAll("_", "/");
-		const entries = pages.map((page) => [page, toPath(page)]);
-		return Object.fromEntries(entries);
+	static PAGES_FOLDER_PATH = "./src/site/src/pages";
+	static collection = new Collection();
+
+	static resolvePages(PagesURLs){
+		for (const path of PagesURLs){
+			const page = this.resolvePage(path);
+			this.addPage(page);
+		}
+	}
+
+	static resolvePage(path){
+		const key = this.resolvePageName(path);
+
+		const relativeToPages = (() => {
+			const isFolderIndex = (arr) => arr.at(-1).toLowerCase() === "index";
+			const arr = key.split("_");
+			
+			isFolderIndex(arr) && arr.splice(-1, 1);
+			return arr.join("/");
+		})();
+		
+		return {source: path, key, relativeToPages};
+	}
+
+	static resolvePageName(path){
+		const directory = this.PAGES_FOLDER_PATH;
+	
+	
+		const name = Path
+			.relative(directory, path)
+			.replaceAll(/\\|\//g, "_")
+			.replaceAll("+", "")
+			.replaceAll(/\..+$/g, "")
+			.toLowerCase()
+			.trim();
+
+		return name;
+	}
+
+	
+
+	static addPage(page){
+		const collection = this.collection;
+		collection.set(page.key , page);
+		collection.set(page.relativeToPages , page);
+		collection.set(page.source , page);
+	}
+
+	static getPageBy(any){
+		return this.collection.get(any);
 	}
 
 	static get pages(){
-		return this.#pathEnum;
+		return Object.fromEntries(
+			[...this.collection.entries()]
+				.map(([any, {key}]) => [any, key.replaceAll("_", "/")])
+		);
 	}
 
 	static relativeToPage(path){
+		const svelteApp = app.svelte;
+		return relativeSiteRoot(svelteApp, path);
+	}
 
-		relativeSiteRoot(location, path)
+	static redirect(path){
+		app.svelte.document.location.href
+			= this.relativeToPage(path);
 	}
 }
 
-export default Router;
+
+
+export default PagesRouter;
+export { PagesRouter };
