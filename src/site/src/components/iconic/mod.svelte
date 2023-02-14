3daa519b686c4c2a8503a0bff04329634c62ba51
zoodogood <zoodogood@gmail.com>
2023-02-14 18:27:39 +0200

    Create iconic

---
+++ src/site/src/components/iconic/mod.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let code = "";
+</script>
+
+<span class = "icon" data-symbol = { code }></span>
+
+<style>
+	.icon::after
+	{
+		font-family: 'Icons';
+		content: attr(data-symbol);
+	}
+</style>
