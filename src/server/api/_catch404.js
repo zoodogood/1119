0fd82bc109560f3c8993e2803c4db72ad8392c19
zoodogood <zoodogood@gmail.com>
2023-02-14 19:19:41 +0200

    Catch 404

---
+++ src/server/api/_catch404.js
@@ -0,0 +1,25 @@
+import ErrorsHandler from "#src/lib/modules/ErrorsHandler.js";
+import { sleep } from "#lib/util.js";
+import { BaseRoute } from "#server/router.js";
+
+
+class Route extends BaseRoute {
+	constructor(express){
+		super();
+
+		this.start();
+		this.express = express;
+	}
+
+	async catch(request, response){
+		response.status(404);
+		response.send("404");
+	}
+
+	async start(){
+		await sleep(1);
+		this.express.use(this.catch);
+	}
+}
+
+export default Route;
