40a26c3688f3f87d735bf6493c34d99f9aa1a8f1
zoodogood <zoodogood@gmail.com>
2023-02-14 18:29:56 +0200

    Refact

--- src/site/src/core/router.js
+++ src/site/src/core/router.js
@@ -1,24 +1,21 @@
-import * as PagesEnum from '#static/build/svelte-pages/exports[builded].mjs';
+import * as PagesExports from '#static/build/svelte-pages/exports[builded].mjs';
+import PagesURLs from '#static/build/svelte-pages/enum[builded].mjs';
 
+import Router from '#site/lib/Router.js';
 
+Router.resolvePages(PagesURLs);
 
 function route(svelteApp){
 	
 	const subpath = svelteApp.url.subpath
 		.filter(subpath => !subpath.startsWith(":"))
 		.join("_");
-
-
-	const _Page = Object.entries(PagesEnum)
-		.find(([name]) => name.toLocaleLowerCase() === subpath.toLocaleLowerCase())
-		?.at(1) ?? PagesEnum.index;
-
-
-	new _Page({
-		target: svelteApp.document.body
-	});
+	
+	const key = Router.getPageBy( subpath.toLocaleLowerCase() )?.key;
+	const _Page = PagesExports[key] ?? PagesExports.index;
 
 	
+	return _Page;
 }
 
 export { route };
