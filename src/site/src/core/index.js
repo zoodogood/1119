36ae28bb7fe92a3ff58e7134a32051508641748f
zoodogood <zoodogood@gmail.com>
2023-02-14 18:29:43 +0200

    Rename & Refact

---
+++ src/site/src/core/index.js
@@ -0,0 +1,14 @@
+import {route} from '#site/core/router.js';
+import svelteApp from './svelte-app.js';
+import PageWrapper from '#site-component/PageWrapper';
+
+import app from '#app';
+app.svelte = svelteApp;
+
+const page = route(svelteApp);
+new PageWrapper({
+	target: svelteApp.document.body,
+	props: {
+		page
+	}
+});
--- src/site/src/core/main.js
+++
@@ -1,8 +0,0 @@
-import {route} from '#site/core/router.js';
-import svelteApp from './svelte-app.js';
-
-import app from '#app';
-
-app.svelte = svelteApp;
-
-route(svelteApp);
