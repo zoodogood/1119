f9e89d88421a2e83587cbf1b203a226ab222c942
zoodogood <zoodogood@gmail.com>
2023-02-15 10:35:55 +0200

    Remove hash; Save query

--- src/site/src/lib/util.js
+++ src/site/src/lib/util.js
@@ -1,10 +1,10 @@
-function relativeSiteRoot(svelteApp, path, removeHash = true){
-	 const {origin, hash} = svelteApp.document.location;
+function relativeSiteRoot(svelteApp, path, removeQueries = true){
+	 const {origin, search} = svelteApp.document.location;
 	 const pathname = Object.values(svelteApp.url.base)
 		 .filter(Boolean)
 		 .join("/");
 		 
-	 return `${ origin }/${ pathname }/${ path }${ removeHash ? "" : `#${ hash }` }`;
+	 return `${ origin }/${ pathname }/${ path }${ removeQueries ? "" : `${ search }` }`;
  }
 
  export { relativeSiteRoot };
