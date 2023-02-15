1c0deda1d00226c4ee38baf01784f08d84205b2e
Alexsandr1x <zoodogood@gmail.com>
2023-02-15 17:16:38 +0200

    Working on a page

--- src/site/src/pages/public/+index.svelte
+++ src/site/src/pages/public/+index.svelte
@@ -1,6 +1,12 @@
 <Header/>
-123
+<main>
+	Сундук Кота это круто
+</main>
+<Footer/>
 
 <script>
 	import Header from '#site-component/Header';
+	import Footer from '#site-component/Footer';
+
+
 </script>
