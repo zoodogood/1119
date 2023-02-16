4b46af42e6f3053061c0f82f0c11863425d1a1d5
zoodogood <zoodogood@gmail.com>
2023-02-16 17:43:08 +0200

    Working on a page

---
+++ src/site/src/pages/navigation/+index.svelte
@@ -0,0 +1,83 @@
+
+<Header/>
+
+<Main>
+	<center>
+		<h1>Навигация</h1>
+		<hr>
+	</center>
+
+	<section>
+
+
+		<details open>
+			<summary>Страницы</summary>
+			<ul>
+				{#each Object.values(PagesRouter.pages) as pageKey}
+					{@const url = PagesRouter.relativeToPage(pageKey)}
+					<li>	
+						<a href={url}>{ url.replace(config.server.origin, "") }</a>
+					</li>
+				{/each}
+			</ul>
+		</details>
+		
+		<details open>
+			<summary>API'шечки</summary>
+			Пока здесь пусто ;)
+		</details>
+
+
+	</section>
+</Main>
+
+<Footer/>
+
+<script>
+	console.info({pages: PagesRouter.pages});
+	import PagesRouter from '#site/lib/Router.js';
+	
+	import Header from '#site-component/Header';
+	import Main from '#site-component/Main';	
+	import Footer from '#site-component/Footer';
+  	import config from '#config';
+</script>
+
+<style>
+	section 
+	{
+		display: flex;
+		flex-wrap: wrap;
+		align-items: stretch;
+	}
+
+	summary
+	{
+		font-weight: 100;
+	}
+
+	li 
+	{
+		font-size: 0.7em;
+		list-style: inside;
+		color: #17a3d6;
+		opacity: 0.8;
+
+		font-weight: 800;
+	}
+
+	details
+	{
+		border-radius: 15px;
+		background-color: #88888822;
+		padding: 15px;
+		margin: 15px;
+		width: max-content;
+		min-width: 300px;
+	}
+
+	details:not([open])
+	{
+		align-self: flex-start;
+	}
+</style>
