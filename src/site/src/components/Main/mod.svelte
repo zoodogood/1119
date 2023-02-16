a176bce4b0f7d92d31820eb0dac985c3b2655927
zoodogood <zoodogood@gmail.com>
2023-02-16 17:42:44 +0200

    Create Main

---
+++ src/site/src/components/Main/mod.svelte
@@ -0,0 +1,16 @@
+<main>
+	<slot/>
+</main>
+
+<style>
+	main
+	{
+		padding: calc(1em + 3vw);
+		display: flex;
+		flex-direction: column;
+
+		gap: 2vh;
+		padding-top: 1.5vw;
+		min-height: 50vh;
+	}
+</style>
