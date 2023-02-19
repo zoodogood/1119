7bbbc811e618f74da28c5c87eb1df56ca693739d
Alexsandr1x <zoodogood@gmail.com>
2023-02-19 12:22:58 +0200

    Init a page

---
+++ src/site/src/pages/friendship/+index.svelte
@@ -0,0 +1,28 @@
+<Header/>
+<Main>
+	<main>
+		<img src="https://media.discordapp.net/attachments/629546680840093696/1075447332226744371/campfire_.png" alt="fire">
+	</main>
+</Main>
+
+
+<style>
+	main
+	{
+		display: flex;
+		justify-content: center;
+	}
+
+	img
+	{
+		width: 70vw;
+	}
+</style>
+
+
+<script>
+	import Header from '#site-component/Header';
+	import Main from '#site-component/Main';
+
+
+</script>
