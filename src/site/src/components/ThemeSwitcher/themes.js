7f9f1ecfba24046801f538553972297e8e6407f3
zoodogood <zoodogood@gmail.com>
2023-02-23 20:47:56 +0200

    Add theme-accent-name field

--- src/site/src/components/ThemeSwitcher/themes.js
+++ src/site/src/components/ThemeSwitcher/themes.js
@@ -2,55 +2,64 @@ export default new Map(Object.entries({
 	brightGreen: {
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#349a4b"
+		"--main-color": "#349a4b",
+		"--theme-accent-name": "dark"
 	},
 
 	darkGreen: {
 		"--main-color": "#3b7c4c",
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
+		"--theme-accent-name": "dark"
 	},
 
 	lightGreen: {
 		"--main-color": "#3b7c4c",
 		"--text-theme-accent": "var(--dark)",
 		"--background-theme-accent": "var(--white)",
+		"--theme-accent-name": "light"
 	},
 
 	brightPurple: {
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#3b37a9"
+		"--main-color": "#3b37a9",
+		"--theme-accent-name": "dark"
 	},
 
 	darkPurple: {
 		"--main-color": "#3f3c8d",
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
+		"--theme-accent-name": "dark"
 	},
 
 	lightPurple: {
 		"--main-color": "#3f3c8d",
 		"--text-theme-accent": "var(--dark)",
 		"--background-theme-accent": "var(--white)",
+		"--theme-accent-name": "light"
 	},
 
 	brightBlue: {
 		"--main-color": "#409ff6",
 		"--text-theme-accent": "var(--dark)",
 		"--background-theme-accent": "var(--white)",
+		"--theme-accent-name": "light"
 	},
 
 	darkBlue: {
 		"--main-color": "#409ff6",
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
+		"--theme-accent-name": "dark"
 	},
 
 	lightBlue: {
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#0085ff"
+		"--main-color": "#0085ff",
+		"--theme-accent-name": "light"
 	},
 
 	
@@ -58,12 +67,14 @@ export default new Map(Object.entries({
 	contrast: {
 		"--text-theme-accent": "var(--dark)",
 		"--background-theme-accent": "var(--white)",
-		"--main-color": "#000000"
+		"--main-color": "#000000",
+		"--theme-accent-name": "light"
 	},
 
 	gray: {
 		"--text-theme-accent": "var(--white)",
 		"--background-theme-accent": "#1b1d21",
-		"--main-color": "#414d6b"
+		"--main-color": "#414d6b",
+		"--theme-accent-name": "dark"
 	}
 }))
