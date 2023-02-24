46f5cf6643d9a7ba92f6361f4b7dff6faf76b889
zoodogood <zoodogood@gmail.com>
2023-02-24 09:43:07 +0200

    Working on a page

---
+++ src/site/src/pages/articles/+create.svelte
@@ -0,0 +1,72 @@
+<Layout>
+	{#if !svelteApp.user}
+		<p>Сначала авторизуйтесь*</p>
+	{/if}
+	<h1>Написать статью</h1>
+
+	<main>
+		<label for="load-file" bind:this = { labelNode }>
+			<input type="file" id="load-file" accept=".md" on:change={onFileUpload}>
+			<button on:click = {() => labelNode.click()} disabled = {!svelteApp.user || null}>Загрузить .md файл</button>
+		</label>
+		
+	</main>
+	{#each Contents.fileSendAnswers as answer}
+		<p>Ответ от сервера:</p>
+		<span>{ answer }</span>
+	{/each}
+	<p>После загрузки содержимое обретёт публичный характер.</p>
+	<p>Вы сможете внести изменения в любой момент времени.</p>
+	<p>Ваши никнейм и аватар будут видны другим.</p>
+	<p>Других предупреждений нет.</p>
+	
+
+</Layout>
+
+
+<style>
+	input
+	{
+		display: none;
+	}
+</style>
+
+
+<script>
+	import Layout from '#site-component/Layout';
+	import Dialog from '#site/components/Dialog/mod.svelte';
+
+	import PagesRouter from '#site/lib/Router.js';
+  	import { fetchFromInnerApi, sleep } from '#lib/safe-utils.js';
+  	import { svelteApp } from '#site/core/svelte-app.js';
+
+	const Contents = {
+		fileSendAnswers: []
+	}
+	let labelNode;
+
+	const onFileUpload = (async (inputEvent) => {
+		const button = labelNode.querySelector("button");
+		button.setAttribute("disabled", true);
+
+		const files = inputEvent.target.files;
+		const file = files[0];
+
+		const headers = {Authorization: ""};
+		
+
+		const answer = await fetchFromInnerApi(
+			"site/articles/create",
+			{method: "POST", body: file, headers}
+		);
+
+		new Dialog({
+			target: document.body,
+			props: {title: "Ответ сервера:", description: answer, useClassic: true}
+		});
+		button.removeAttribute("disabled");
+	});
+	
+
+
+</script>
