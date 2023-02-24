ab4c4681e1a4367fb3d15ce8c66dd1a2462cf5e1
zoodogood <zoodogood@gmail.com>
2023-02-24 11:07:39 +0200

    Working on a oauth2/user point

---
+++ src/server/api/oauth2/user.js
@@ -0,0 +1,68 @@
+const PREFIX = "/oauth2/user";
+import client from '#bot/client.js';
+import { BaseRoute } from '#server/router.js';
+import oauth from './.mod.js';
+
+class TokensUsersExchanger {
+	static #cacheMap = new Map();
+
+	static fromCache(token){
+		const id = this.#cacheMap.get(token);
+		if (!id){
+			return null;
+		}
+
+		return client.users.cache.get(id) ?? null;
+	}
+
+	static addToCache(token, userId){
+		this.#cacheMap.set(token, userId);
+		return;
+	}
+
+	static async fromOAuth(token){
+		const data = await oauth.getOAuth2Data(token) ?? {};
+		const {user, guilds} = data;
+		
+		if (!user){
+			return null;
+		}
+
+		user.guilds = guilds;
+		return user;
+	}
+
+	static async getUser(token, {requireOAuth}){
+		return (!requireOAuth && this.fromCache(token)) || (await this.fromOAuth(token)) || null;
+	}
+}
+
+
+class Route extends BaseRoute {
+	prefix = PREFIX;
+
+	constructor(express){
+		super();
+	}
+
+	#cacheMap = new Map();
+
+	async get(request, responce){
+		const token = request.headers.authorization;
+		if (!token){
+			responce.status(401).send(`"Not authorized"`);
+			return;
+		}
+
+		const user = await TokensUsersExchanger.getUser(token, {requireOAuth: true});
+		if (user === null){
+			responce.status(401).send(`"Bad authorization"`);
+			return;
+		}
+
+		responce.json(user);
+	}
+}
+
+export default Route;
+export { TokensUsersExchanger };
