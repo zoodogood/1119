f1f7e775c5bbcb1275138c44466a8b2ea693aa3e
zoodogood <zoodogood@gmail.com>
2023-03-09 11:57:16 +0200

    Clean a code; Add whenDocumentReadyStateIsComplete

--- src/site/src/lib/util.js
+++ src/site/src/lib/util.js
@@ -1,10 +1,19 @@
 function relativeSiteRoot(svelteApp, path, removeQueries = true){
-	 const {origin, search} = svelteApp.document.location;
-	 const pathname = Object.values(svelteApp.url.base)
-		 .filter(Boolean)
-		 .join("/");
-		 
-	 return `${ origin }/${ pathname }/${ path }${ removeQueries ? "" : `${ search }` }`;
- }
+	const {origin, search} = svelteApp.document.location;
+	const pathname = Object.values(svelteApp.url.base)
+		.filter(Boolean)
+		.join("/");
+		
+	return `${ origin }/${ pathname }/${ path }${ removeQueries ? "" : `${ search }` }`;
+}
 
- export { relativeSiteRoot };
+async function whenDocumentReadyStateIsComplete(document){
+	return !document.readyState !== "complete" && await new Promise(resolve => 
+		document.addEventListener("readystatechange", resolve, {once: true})
+	);
+}
+
+export {
+	relativeSiteRoot,
+	whenDocumentReadyStateIsComplete
+};
