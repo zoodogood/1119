70bc7c953ff527ae9f4fbe5d4416d2b2db5450a4
Alexsandr1x <zoodogood@gmail.com>
2023-03-18 18:18:57 +0200

    Create api point

--- src/server/api/user/chest-open.js
+++ src/server/api/user/chest-open.js
@@ -1,5 +1,7 @@
+import client from "#bot/client.js";
 import { BaseRoute } from "#server/router.js";
-
+import { TokensUsersExchanger } from "#server/api/oauth2/user.js";
+import { ChestManager } from '#folder/commands/chest.js';
 const PREFIX = "/user/chest-open";
 
 
@@ -11,7 +13,30 @@ class Route extends BaseRoute {
 	}
 
 	async post(request, response){
+		const token = request.headers.authorization;
+		if (!token){
+			response.status(401).send(`"Not authorized"`);
+			return;
+		}
 		
+
+		const rawUser = await TokensUsersExchanger.getUserRaw(token, {requireOAuth: true});
+		if (rawUser === null){
+			response.status(401).send(`"Authorization failed"`);
+			return;
+		}
+
+		const user = client.users.cache.get(rawUser.id);
+		const cooldown = ChestManager.cooldown.for(user.data);
+
+		if (cooldown.checkYet()){
+			response.status(405).json({notAllowed: "cooldown", value: -cooldown.diff()});
+			return;
+		}
+
+		const resources = ChestManager.open({userData: user.data});
+		cooldown.install();
+		response.json(resources);
 	}
 }
 
