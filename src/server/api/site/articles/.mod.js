d6a1d6a652d17848c824f5ed277df7ccc9aa7f11
Alexsandr1x <zoodogood@gmail.com>
2023-03-19 17:52:37 +0200

    Add createArticle

--- src/server/api/site/articles/.mod.js
+++ src/server/api/site/articles/.mod.js
@@ -1,4 +1,6 @@
+import { MarkdownMetadata } from '#lib/util.js';
 import { ImportDirectory } from '@zoodogood/import-directory';
+import FileSystem from 'fs/promises';
 import Path from 'path';
 
 
@@ -6,11 +8,13 @@ const DIRECTORY = "static/articles";
 		
 
 class ArticlesManager {
+	static directory = DIRECTORY;
+
 	static async fetchArticles(){
-		const filesPaths = await new ImportDirectory({regex: /\.md$/}).takeFilesPath({path: DIRECTORY});
+		const filesPaths = await new ImportDirectory({regex: /\.md$/}).takeFilesPath({path: DIRECTORY, subfolders: true});
 
 		const articles = filesPaths
-			.map(path => Path.relative(DIRECTORY, path));
+			.map(path => Path.relative(DIRECTORY, path).replaceAll("\\", "/"));
 
 		return articles;
 	}
@@ -20,7 +24,7 @@ class ArticlesManager {
 		const full = Path.resolve(this.directory, `${ path }.md`);
 		try {
 			const content = String(await FileSystem.readFile(full));
-			response.json( content );
+			return content;
 		}
 		catch (error){
 			if (error.code !== "ENOENT"){
@@ -29,6 +33,22 @@ class ArticlesManager {
 
 			return null;
 		}
+	}
+
+	static async createArticle({id, author, content}){
+		const path = id.replace(/\.md$/, "");
+		const full = Path.resolve(this.directory, `${ path }.md`);
+
+		
+		await FileSystem.mkdir(Path.resolve(full, "../"), { recursive: true });
+		const metadata = MarkdownMetadata.parse(content) ?? {};
+		Object.assign(metadata, {author, timestamp: Date.now()});
+
+		content = MarkdownMetadata.replace(content, metadata);
+
+		await FileSystem.mkdir(Path.resolve(full, "../"), { recursive: true });
+		await FileSystem.writeFile(full, content);
+		return metadata;
 	}
 }
 
