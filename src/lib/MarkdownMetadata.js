351016d8337d82321b56bf540561481fa5fc1762
Alexsandr1x <zoodogood@gmail.com>
2023-03-19 17:51:39 +0200

    Refact; Improve; Fix; Add removeMetadataFieldIn

--- src/lib/MarkdownMetadata.js
+++ src/lib/MarkdownMetadata.js
@@ -1,35 +1,45 @@
 import yaml from 'yamljs';
 
 class MarkdownMetadata {
-	regex = /(?<=^---\n).*?(?=\n^---)/sm;
+	static regex = /(?<open>(?:\s\t)*^---(?:\s\t)*\n)(?<content>.*?)(?<close>(?:\s\t)*^---(?:\s\t)*\n)/sm;
+	static FIELD_CONTENT = "---\n\n---\n";
 
-	replace(content, data){
+	static yaml = yaml;
+
+	static replace(content, data){
 		if (!content.match(this.regex)){
 			content = this.createMetadataFieldIn(content);
 		}
 
 
 		data = yaml.stringify(data);
-		return content.replace(this.regex, data);
+		return content.replace(
+			this.regex,
+			(_full, open, _content, close) => `${ open }${ data }${ close }`
+		);
 	}
 
-	parse(content){
-		const metadata = content.match(this.regex)?.[0];
+	static parse(content){
+		const metadata = content.match(this.regex);
 		if (!metadata){
 			return null;
 		}
 
-		return yaml.parse(metadata);
+		return yaml.parse(metadata.groups.content);
 	}
 
-	assign(content, data){
+	static assign(content, data){
 		data = Object.assign(data, this.parse(content));
 		return this.replace(content, data);
 	}
 
-	createMetadataFieldIn(content){
-		const field = "---\n\n\n---\n"
+	static createMetadataFieldIn(content){
+		const field = this.FIELD_CONTENT;
 		return `${ field }${ content }`;
+	}
+
+	static removeMetadataFieldIn(content){
+		return content.replace(this.regex, "");
 	}
 }
 
