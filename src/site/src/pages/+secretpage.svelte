d5a145c55703d29759ba67be91b33a6671268a3c
zoodogood <zoodogood@gmail.com>
2023-03-24 13:05:07 +0200

    Create page

---
+++ src/site/src/pages/+secretpage.svelte
@@ -0,0 +1,66 @@
+
+
+<main>
+	<p>Вы оказались на первой секретной странице</p>
+	<small>(радужно смотрит в будущее...)</small>
+</main>
+
+
+<nav class = "theme-switcher-container">
+	<a href = {PagesRouter.relativeToPage(PagesRouter.getPageBy("public").key)}><Icon code = ""/></a>
+	<ThemeSwitcher/>
+</nav>
+
+
+
+<style>
+	main 
+	{
+		--angle: 182deg;
+		--color: #ddffdd88;
+
+		text-transform: uppercase;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		min-height: 100vh;
+		min-height: 100svh;
+		padding-bottom: 20vh;
+		gap: 0.2em;
+
+		background-image: conic-gradient(
+			#00000000 var( --angle ), 
+			color-mix(in lch, var( --color ), var(--main-color)) 0
+		);
+		
+	}
+
+	
+
+	small
+	{
+		opacity: 0.3;
+		user-select: none;
+		font-size: 0.3em;	
+	}
+
+	nav
+	{
+		position: fixed;
+		right: 1vw;
+		top: 1vw;
+		opacity: 0.5;
+
+		display: flex;
+		gap: 1vw;
+	}
+</style>
+
+
+<script>
+	import ThemeSwitcher from '#site-component/ThemeSwitcher';
+	import Icon from '#site-component/iconic';
+	import PagesRouter from '#site/lib/Router.js';
+</script>
