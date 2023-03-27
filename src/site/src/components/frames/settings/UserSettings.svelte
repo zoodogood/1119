f701de7a76a10120891dc52bdc51d1eee884a235
zoodogood <zoodogood@gmail.com>
2023-03-27 19:29:07 +0300

    Init UserSettings frame

---
+++ src/site/src/components/frames/settings/UserSettings.svelte
@@ -0,0 +1,20 @@
+<Wrapper>
+	<aside>
+		День рождения:
+		<input type="text">
+	</aside>
+</Wrapper>
+
+
+<style>
+	
+</style>
+
+
+<script>
+	export let 
+		svelteApp = null,
+		target = {};
+
+	import Wrapper from "./wrapper.svelte";
+</script>
