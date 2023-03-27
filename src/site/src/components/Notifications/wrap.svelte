c5d5b63f289ea67bc2dd2282723c511b651ab7ad
zoodogood <zoodogood@gmail.com>
2023-03-27 19:19:10 +0300

    Create wrapper

---
+++ src/site/src/components/Notifications/wrap.svelte
@@ -0,0 +1,23 @@
+<Notifications>
+	<slot></slot>
+</Notifications>
+
+<style>
+	:global(.notification)
+	{
+		filter: brightness(0.7) contrast(0.9) opacity(0.9);
+		color: var( --white ) !important;
+		background-color: var( --main-color ) !important;
+		border-radius: 100vw !important;
+		padding-left: 1em !important;
+	}
+
+	:global(.notification-button){
+		color: var( --white ) !important;
+		border: none !important;
+	}
+</style>
+
+<script>
+	import Notifications from "svelte-notifications";
+</script>
