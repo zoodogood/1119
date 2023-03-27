c1afa633a79467166b464dfa0bd8b628e0613f2d
Alexsandr1x <zoodogood@gmail.com>
2023-03-27 10:30:39 +0300

    Working on a page

--- src/site/src/pages/user/+panel.svelte
+++ src/site/src/pages/user/+panel.svelte
@@ -2,26 +2,48 @@
 <Layout>
 	
 	
-	<ul style:--elements-size = "{ 3 - State.guilds?.length * 0.15 }em" class = "guilds-list">
+	<ul style:--elements-size = "{ 2.85 - State.guilds?.length * 0.15 }em" class = "guilds-list">
 		{#each State.guilds as guild}
-		{@const onClick = () => State.selectedGuildId !== guild.id ? svelteApp.Hash.include({key: "guildId", value: guild.id}).apply() : svelteApp.Hash.remove("guildId").apply() }
+		{@const onClick = () => State.target.id !== guild.id ? svelteApp.Hash.include({key: "guildId", value: guild.id}).apply() : svelteApp.Hash.remove("guildId").apply() }
 		<li
 			title = "Сервер { guild.name }"
-			class:selected = { State.selectedGuildId === guild.id }
+			class:selected = { State.target.id === guild.id }
 			data-id = { guild.id }
 			on:click = { onClick }
 			on:keydown = { onClick }
 			on:contextmenu|preventDefault = { navigator.clipboard.writeText(guild.id) & alert(`Скопирован ID сервера ${ guild.name }`) }
 		>
 			{#if guild.iconURL}
-				<img src = {guild.iconURL} alt = "guild-icon">
+				<img src = {guild.iconURL} alt = "guild-icon" class = "icon">
 			{:else}
-				<span>{ guild.name.at(0) }</span>
+				<span class = "icon">{ guild.name.at(0) }</span>
 			{/if}
 		</li>
-			
 		{/each}
+		<hr>
+		<li class = "invite" title = "Пригласить">
+			<a target = "_blank" href = { svelteApp.getBot().invite } rel = "noreferrer">
+				<span class = "icon">
+					{ "+" }
+				</span>
+			</a>
+		</li>
 	</ul>
+
+	<main class = "page-main">
+		<section>
+			<span class = "icon-container">
+				{#if State.target.icon}
+					<img src = {State.target.icon} alt = "guild-icon" class = "icon">
+				{:else}
+					<span class = "icon">{ State.target.name?.at(0) ?? "/" }</span>
+				{/if}
+			</span>
+			
+		<h1>{State.target.name}</h1>
+		</section>
+		
+	</main>
 	
 </Layout>
 
@@ -40,6 +62,7 @@
 		padding: 0.075em;
 
 		align-items: center;
+		
 	}
 
 	@media (max-width: 800px){
@@ -65,6 +88,17 @@
 		{
 			margin-top: var( --margin, 0 );
 		}
+
+		.guilds-list hr
+		{
+			width: 3px;
+			height: 15px;
+		}
+
+		.page-main
+		{
+			padding-top: 10vh;
+		}
 	}
 
 	@media (min-width: 800px){
@@ -89,6 +123,17 @@
 		{
 			margin-left: var( --margin, 0 );
 		}
+
+		.guilds-list hr
+		{
+			width: 15px;
+			height: 3px;
+		}
+
+		.page-main
+		{
+			padding-left: 5vw;
+		}
 	}
 
 
@@ -111,19 +156,24 @@
 		transition: filter 300ms;
 	}
 
-	.guilds-list > li img 
+	.guilds-list > li > *
 	{
-		width: 100%;
+		flex-grow: 1;
 	}
 
-	.guilds-list > li span 
+	.guilds-list > hr:first-child
+	{
+		display: none;
+	}
+
+
+	.icon 
 	{
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		background-color: var( --dark );
 		color: var( --white );
-		filter: contrast(0.7);
 		width: 100%;
 		height: 100%;
 
@@ -133,15 +183,58 @@
 		user-select: none;
 	}
 
+	span.icon 
+	{
+		filter: contrast(0.7);
+	}
+
+
 	.guilds-list > li:hover
 	{
 		border-radius: 20%;
 		background: #88888833;
+		filter: brightness(1);
 	}
 
-	.guilds-list li.selected
+	.guilds-list > li.selected
 	{
 		filter: brightness(1);
+	}
+
+	.guilds-list > li.invite
+	{
+		filter: brightness(1);
+	}
+
+	.guilds-list > li.invite span
+	{
+		font-weight: 100;
+		color: var( --white );
+		transition: transform 300ms, background-color 1s, color 1s;
+	}
+
+	.guilds-list > li.invite:hover span
+	{
+		font-weight: 100;
+		background-color: var( --main-color );
+		transform: rotateZ(360deg);
+	}
+
+
+
+
+
+
+
+
+
+	.icon-container
+	{
+		display: flex;
+		width: calc(3vw + 30px);
+		aspect-ratio: 1 / 1;
+		border-radius: 15px;
+		overflow: hidden;
 	}
 </style>
 
@@ -155,13 +248,29 @@
 
 	const hashStore = svelteApp.Hash.store;
 
+	const TargetType = {
+		User: "User",
+		Guild: "Guild"
+	};
 	const State = {
 		guilds: [],
-		selectedGuildId: null
+		selectedGuild: null,
+		target: {}
 	}
 	
-	$: 
-		State.selectedGuildId = $hashStore.guildId;
+	$: {
+		State.selectedGuild = State.guilds.find(guild => guild.id === $hashStore.guildId) ?? null;
+		const targetType = State.selectedGuild ? TargetType.Guild : TargetType.User;
+		const targetEntity = targetType === TargetType.User ? svelteApp.user : State.selectedGuild;
+
+		State.target = {
+			icon: targetEntity.avatarURL ?? targetEntity.iconURL,
+			name: targetEntity.username ?? targetEntity.name,
+			id: targetEntity.id,
+			type: targetType
+		};
+	}
+		
 	
 
 	onMount(async () => {
