cbedfe6da83dec547d3b63d650b045ae0e20dcb6
zoodogood <zoodogood@gmail.com>
2023-03-27 19:31:10 +0300

    Create frames/settings

---
+++ src/site/src/components/frames/settings/mod.svelte
@@ -0,0 +1,6 @@
+<script context = "module">
+	import GuildSettings from "./GuildSettings.svelte";
+	import UserSettings from "./UserSettings.svelte";
+	export {GuildSettings, UserSettings};
+
+</script>
