bf7824cc128692ca7723b65fc812eef37bd30e5c
zoodogood <zoodogood@gmail.com>
2023-03-27 19:18:36 +0300

    Add notifications

--- src/site/src/components/PageWrapper/mod.svelte
+++ src/site/src/components/PageWrapper/mod.svelte
@@ -1,6 +1,10 @@
-<svelte:component this = {page}/>
+<Notifications>
+	<svelte:component this = {page}/>
+</Notifications>
 
 <script>
 	export let page;
 	
+	import Notifications from '#site-component-lib/Notifications/wrap.svelte';
 </script>
+
