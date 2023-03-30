4d46df9b9cc0286b21687afb3b4fbc8ad7358b89
Alexsandr1x <zoodogood@gmail.com>
2023-03-30 17:37:00 +0300

    Working on a command

--- src/site/src/pages/commands/+index.svelte
+++ src/site/src/pages/commands/+index.svelte
@@ -4,10 +4,16 @@
 			{#each commands as command}
 			<li>
 				<h2>{ command.name }</h2>
-				<SvelteMarkdown source = { command.media.description.trim() }/>
+				<span class = "type">{ CategoryEnum[ command.type ]}</span>
+				<EditableMarkdown source = { command.media.description.trim() }/>
 				{#if command.media.poster}
 					<img src = { command.media.poster } alt="poster" fetchpriority = "low" loading = "lazy">
 				{/if}
+				<h6>
+					{#each command.allias.split(" ") as allias}
+						<span>!{ allias }</span>
+					{/each}
+				</h6>
 			</li>
 			{/each}
 		</ul>
@@ -29,7 +35,7 @@
 
 	li
 	{
-		width: min(100%, 700px);
+		width: min(100%, 1000px);
 
 		display: flex;
 		gap: 1em;
@@ -52,13 +58,42 @@
 	{
 		opacity: 1;
 	}
+
+	.type 
+	{
+		background-color: #88888811;
+		display: inline-block;
+		width: fit-content;
+		padding: 0.2em;
+	}
+
+	h6
+	{
+		margin-top: auto;
+		opacity: 0.65;
+		display: flex;
+		gap: 0.5em;
+		font-weight: 0.5em;
+		flex-wrap: wrap;
+	}
 </style>
 
 <script>
   	import { fetchFromInnerApi } from '#lib/safe-utils.js';
 	import Layout from '#site-component/Layout';
-  	import SvelteMarkdown from 'svelte-markdown';
+	import EditableMarkdown from '#site-component/EditableMarkdown';
+  	import svelteApp from '#site/core/svelte-app.js';
+
+	const i18n = svelteApp.i18n.pages.commandsIndex;
 	
+	const CategoryEnum = {
+		delete: i18n.category.delete,
+		dev: i18n.category.dev,
+		user: i18n.category.user,
+		guild: i18n.category.guild,
+		bot: i18n.category.bot,
+		other: i18n.category.other
+	}
 	
 	let commands = [];
 	(async () => {
