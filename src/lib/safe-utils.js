a156eae480d6936783e44de8cb43b187c00a8f22
Alexsandr1x <zoodogood@gmail.com>
2023-03-30 11:38:30 +0300

    Fix

--- src/lib/safe-utils.js
+++ src/lib/safe-utils.js
@@ -267,7 +267,7 @@ async function getPackageJSON(){
 }
 
 function ReplaceTemplate(string, values){
-	string.replaceAll(/(?<!\\)\$\{[a-zа-яъё]+\}/i, (match) => values[match.trim()]);
+	string.replaceAll(/(?<!\\)\$\{[a-zа-яъё]+\}/ig, (match) => values[match.trim()]);
 }
 
 
