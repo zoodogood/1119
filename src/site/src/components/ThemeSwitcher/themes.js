3cc88c8d79bbcb3635236da9bc9d9a730c5388d9
Alexsandr1x <zoodogood@gmail.com>
2023-03-30 16:15:36 +0300

    Add orange theme

--- src/site/src/components/ThemeSwitcher/themes.js
+++ src/site/src/components/ThemeSwitcher/themes.js
@@ -62,6 +62,27 @@ export default new Map(Object.entries({
 		"--theme-accent-name": "light"
 	},
 
+	brightGreen: {
+		"--text-theme-accent": "var(--white)",
+		"--background-theme-accent": "var(--dark)",
+		"--main-color": "#ff9f64",
+		"--theme-accent-name": "dark"
+	},
+
+	darkGreen: {
+		"--main-color": "#e1864e",
+		"--text-theme-accent": "var(--white)",
+		"--background-theme-accent": "var(--dark)",
+		"--theme-accent-name": "dark"
+	},
+
+	lightGreen: {
+		"--main-color": "#e1864e",
+		"--text-theme-accent": "var(--dark)",
+		"--background-theme-accent": "var(--white)",
+		"--theme-accent-name": "light"
+	},
+
 	
 
 	contrast: {
