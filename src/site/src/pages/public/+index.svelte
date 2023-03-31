3c5b28ca74f1d3e9b34d5ac00923733244a2a155
zoodogood <zoodogood@gmail.com>
2023-03-31 21:22:25 +0300

    Improve

--- src/site/src/pages/public/+index.svelte
+++ src/site/src/pages/public/+index.svelte
@@ -1,3 +1,13 @@
+{#if $CurrentThemeStore === Theme.enum.brightOrange}
+	<style>
+		body {
+			background: url('https://cdn.discordapp.com/attachments/629546680840093696/1091416746684063824/autumn.png') var( --background-theme-accent );
+			background-size: min(100vmax, 200vw);
+			background-repeat: no-repeat;
+		}
+	</style>
+{/if}
+
 
 <Layout>
 	<h1>{ i18n.hi }</h1>
@@ -10,6 +20,9 @@
 	import ChangeLanguage from '#site-component/ChangeLanguage';
 	import svelteApp from '#site/core/svelte-app.js';
 	const i18n = svelteApp.i18n.pages.public;
+
+	import { Theme } from '#site-component/ThemeSwitcher';
+	const CurrentThemeStore = Theme.current;
 </script>
 
 
