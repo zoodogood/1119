8aa0f88b5827dbee9424f26f3647cd78f406c2ca
zoodogood <zoodogood@gmail.com>
2023-03-31 08:11:25 +0300

    Add feature: Can change locale via interface

--- src/site/src/pages/public/+index.svelte
+++ src/site/src/pages/public/+index.svelte
@@ -2,10 +2,12 @@
 <Layout>
 	<h1>{ i18n.hi }</h1>
 	<p>{ i18n.cool }</p>
+	<ChangeLanguage/>
 </Layout>
 
 <script>
 	import Layout from '#site-component/Layout';
+	import ChangeLanguage from '#site-component/ChangeLanguage';
 	import svelteApp from '#site/core/svelte-app.js';
 	const i18n = svelteApp.i18n.pages.public;
 </script>
