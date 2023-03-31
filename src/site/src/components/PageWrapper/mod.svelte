75cf5e0ca2cb4a5a543e32122861f09ef9a2ba59
zoodogood <zoodogood@gmail.com>
2023-03-31 09:32:42 +0300

    Add metatags

--- src/site/src/components/PageWrapper/mod.svelte
+++ src/site/src/components/PageWrapper/mod.svelte
@@ -1,10 +1,46 @@
 <Notifications>
-	<svelte:component this = {page}/>
+	<svelte:component this = { page }/>
 </Notifications>
+
+
+
+<MetaTags
+	title = { i18n.title }
+	{languageAlternates}
+	canonical = { relativeSiteRoot(svelteApp) }
+	additionalMetaTags = {[
+		{
+			name: "theme-color",
+			content: Theme.collection.get(Theme.current)["--main-color"]
+		}
+	]}
+/>
 
 <script>
 	export let page;
+
 	
 	import Notifications from '#site-component-lib/Notifications/wrap.svelte';
+	import { MetaTags } from 'svelte-meta-tags';
+	
+	import svelteApp from '#site/core/svelte-app.js';
+  	import config from '#config';
+  	import { relativeSiteRoot } from '#lib/safe-utils.js';
+	import { Theme } from '#site-component/ThemeSwitcher';
+	
+
+	const _currentURLLang = svelteApp.url.base.lang;
+	const languageAlternates = config.i18n.availableLanguages
+		.map(locale => {
+			svelteApp.url.base.lang = locale;
+			const href = relativeSiteRoot(svelteApp, svelteApp.url.subpath.join("/"));
+			return {hrefLang: locale, href};
+		});
+
+	svelteApp.url.base.lang = _currentURLLang;
+
+
+	const i18n = svelteApp.i18n.general;
+	document.documentElement.setAttribute("lang", svelteApp.lang);
 </script>
 
