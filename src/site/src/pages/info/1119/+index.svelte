e95ef29287c637b7709408d40831824a497fdca9
Alexsandr1x <zoodogood@gmail.com>
2023-04-05 16:14:09 +0300

    Working on a page

--- src/site/src/pages/info/1119/+index.svelte
+++ src/site/src/pages/info/1119/+index.svelte
@@ -1,9 +1,29 @@
 <Layout>
 	<header>
 		<span>
-			<iframe src="https://greenghost.mooo.com:8119/pages/secretpage" title = "x" frameborder="0"></iframe>
+			<img src = "https://images-ext-2.discordapp.net/external/fBq1I0O3Tdhoi-DeVVm7nDadXN-uzdgKveyekp-Vm88/https/media.discordapp.net/attachments/769566192846635010/872776969341796382/mute.gif" alt = "muteCommand">
 		</span>
 	</header>
+
+	<h1>Бот без явного функционала</h1>
+	<p>Я действительно не могу описать его возможностей, ведь нет определённой задачи или темы к которой он был бы привязан.</p>
+	<p>Нет и особо уникальных черт, ведь всё, что имеет этот бот уже когда-то существовало: проклятия, испытания, недо-экономика, общая казна, шуточные вещи, утилиты и конечно же модеративные команды.</p>
+	<p>
+		<a href = { PagesRouter.relativeToPage( PagesRouter.getPageBy("commands").key ) }>Список команд.</a>
+	</p>
+
+	<h3>Почему "это" всё-ещё существует.</h3>
+	<p>Этого бота зовут Призрак. — Захотелось так. И я уже смирился с тем, что он важен для меня и людей, которые дают понять, что он им не безразличен.</p>
+	<p>Поэтому он будет продолжать жить и иногда обновляться пока мир не помешает этому. Так что не дождётесь, хейтеры.</p>
+	
+	<h3>Эй! Давай больше обновлений!</h3>
+	<p>Кусь вам, ведь я хочу заниматься чем-то новым!</p>
+
+	<h2>Статистика</h2>
+	<section>
+		<p>Я осмелюсь прорекламировать здесь другие свои хобби проекты</p>
+	</section>
+
 	
 </Layout>
 
@@ -11,35 +31,49 @@
 	header 
 	{
 		display: flex;
-		justify-content: center;
+		justify-content: start;
 	}
-	iframe
+
+	header span
 	{
+		--padding: 10px;
 		border-radius: 30px;
-		width: 50vw;
-		height: 50vh;
+		width: 20vw;
+		height: 20vh;
 
-		padding-bottom: 10px;
-		border: 10px solid var( --text-theme-accent );
+		padding-bottom: var( --padding );
+		border: var( --padding ) solid #88888811;
 		background: var( --text-theme-accent );
-	}
 
-	span 
-	{
+		overflow: hidden;
+
 		position: relative;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+
+		/* clip-path: polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%); */
+		/* clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 75% 75%, 75% 100%, 50% 75%, 0% 75%); */
 	}
 
-	span::after
+	header span img 
+	{
+		object-fit: cover;
+		object-position: left;
+		height: 100%;
+		width: 100%;
+	}
+
+
+
+	header span::after
 	{
 		content: '';
 		position: absolute;
-		bottom: 10px;
+		bottom: 0;
 		border: 2px solid var( --background-theme-accent );
 		border-radius: 50%;
-		width: 5px;
+		width: calc( var(--padding) / 2 );
 		aspect-ratio: 1 / 1;
 		transform: translateY(50%);
 	}
@@ -47,4 +81,5 @@
 
 <script>
 	import Layout from '#site-component/Layout';
+  import PagesRouter from '#site/lib/Router.js';
 </script>
