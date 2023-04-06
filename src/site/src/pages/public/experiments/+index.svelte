b72ccabd989ade90f9e833cf16967682a51f9953
zoodogood <zoodogood@gmail.com>
2023-04-06 19:24:51 +0300

    Improve

--- src/site/src/pages/public/experiments/+index.svelte
+++ src/site/src/pages/public/experiments/+index.svelte
@@ -1,9 +1,9 @@
 <Layout>
 	<h1>Эксперименты</h1>
 	<p>Я не знаю кто на такое решится, но вы можете добавлять на эту страницу разные штуки, предлагая изменения на странице Гитхаба</p>
-	<p>Практическая польза заключается в том, что я могу быстро вспомнить или проверить некоторую возможность. Для пользователей это место может оказаться бесполезным</p>
+	<p>Практическая польза буквально заключается в том, что я могу быстро вспомнить или проверить некоторую возможность. Для пользователей это место может оказаться бесполезным</p>
 	<element-embed>
-		<p>Вы действительно можете вносить изменения в каталоге по адресу <a target = "_blank" rel = "noreferrer" href = "{ config.enviroment.github }/tree/{ config.enviroment.branch }/src/site/src/pages/public/experiments">experiments</a>:</p>
+		<p>Вы действительно можете вносить изменения в каталоге, <a target = "_blank" rel = "noreferrer" href = "{ config.enviroment.github }/tree/{ config.enviroment.branch }/src/site/src/pages/public/experiments">experiments</a>:</p>
 		<p>Создавать новые файлы и редактировать существующие. Изменения будут приняты и позднее отображены на этой странице.</p>
 		<p>Ограничения существуют по принципу морали и безопасности. Другие пользователи могут удалять ваши изменения по тем же принципам.</p>
 		<p>Подробнее см. в файле <code>readme.md</code> того же каталога.</p>
@@ -12,6 +12,10 @@
 	<section>
 		<button on:click = { createNotification }>Создать уведомление</button>
 		<button on:click = { createDialog }>Создать диалог</button>
+	</section>
+
+	<section class = "comments-container">
+		<Giscus tag = "special: experiments"/>
 	</section>
 
 </Layout>
@@ -27,11 +31,12 @@
 <script>
 	import Layout from '#site-component/Layout';
 	import Dialog from '#site-component/Dialog';
+	import Giscus from '#site-component/Giscus';
 
 	import { getNotificationsContext } from 'svelte-notifications';
 	const { addNotification } = getNotificationsContext();
 	import svelteApp from '#site/core/svelte-app.js';
-  import config from '#config';
+  	import config from '#config';
 	
 
 	const i18n = svelteApp.i18n.pages.publicExperiments;
