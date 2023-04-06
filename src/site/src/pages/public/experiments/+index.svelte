57ceba98a7d9b14cd539fbcd2dd4b459e1461131
zoodogood <zoodogood@gmail.com>
2023-04-06 18:01:39 +0300

    Move file

--- src/site/src/pages/public/experiments/+index.svelte
+++ src/site/src/pages/public/experiments/+index.svelte
@@ -1,10 +1,13 @@
 <Layout>
 	<h1>Эксперименты</h1>
-	<element-embed>
-		{ i18n.thisPageNotingTranslate }
-	</element-embed>
 	<p>Я не знаю кто на такое решится, но вы можете добавлять на эту страницу разные штуки, предлагая изменения на странице Гитхаба</p>
 	<p>Практическая польза заключается в том, что я могу быстро вспомнить или проверить некоторую возможность. Для пользователей это место может оказаться бесполезным</p>
+	<element-embed>
+		<p>Вы действительно можете вносить изменения в каталоге по адресу <a target = "_blank" rel = "noreferrer" href = "{ config.enviroment.github }/tree/{ config.enviroment.branch }/src/site/src/pages/public/experiments">experiments</a>:</p>
+		<p>Создавать новые файлы и редактировать существующие. Изменения будут приняты и позднее отображены на этой странице.</p>
+		<p>Ограничения существуют по принципу морали и безопасности. Другие пользователи могут удалять ваши изменения по тем же принципам.</p>
+		<p>Подробнее см. в файле <code>readme.md</code> того же каталога.</p>
+	</element-embed>
 	
 	<section>
 		<button on:click = { createNotification }>Создать уведомление</button>
@@ -28,6 +31,7 @@
 	import { getNotificationsContext } from 'svelte-notifications';
 	const { addNotification } = getNotificationsContext();
 	import svelteApp from '#site/core/svelte-app.js';
+  import config from '#config';
 	
 
 	const i18n = svelteApp.i18n.pages.publicExperiments;
