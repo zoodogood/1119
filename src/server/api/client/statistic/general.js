58aef58295bd937f72885f716574452d67af3d47
Alexsandr1x <zoodogood@gmail.com>
2023-04-09 14:17:02 +0300

    Create api point

---
+++ src/server/api/client/statistic/general.js
@@ -0,0 +1,27 @@
+import { BaseRoute } from "#server/router.js";
+import client from "#bot/client.js";
+import CommandsManager from "#lib/modules/CommandsManager.js";
+
+const PREFIX = "/client/statistic/general";
+
+
+
+class Route extends BaseRoute {
+	prefix = PREFIX;
+
+	constructor(express){
+		super();
+	}
+
+	async get(request, response){
+		const data = {
+			guilds: client.guilds.cache.size,
+			users: client.users.cache.size,
+			channels: client.channels.cache.size,
+			commands: CommandsManager.collection.size
+		}
+		response.json(data);
+	}
+}
+
+export default Route;
