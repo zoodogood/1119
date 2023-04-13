983ff571c5455e9425be4680ac8376567579d21c
Alexsandr1x <zoodogood@gmail.com>
2023-04-13 20:15:49 +0300

    Rename files

--- src/site/src/components/frames/settings/mod.svelte
+++
@@ -1,6 +0,0 @@
-<script context = "module">
-	import GuildSettings from "./GuildSettings.svelte";
-	import UserSettings from "./UserSettings.svelte";
-	export {GuildSettings, UserSettings};
-
-</script>
