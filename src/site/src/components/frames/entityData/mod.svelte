29af55e2dce290f128783f77dd3fb385817e3c94
Alexsandr1x <zoodogood@gmail.com>
2023-04-13 20:17:18 +0300

    Move file

---
+++ src/site/src/components/frames/entityData/mod.svelte
@@ -0,0 +1,9 @@
+<script context = "module">
+  	import GuildProgress from "./GuildProgress.svelte";
+  	import UserProgress from "./UserProgress.svelte";
+	import GuildSettings from "./GuildSettings.svelte";
+	import UserSettings from "./UserSettings.svelte";
+
+	export {GuildSettings, UserSettings, GuildProgress, UserProgress};
+
+</script>
