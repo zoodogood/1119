5663b8e29cbf99f089290b56c4668e3628fc0631
Alexsandr1x <zoodogood@gmail.com>
2023-04-13 20:16:42 +0300

    Working on a component

---
+++ src/site/src/components/frames/entityData/UserProgress.svelte
@@ -0,0 +1,51 @@
+<Wrapper>
+	<element-group style:width = "30%" >
+		<Progressbar value = { State.user.experience } max = { (State.user.level || 1) * LEVELINCREASE_EXPERIENCE_PER_LEVEL } targetLabel = "{ State.user.level } ур."/>
+	</element-group>
+</Wrapper>
+
+
+<style>
+	
+</style>
+
+
+<script>
+	export let 
+		svelteApp = null,
+		target = {};
+	
+
+	import Wrapper from "./wrapper.svelte";
+	import Progressbar from '#site-component/Progressbar';
+
+	import { LEVELINCREASE_EXPERIENCE_PER_LEVEL } from "#constants/users/events.js";
+  	import { fetchFromInnerApi } from "#lib/safe-utils.js";
+  	import { onMount } from "svelte";
+	
+
+	const Component = {
+		async getData(){
+			const headers = {Authorization: svelteApp.storage.getToken()};
+			const data = await fetchFromInnerApi("user/data", {headers});
+			
+			return data;
+		}
+	}
+
+	const State = {
+		user: {
+			level: 0,
+			experience: 0
+		}
+	}
+	onMount(async () => {
+		const userData = await Component.getData();
+		if (!userData.id){
+			return;
+		}
+		
+		State.user.level = userData.level;
+		State.user.experience = userData.exp;
+	}) 
+</script>
