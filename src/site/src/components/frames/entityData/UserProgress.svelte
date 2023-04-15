d92bab327d047e7a3fbd964d13f658d8f73ab19e
Alexsandr1x <zoodogood@gmail.com>
2023-04-15 10:02:12 +0300

    Working on a component

--- src/site/src/components/frames/entityData/UserProgress.svelte
+++ src/site/src/components/frames/entityData/UserProgress.svelte
@@ -1,6 +1,14 @@
 <Wrapper>
-	<element-group style:width = "30%" >
+	<element-group style:width = "30%" style:min-width = "200px">
 		<Progressbar value = { State.user.experience } max = { (State.user.level || 1) * LEVELINCREASE_EXPERIENCE_PER_LEVEL } targetLabel = "{ State.user.level } ур."/>
+	</element-group>
+
+	<element-group>
+		<element-container title = "Богатств">
+			<span style:color = "#88888888"><Icon code = ""/></span>
+			{ NumberFormatLetterize(State.user.coins) }
+			
+		</element-container>
 	</element-group>
 </Wrapper>
 
@@ -18,9 +26,10 @@
 
 	import Wrapper from "./wrapper.svelte";
 	import Progressbar from '#site-component/Progressbar';
+	import Icon from '#site-component/iconic';
 
 	import { LEVELINCREASE_EXPERIENCE_PER_LEVEL } from "#constants/users/events.js";
-  	import { fetchFromInnerApi } from "#lib/safe-utils.js";
+  	import { NumberFormatLetterize, fetchFromInnerApi } from "#lib/safe-utils.js";
   	import { onMount } from "svelte";
 	
 
@@ -36,7 +45,8 @@
 	const State = {
 		user: {
 			level: 0,
-			experience: 0
+			experience: 0,
+			coins: null
 		}
 	}
 	onMount(async () => {
@@ -47,5 +57,6 @@
 		
 		State.user.level = userData.level;
 		State.user.experience = userData.exp;
+		State.user.coins = userData.coins;
 	}) 
 </script>
