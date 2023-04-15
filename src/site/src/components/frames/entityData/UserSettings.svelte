c176d7690c6bd9b6ea1bb08a6542c2bff94410b4
Alexsandr1x <zoodogood@gmail.com>
2023-04-15 10:02:26 +0300

    Working on a component

--- src/site/src/components/frames/entityData/UserSettings.svelte
+++ src/site/src/components/frames/entityData/UserSettings.svelte
@@ -1,13 +1,34 @@
 <Wrapper>
+	
 	<aside>
-		День рождения:
-		<input type="text">
+		<details>
+			<summary><h2>Общие параметры</h2></summary>
+			<element-group>
+				<small>День рождения:</small>
+				<input type="text" placeholder = "31.12">
+			</element-group>
+		</details>
+		
 	</aside>
 </Wrapper>
 
 
 <style>
+	aside
+	{
+		display: flex;
+		gap: 0.5em;
+	}
+
+	h2
+	{
+		font-size: 1.2em;
+	}
 	
+	small
+	{
+		font-size: 0.5em;
+	}
 </style>
 
 
