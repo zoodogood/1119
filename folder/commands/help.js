918f0bc1238909c3a80cbae95b0968c7f323522a
zoodogood <zoodogood@gmail.com>
2023-04-27 19:39:41 +0300

    Improve

--- folder/commands/help.js
+++ folder/commands/help.js
@@ -1,69 +1,74 @@
-import { ButtonStyle, ComponentType } from 'discord.js';
-import CommandsManager from '#lib/modules/CommandsManager.js';
+import { ButtonStyle, ComponentType } from "discord.js";
+import CommandsManager from "#lib/modules/CommandsManager.js";
 
 class Guidances {
-  get selectOptions(){
-    return this.guidances.map(({id, label}) => ({label, value: id}));
+  get selectOptions() {
+    return this.guidances.map(({ id, label }) => ({ label, value: id }));
   }
 
   guidances = [
     {
       id: "basics",
-      label: "Основы"
-    }
-  ]
-};
-
+      label: "Основы",
+    },
+  ];
+}
 
 class Command {
-
-  run(interaction){
+  run(interaction) {
     const guildCommands = [];
     const commands = CommandsManager.collection;
-    const isHidden = ({options}) => options.hidden || options.type === "dev" || options.removed;
-    const design   = (command) => `\`!${ command.options.name }\``;
+    const isHidden = ({ options }) =>
+      options.hidden || options.type === "dev" || options.removed;
+    const design = (command) => `\`!${command.options.name}\``;
 
-    if (interaction.guild.data.commands){
+    if (interaction.guild.data.commands) {
       const list = interaction.guild.data.commands;
       guildCommands.push({
         name: "Кастомные команды <:cupS:806813704913682442>",
-        value: list.map(design).join(" ")
+        value: list.map(design).join(" "),
       });
     }
-
 
     const fields = [
       {
         name: "Управление сервером <a:diamond:725600667586134138>",
         value: commands
-          .filter(command => command.options.type === "guild" && !isHidden(command))
+          .filter(
+            (command) => command.options.type === "guild" && !isHidden(command)
+          )
           .map(design)
-          .join(" ")
+          .join(" "),
       },
       {
         name: "Пользователи <:berry:756114492055617558>",
         value: commands
-          .filter(command => command.options.type === "user" && !isHidden(command))
+          .filter(
+            (command) => command.options.type === "user" && !isHidden(command)
+          )
           .map(design)
-          .join(" ")
+          .join(" "),
       },
       {
         name: "Бот <:piggeorg:758711403027759106>",
         value: commands
-          .filter(command => command.options.type === "bot" && !isHidden(command))
+          .filter(
+            (command) => command.options.type === "bot" && !isHidden(command)
+          )
           .map(design)
-          .join(" ")
+          .join(" "),
       },
       ...guildCommands,
       {
         name: "Другое <:coin:637533074879414272>",
         value: commands
-          .filter(command => command.options.type === "other" && !isHidden(command))
+          .filter(
+            (command) => command.options.type === "other" && !isHidden(command)
+          )
           .map(design)
-          .join(" ")
-      }
+          .join(" "),
+      },
     ];
-
 
     const embed = {
       title: "Команды, которые не сломают ваш сервер",
@@ -75,49 +80,60 @@ class Command {
           label: "Discord",
           style: ButtonStyle.Link,
           url: "https://discord.gg/76hCg2h7r8",
-          emoji: {id: "849587567564554281"}
+          emoji: { id: "849587567564554281" },
         },
         {
           type: ComponentType.Button,
           label: "Тишком-нишком",
           customId: "@command/help/guidances-preview",
           style: ButtonStyle.Primary,
-          emoji: "❄️"
+          emoji: "❄️",
           // options: this.createGuidances().selectOptions
-        }
-      ]
-    }
+        },
+      ],
+    };
 
     interaction.channel.msg(embed);
   }
 
-	async onChatInput(msg, interaction){
+  async onChatInput(msg, interaction) {
     this.run(interaction);
   }
 
-  async onSlashCommand(interaction){
+  async onSlashCommand(interaction) {
     this.run(interaction);
   }
 
-  createGuidances(){
+  createGuidances() {
     return new Guidances();
   }
 
+  onComponent({ params, interaction }) {
+    const content =
+      "Новая года. Читатель, люби каждый день как новый год. Люби новый год за то, что ты можешь выйти на улицу и увидеть вокруг себя людей. Или опустевшие улицы";
 
-	options = {
-	  "name": "help",
-	  "id": 4,
-    "slash": {
-      "name": "help"
+    params === "guidances-preview" &&
+      interaction.msg({
+        content,
+        ephemeral: true,
+      });
+  }
+
+  options = {
+    name: "help",
+    id: 4,
+    slash: {
+      name: "help",
     },
-	  "media": {
-	    "description": "\n\nСтандартная команда отображающую основную информацию о возможностях бота. Она нужна чтобы помочь новым пользователям. Её так же можно вызвать отправив `/help`\n\n✏️\n```python\n!help #без аргументов\n```\n\n"
-	  },
-	  "allias": "хелп помощь cmds commands команды х",
-		"allowDM": true,
-		"cooldown": 15000000,
-		"type": "other"
-	};
-};
+    media: {
+      description:
+        "\n\nСтандартная команда отображающую основную информацию о возможностях бота. Она нужна чтобы помочь новым пользователям. Её так же можно вызвать отправив `/help`\n\n✏️\n```python\n!help #без аргументов\n```\n\n",
+    },
+    allias: "хелп помощь cmds commands команды х",
+    allowDM: true,
+    cooldown: 15000000,
+    type: "other",
+  };
+}
 
 export default Command;
