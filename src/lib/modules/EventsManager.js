3dfbeac5425adce7b7a7684618169d442efcf44f
zoodogood <zoodogood@gmail.com>
2023-09-18 19:20:15 +0300

    Fix

--- src/lib/modules/EventsManager.js
+++ src/lib/modules/EventsManager.js
@@ -47,7 +47,7 @@ class BaseEvent {
 	#logger({event, args}){
 		console.info(`Event: ${ this.eventName }`);
 		// to-do: clean when bun supports the console.dir
-		(Bun === undefined) && console.dir(args, {showHidden: true, depth: 0});
+		(window.Bun === undefined) && console.dir(args, {showHidden: true, depth: 0});
 	}
  
 	async #beforeRun(...args){
