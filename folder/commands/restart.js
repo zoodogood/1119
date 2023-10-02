0eb654b9425620723134e6a10d4b434536ea3e3e
zoodogood <zoodogood@gmail.com>
2023-10-02 15:35:38 +0300

    Clean a code

--- folder/commands/restart.js
+++ folder/commands/restart.js
@@ -1,6 +1,6 @@
 
-import config from '#config';
-import get from '#lib/child-process-utils.js';
+import config from "#config";
+import get from "#lib/child-process-utils.js";
 const {run, info, _npm} = get({ root: process.cwd() });
 
 
@@ -8,55 +8,55 @@ const toANSIBlock = (content) => `\`\`\`ansi\n${ content }\`\`\``;
 
 class Command {
 
-	async onChatInput(msg, interaction){
-		const COMMANDS = [
-			"git pull",
-			`${ _npm } run build`,
-			config.pm2.id ? `${ _npm } run pm2-please-restart ${ config.pm2.id }` : "echo pm2 not setted"
-		];
+  async onChatInput(msg, interaction){
+    const COMMANDS = [
+      "git pull",
+      `${ _npm } run build`,
+      config.pm2.id ? `${ _npm } run pm2-please-restart ${ config.pm2.id }` : "echo pm2 not setted"
+    ];
 		
-		const embed = {
-			title: "<:emoji_50:753916145177722941>",
-			color: "#2c2f33",
-			description: `**RESTARTING...**`
-		};
+    const embed = {
+      title: "<:emoji_50:753916145177722941>",
+      color: "#2c2f33",
+      description: "**RESTARTING...**"
+    };
 
-		const message = await msg.msg(embed);
-		embed.edit = true;
+    const message = await msg.msg(embed);
+    embed.edit = true;
 
-		const updateDescription = (content) => {
-			embed.description += `\n${ content }`;
-			message.msg(embed);
-		}
+    const updateDescription = (content) => {
+      embed.description += `\n${ content }`;
+      message.msg(embed);
+    };
 		
-		for (const string of COMMANDS) {
-			const [command, ...params] = string.split(" ");
-			updateDescription(
-				`\n> ${ string }`
-			);
+    for (const string of COMMANDS) {
+      const [command, ...params] = string.split(" ");
+      updateDescription(
+        `\n> ${ string }`
+      );
 
-			const result = await run(command, params)
-				.catch((error) => `Error: ${ error.message }`);
+      const result = await run(command, params)
+        .catch((error) => `Error: ${ error.message }`);
 
-			updateDescription(
-				toANSIBlock(result)
-			);
-		}
+      updateDescription(
+        toANSIBlock(result)
+      );
+    }
   	}
 
 
-	options = {
+  options = {
 	  	"name": "restart",
 	  	"id": 61,
 	  	"media": {
 	   	"description": "Перезапускает процесс"
 	  	},
 	  	"allias": "перезапустить",
-		"allowDM": true,
-		"cooldown": 100_000,
-		"cooldownTry": 5,
-		"type": "dev"
-	};
-};
+    "allowDM": true,
+    "cooldown": 100_000,
+    "cooldownTry": 5,
+    "type": "dev"
+  };
+}
 
 export default Command;
