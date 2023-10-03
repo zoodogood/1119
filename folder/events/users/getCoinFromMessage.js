f2a67224e4b42e5134c7d36abb31f3661ec5a135
zoodogood <zoodogood@gmail.com>
2023-10-03 20:21:53 +0300

    Fix

--- folder/events/users/getCoinFromMessage.js
+++ folder/events/users/getCoinFromMessage.js
@@ -1,5 +1,7 @@
+import { Actions } from "#lib/modules/ActionManager";
+import DataManager from "#lib/modules/DataManager";
 import { BaseEvent, EventsManager } from "#lib/modules/EventsManager.js";
-
+import * as Util from "#lib/util.js";
 
 
 
@@ -12,36 +14,36 @@ class Event extends BaseEvent {
 
   async onGetCoinsFromMessage({userData, message}){
     message.author.action(Actions.coinFromMessage, {channel: message.channel});
-	 
+
     let reaction = "637533074879414272";
     let k = 1;
-	 
+
     if (DataManager.data.bot.dayDate === "31.12"){
-		  reaction = "❄️";
-		  k += 0.2;
+      reaction = "❄️";
+      k += 0.2;
     }
-	 
+
     if (message.guild && "cloverEffect" in message.guild.data) {
-		  reaction = "☘️";
-		  let multiplier = 0.08 + (0.07 * ((1 - 0.9242 ** message.guild.data.cloverEffect.uses) / (1 - 0.9242)));
-		  multiplier *= 2 ** (userData.voidMysticClover ?? 0);
-		  k += multiplier;
-		  message.guild.data.cloverEffect.coins++;
+      reaction = "☘️";
+      let multiplier = 0.08 + (0.07 * ((1 - 0.9242 ** message.guild.data.cloverEffect.uses) / (1 - 0.9242)));
+      multiplier *= 2 ** (userData.voidMysticClover ?? 0);
+      k += multiplier;
+      message.guild.data.cloverEffect.coins++;
     }
-	 
+
     const coins = Math.round((35 + (userData.coinsPerMessage ?? 0)) * k);
     userData.coins += coins;
     userData.chestBonus = (userData.chestBonus ?? 0) + 5;
-	 
+
     const react = await message.awaitReact({user: message.author, removeType: "full", time: 20000}, reaction);
-	 
+
     if (!react) {
-		  return;
+      return;
     }
-	 
+
     const messageContent = `> У вас ${ Util.ending(userData.coins, "коин", "ов", "", "а")} <:coin:637533074879414272>!\n> Получено ${coins}\n> Бонус сундука: ${userData.chestBonus || 0}`;
     message.msg({content: messageContent, delete: 2500});
-	 }
+  }
 
   async run({userData, message}){
     this.onGetCoinsFromMessage({userData, message});
