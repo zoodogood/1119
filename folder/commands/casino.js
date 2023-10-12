698002f9460cf4a5811d5ee86f21298bcfc03c54
zoodogood <zoodogood@gmail.com>
2023-10-12 15:09:46 +0300

    Now returned

--- folder/commands/casino.js
+++ folder/commands/casino.js
@@ -1,9 +1,9 @@
-import * as Util from '#lib/util.js';
+import * as Util from "#lib/util.js";
 
 class Command {
 
-	async onChatInput(msg, interaction){
-    if (interaction.userData.){
+  async onChatInput(msg, interaction){
+    if (interaction.userData.voidCasino){
       msg.msg({title: "Казино закрыто", description: "Казино закрыто. Боюсь что оно больше не откроется.\nЭтого не могло не случится, извините.\n\n — Прощайте. ©️Мэр-Миллиардер Букашка", delete: 20000});
       return;
     }
@@ -26,37 +26,37 @@ class Command {
       return;
     }
 
-    const diceRoll = Util.random(100);
+    const diceRoll = Util.random(8);
     const options = {
       title: "Лесовитое казино",
       author: { name: msg.author.username, iconURL: msg.author.avatarURL() },
       delete: 20000,
       footer: {text: `Ставка: ${ bet }`}
-    }
+    };
     const isWon = diceRoll % 2;
     options.description = `
 **${ isWon ? "Вы выиграли." : "Проиграли" }**
 **Кидаем кубик.. выпадает:** \`${ diceRoll }\`; ${ isWon ? "🦝" : "❌" }
 
 ${ isWon ? `\\*Вам достается куш — ${ Util.ending(bet * 2, "коин", "ов", "", "а") } <:coin:637533074879414272>\\*` : "Чтобы выиграть дожно выпасть число, которое не делится на 2" }
-    `;
+`;
 
     interaction.userData.coins -= (-1) ** isWon * bet;
     msg.msg(options);
   }
 
 
-	options = {
-	  "name": "casino",
-	  "id": 57,
-	  "media": {
-	    "description": "\n\nМеня долго просили сделать Казино. И вот оно здесь!\nТакое же пустое как и ваши кошельки\n\n✏️\n```python\n!casino {coinsBet | \"+\"}\n```\n\n"
-	  },
-	  "allias": "казино bet ставка",
+  options = {
+    "name": "casino",
+    "id": 57,
+    "media": {
+      "description": "\n\nМеня долго просили сделать Казино. И вот оно здесь!\nТакое же пустое как и ваши кошельки\n\n✏️\n```python\n!casino {coinsBet | \"+\"}\n```\n\n"
+    },
+    "allias": "казино bet ставка",
     "cooldown": 300_000,
-		"allowDM": true,
-		"type": "other"
-	};
-};
+    "allowDM": true,
+    "type": "other"
+  };
+}
 
 export default Command;
