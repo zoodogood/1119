5103ad22c0a6328600bcc052dc70114c0182aa4d
zoodogood <zoodogood@gmail.com>
2023-10-12 09:30:28 +0300

    Add I18nMaanager

--- src/lib/modules/mod.js
+++ src/lib/modules/mod.js
@@ -2,22 +2,23 @@ import DataManager from "./DataManager.js";
 import Template from "./Template.js";
 import CurseManager from "./CurseManager.js";
 import BossManager from "./BossManager.js";
-import TimeEventsManager from "./TimeEventsManager.js"
+import TimeEventsManager from "./TimeEventsManager.js";
 import CommandsManager from "./CommandsManager.js";
 import QuestManager from "./QuestManager.js";
 import ActionManager from "./ActionManager.js";
 import CounterManager from "./CounterManager.js";
 import ErrorsHandler from "./ErrorsHandler.js";
-import EventsManager from './EventsManager.js';
-import GuildVariablesManager from './GuildVariablesManager.js';
+import EventsManager from "./EventsManager.js";
+import GuildVariablesManager from "./GuildVariablesManager.js";
 import CooldownManager from "./CooldownManager.js";
 import Properties from "./Properties.js";
 import Executor from "./Executor.js";
+import I18nManager from "./I18nManager.js";
 import APIPointAuthorizationManager from "./APIPointAuthorization.js";
 
 export {
-	DataManager, Template, CurseManager, BossManager, TimeEventsManager,
-	CommandsManager, QuestManager, ActionManager, CounterManager, ErrorsHandler,
-	EventsManager, GuildVariablesManager, CooldownManager, 
-	Properties, Executor, APIPointAuthorizationManager
+  DataManager, Template, CurseManager, BossManager, TimeEventsManager,
+  CommandsManager, QuestManager, ActionManager, CounterManager, ErrorsHandler,
+  EventsManager, GuildVariablesManager, CooldownManager, 
+  Properties, Executor, I18nManager, APIPointAuthorizationManager
 };
