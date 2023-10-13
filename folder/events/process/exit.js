b2c7c99839bf377fb88ee9879a46d79ccbe16bf8
zoodogood <zoodogood@gmail.com>
2023-10-13 13:21:58 +0300

    Move code to events/core/beforeExit

--- folder/events/process/exit.js
+++ folder/events/process/exit.js
@@ -1,24 +1,21 @@
 import { BaseEvent } from "#lib/modules/EventsManager.js";
-import { DataManager, TimeEventsManager, ErrorsHandler } from '#lib/modules/mod.js';
+
 
 
 class Event extends BaseEvent {
-	constructor(){
-		const EVENT = "exit";
-		super(process, EVENT);
-	}
+  constructor(){
+    const EVENT = "exit";
+    super(process, EVENT);
+  }
 
-	async run(){
-		console.info("\n   ЗАВЕРШЕНИЕ...\n");
-		DataManager.file.write();
-		TimeEventsManager.file.write();
-		ErrorsHandler.Audit.createLog();
-	}
+  async run(){
+    console.info("\n   ЗАВЕРШЕНИЕ...\n");
+  }
 
-	options = {
-		name: "process/exit",
-		once: true
-	}
+  options = {
+    name: "process/exit",
+    once: true
+  };
 }
 
 export default Event;
