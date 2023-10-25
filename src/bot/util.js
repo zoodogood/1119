aca31f67df22b3a6a1c0e580cb284edd0affcd9b
zoodogood <zoodogood@gmail.com>
2023-10-25 11:07:29 +0300

    Fix

--- src/bot/util.js
+++ src/bot/util.js
@@ -1,54 +1,63 @@
-import Discord from 'discord.js';
+import Discord from "discord.js";
 
-
-
-async function awaitUserAccept({name, message, channel, userData}){
+async function awaitUserAccept({ name, message, channel, userData }) {
   const prefix = "userAccept_";
-  if (`${ prefix }${name}` in userData) {
+  if (`${prefix}${name}` in userData) {
     return true;
   }
   const context = {};
   context.message = await channel.msg(message);
-  const react = await context.message.awaitReact({user: userData, removeType: "all"}, "685057435161198594", "763807890573885456");
+  const react = await context.message.awaitReact(
+    { user: userData, removeType: "all" },
+    "685057435161198594",
+    "763807890573885456",
+  );
   await context.message.delete();
 
   if (react === "685057435161198594") {
-    userData[`${ prefix }${ name }`] = 1;
+    userData[`${prefix}${name}`] = 1;
     return true;
   }
   return false;
-};
+}
 
-function awaitReactOrMessage({target, user, time, reactionOptions = {}, messageOptions = {}}){
-  const reactions = reactionOptions.reactions?.filter(Boolean);
+function awaitReactOrMessage({
+  target,
+  user,
+  time,
+  reactionOptions = {},
+  messageOptions = {},
+}) {
+  const reactions = reactionOptions.reactions?.filter(Boolean) ?? [];
 
   const MAX_TIMEOUT = time ?? 900_000;
 
-  const filter = (some, adding) => some instanceof Discord.Message ? 
-    some.author.id === user.id :
-    adding.id === user.id && (!reactions.length || reactions.includes(some.emoji.id ?? some.emoji.name));
+  const filter = (some, adding) =>
+    some instanceof Discord.Message
+      ? some.author.id === user.id
+      : adding.id === user.id &&
+        (!reactions.length ||
+          reactions.includes(some.emoji.id ?? some.emoji.name));
 
   const collectorOptions = { max: 1, time: MAX_TIMEOUT, filter };
 
-  reactions.forEach(reaction => target.react(reaction));
+  reactions.forEach((reaction) => target.react(reaction));
 
   return new Promise(async (resolve) => {
     const collected = await Promise.race([
-      target.channel.awaitMessages({...collectorOptions, ...messageOptions}),
-      target.awaitReactions({...collectorOptions, ...reactionOptions})
+      target.channel.awaitMessages({ ...collectorOptions, ...messageOptions }),
+      target.awaitReactions({ ...collectorOptions, ...reactionOptions }),
     ]);
 
     const some = collected.first();
-    if (some instanceof Discord.Message){
+    if (some instanceof Discord.Message) {
       some.delete();
     }
-    target.reactions.cache.each(reaction => reaction.users.remove(target.client.user));
+    target.reactions.cache.each((reaction) =>
+      reaction.users.remove(target.client.user),
+    );
     resolve(some);
   });
-};
+}
 
-
-export {
-  awaitUserAccept,
-  awaitReactOrMessage
-};
+export { awaitUserAccept, awaitReactOrMessage };
