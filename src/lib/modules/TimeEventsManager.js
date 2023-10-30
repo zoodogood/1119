c6e91a85e0b55d33037fdfe6963f0f9056aa6c2d
zoodogood <zoodogood@gmail.com>
2023-10-30 14:21:59 +0200

    Fix

--- src/lib/modules/TimeEventsManager.js
+++ src/lib/modules/TimeEventsManager.js
@@ -4,49 +4,46 @@ import EventEmitter from "events";
 import * as Util from "#lib/util.js";
 
 class TimeEventsManager {
-
   static #lastSeenDay;
 
-  static at(day){
+  static at(day) {
     return this.data[day];
   }
 
   static Util = Util;
 
-  static create(eventName, ms, params){
+  static create(eventName, ms, params) {
     const event = {
       name: eventName,
       timestamp: Date.now() + ms,
-      params: params ? JSON.stringify(params) : undefined
+      params: params ? JSON.stringify(params) : undefined,
     };
     const day = this.Util.timestampDay(event.timestamp);
 
     this.data[day] ||= [];
     this.data[day].push(event);
     this.data[day].sortBy("timestamp");
-    
-    
-    
-    if (day <= this.#lastSeenDay || !this.#lastSeenDay){
-      (day < this.#lastSeenDay) && (this.#lastSeenDay = null);
+
+    if (day <= this.#lastSeenDay || !this.#lastSeenDay) {
+      day < this.#lastSeenDay && (this.#lastSeenDay = null);
       this.handle();
     }
 
-    console.info(`Ивент создан ${ event.name }`);
+    console.info(`Ивент создан ${event.name}`);
     return event;
   }
 
-  static findEventInRange(filter, range){
+  static findEventInRange(filter, range) {
     const events = this.getEventsInRange(range);
     return events.find(filter);
   }
 
-  static filterEventsInRange(filter, range){
+  static filterEventsInRange(filter, range) {
     const events = this.getEventsInRange(range);
     return events.filter(filter);
   }
 
-  static getEventsInRange(range){
+  static getEventsInRange(range) {
     const days = Util.rangeToArray(range);
     const events = [];
     for (const day of days) {
@@ -57,15 +54,15 @@ class TimeEventsManager {
     return events;
   }
 
-  static remove(event){
+  static remove(event) {
     const day = this.Util.timestampDay(event.timestamp);
-    const index = this.data[day].indexOf(event);
-    if (~index === 0){
+    const index = this.data[day]?.indexOf(event);
+    if (!index || ~index === 0) {
       return false;
     }
     this.data[day].splice(index, 1);
 
-    if (this.data[day].length === 0){
+    if (this.data[day].length === 0) {
       delete this.data[day];
     }
 
@@ -73,113 +70,119 @@ class TimeEventsManager {
     return true;
   }
 
-  static change(event, data){
+  static change(event, data) {
     this.remove(event);
     Object.assign(
       event,
-      Util.omit(data, (key) => ["name", "timestamp", "params"].includes(key))
+      Util.omit(data, (key) => ["name", "timestamp", "params"].includes(key)),
     );
 
     this.create(event.name, event.timestamp - Date.now(), event.params);
     return event;
   }
 
-  static fetchNextEvent(){
+  static fetchNextEvent() {
     const day = this.getDistancePrefferedDayEvents();
     return day?.at(0) ?? null;
   }
 
-  static getDistancePrefferedDayEvents(needCache = true){
-
+  static getDistancePrefferedDayEvents(needCache = true) {
     const day = this.#lastSeenDay || this.getNearestDay();
-
 
     needCache && (this.#lastSeenDay = day);
 
     const dayEvents = this.at(day);
-    if (!dayEvents){
+    if (!dayEvents) {
       return null;
     }
 
     // Day without events is not preffered
-    if (!dayEvents.length){
+    if (!dayEvents.length) {
       this.#lastSeenDay = null;
       return this.getDistancePrefferedDayEvents();
     }
     return dayEvents;
   }
 
-  static getExistsDaysList(){
+  static getExistsDaysList() {
     const days = Object.keys(this.data);
-    if (days.length === 0){
+    if (days.length === 0) {
       return null;
     }
     return days;
   }
 
-  static getNearestDay(){
+  static getNearestDay() {
     const days = this.getExistsDaysList();
-    if (!days){
+    if (!days) {
       return null;
     }
 
-    const day = days
-      .reduce((min, day) => Math.min(min, day));
+    const day = days.reduce((min, day) => Math.min(min, day));
 
     return +day;
   }
 
-  static handle(){
+  static handle() {
     this.file.write();
 
     clearTimeout(this.timeout);
 
     const event = this.fetchNextEvent();
-    if (!event){
+    if (!event) {
       this.#lastSeenDay = null;
       return;
     }
 
     const timeTo = event.timestamp - Date.now();
-    if (timeTo > 10_000){
-      const parse = new Intl.DateTimeFormat("ru-ru", {weekday: "short", hour: "2-digit", minute: "2-digit"}).format();
-      console.info(`{\n\n  Имя события: ${ event.name },\n  Текущее время: ${ parse },\n  Времени до начала: ${Util.timestampToDate(timeTo)}\n\n}`);
+    if (timeTo > 10_000) {
+      const parse = new Intl.DateTimeFormat("ru-ru", {
+        weekday: "short",
+        hour: "2-digit",
+        minute: "2-digit",
+      }).format();
+      console.info(
+        `{\n\n  Имя события: ${
+          event.name
+        },\n  Текущее время: ${parse},\n  Времени до начала: ${Util.timestampToDate(
+          timeTo,
+        )}\n\n}`,
+      );
     }
 
     this.timeout = setTimeout(this.onTimeout.bind(this), timeTo);
     return;
   }
 
-  static onTimeout(){
+  static onTimeout() {
     const event = this.fetchNextEvent();
-    if (!event){
+    if (!event) {
       return;
     }
-    if (event.timestamp > Date.now()){
+    if (event.timestamp > Date.now()) {
       return this.handle();
     }
 
     this.executeEvent(event);
-    this.handle(); 
+    this.handle();
   }
 
-
-  static executeEvent(event){
+  static executeEvent(event) {
     this.remove(event);
 
     const data = {
       name: event.name,
       isLost: Date.now() - event.timestamp < -10000,
-      params: event.params ? JSON.parse(event.params) : undefined
+      params: event.params ? JSON.parse(event.params) : undefined,
     };
     this.emitter.emit("event", data);
-    console.info(`Ивент выполнен ${ event.name }`);
+    console.info(`Ивент выполнен ${event.name}`);
 
     return;
   }
 
   static file = {
-    path: `${ process.cwd() }/folder/data/time.json`,
+    path: `${process.cwd()}/folder/data/time.json`,
     load: () => {
       const path = this.file.path;
       const content = FileSystem.readFileSync(path, "utf-8");
@@ -190,7 +193,7 @@ class TimeEventsManager {
       const path = this.file.path;
       const data = JSON.stringify(this.data);
       FileSystem.writeFileSync(path, data);
-    }
+    },
   };
 
   static emitter = new EventEmitter();
