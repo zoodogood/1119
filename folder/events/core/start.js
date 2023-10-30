f6da79405c131a6ec7dea02dd649f35ecdae93f3
zoodogood <zoodogood@gmail.com>
2023-10-30 14:58:17 +0200

    Clean a code

--- folder/events/core/start.js
+++ folder/events/core/start.js
@@ -2,18 +2,24 @@ import { BaseEvent } from "#lib/modules/EventsManager.js";
 import { assert } from "console";
 import { omit, timestampDay } from "#src/lib/util.js";
 
-import { DataManager, TimeEventsManager, CommandsManager, CounterManager, EventsManager } from "#lib/modules/mod.js";
+import {
+  DataManager,
+  TimeEventsManager,
+  CommandsManager,
+  CounterManager,
+  EventsManager,
+} from "#lib/modules/mod.js";
 import { client } from "#bot/client.js";
 
 import app from "#app";
 
 class Event extends BaseEvent {
-  constructor(){
+  constructor() {
     const EVENT = "start";
     super(EventsManager.emitter, EVENT);
   }
 
-  async run(){
+  async run() {
     EventsManager.listenAll();
 
     await DataManager.file.load();
@@ -23,41 +29,34 @@ class Event extends BaseEvent {
 
     this.checkDataManagerFullset();
 
-
     await CommandsManager.importCommands();
     CommandsManager.createCallMap();
 
-
-
     TimeEventsManager.handle();
 
-
-
-    const needUpdate = DataManager.data.bot.currentDay !== timestampDay( Date.now() );
-    if (needUpdate){
-      await EventsManager.collection.get("TimeEvent/new-day")
-        .run(true);
-
+    const needUpdate =
+      DataManager.data.bot.currentDay !== timestampDay(Date.now());
+    if (needUpdate) {
+      await EventsManager.collection.get("TimeEvent/new-day").run(true);
     }
-
 
     app.client = client;
     client.login(process.env.DISCORD_TOKEN);
   }
 
-  checkDataManagerFullset(){
+  checkDataManagerFullset() {
     const Data = DataManager.data;
 
     assert(Data.users);
     assert(Data.guilds);
     assert(Data.bot);
     const defaultData = {
-      commandsUsed: {}
+      commandsUsed: {},
     };
 
     Object.assign(
-      Data.bot, 
-      omit(defaultData, (k) => k in Data.bot === false)
+      Data.bot,
+      omit(defaultData, (k) => k in Data.bot === false),
     );
 
     Data.bot.messagesToday ||= 0;
@@ -73,8 +72,10 @@ class Event extends BaseEvent {
     Data.dailyAudit ||= {};
 
     const now = Date.now();
-    Data.users.forEach(user =>
-      Object.keys(user).forEach(key => key.startsWith("CD") && user[key] < now ? delete user[key] : false)
+    Data.users.forEach((user) =>
+      Object.keys(user).forEach((key) =>
+        key.startsWith("CD") && user[key] < now ? delete user[key] : false,
+      ),
     );
     Data.users = Data.users.sort((a, b) => b.level - a.level);
 
@@ -83,7 +84,7 @@ class Event extends BaseEvent {
   }
 
   options = {
-    name: "core/start"
+    name: "core/start",
   };
 }
 
