515f0488bc48c054fdb530d5e1b11dd0484ef3e7
zoodogood <zoodogood@gmail.com>
2023-11-01 22:34:34 +0200

    Improve

--- src/site/src/enviroment/values.js
+++ src/site/src/enviroment/values.js
@@ -1,36 +1,38 @@
-
 import config from "#config";
-import { fetchFromInnerApi, getPackageJSON, yaml } from "#lib/safe-utils.js";
+import { fetchFromInnerApi, yaml } from "#lib/safe-utils.js";
+import { ReadPackageJson } from "#lib/util.js";
 import { ImportDirectory } from "@zoodogood/import-directory";
-import FileSystem from 'fs/promises';
-import Path from 'path';
+import FileSystem from "fs/promises";
+import Path from "path";
 
-const packageJSON = await getPackageJSON();
-const bot = (await fetchFromInnerApi("./client/user").catch(() => null)) ?? null;
+const packageJSON = await ReadPackageJson();
+const bot =
+  (await fetchFromInnerApi("./client/user").catch(() => null)) ?? null;
 
-const i18n = await new Promise(async resolve => {
-	
-	const FOLDER = `${ config.i18n.path }/site`;
-	const files = await new ImportDirectory({regex: /\.yaml$/}).takeFilesPath({path: FOLDER});
-	
-	const locales = {};
+const i18n = await new Promise(async (resolve) => {
+  const FOLDER = `${config.i18n.path}/site`;
+  const files = await new ImportDirectory({ regex: /\.yaml$/ }).takeFilesPath({
+    path: FOLDER,
+  });
 
-	for (const filePath of files){
-		const content = await FileSystem.readFile(filePath);
-		const name = Path.basename(filePath, ".yaml");
-		locales[name] = yaml.parse(String(content));
-	}
+  const locales = {};
 
-	resolve(locales);
-	return;
+  for (const filePath of files) {
+    const content = await FileSystem.readFile(filePath);
+    const name = Path.basename(filePath, ".yaml");
+    locales[name] = yaml.parse(String(content));
+  }
+
+  resolve(locales);
+  return;
 });
 
 const data = {
-	bot: Object(bot).id ? bot : null,
-	version: packageJSON.version,
-	buildedTimestamp: Date.now(),
-	i18n
-}
+  bot: Object(bot).id ? bot : null,
+  version: packageJSON.version,
+  buildedTimestamp: Date.now(),
+  i18n,
+};
 export default {
-	__json__: JSON.stringify(data)
-}
+  __json__: JSON.stringify(data),
+};
