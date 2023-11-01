8cc673d230087393c8e4acae12ea6eb9c1df92a8
zoodogood <zoodogood@gmail.com>
2023-11-01 22:36:13 +0200

    Improve

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,31 +1,25 @@
-import { OAuth2Scopes, PermissionFlagsBits } from 'discord.js';
-import Path from 'path';
+import { OAuth2Scopes, PermissionFlagsBits } from "discord.js";
+import Path from "path";
 const root = process.cwd();
 
-async function ReadPackageJson(){
-	const {default: FileSystem} = await import("fs/promises");
-	const value = await FileSystem.readFile("package.json");
-	
-	return JSON.parse(value);
+async function ReadPackageJson() {
+  const { default: FileSystem } = await import("fs/promises");
+  const value = await FileSystem.readFile(`${process.cwd()}/package.json`);
+
+  return JSON.parse(value);
 }
 
-
-function takePath(...relativePath){
-	return Path.resolve(root, ...relativePath);
+function takePath(...relativePath) {
+  return Path.resolve(root, ...relativePath);
 }
 
-function generateInviteFor(client){
-	const scopes = [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands];
-	const permissions = [
-		PermissionFlagsBits.Administrator,
-		PermissionFlagsBits.ManageEmojisAndStickers
-
-	];
-	return client.generateInvite({scopes, permissions});
+function generateInviteFor(client) {
+  const scopes = [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands];
+  const permissions = [
+    PermissionFlagsBits.Administrator,
+    PermissionFlagsBits.ManageEmojisAndStickers,
+  ];
+  return client.generateInvite({ scopes, permissions });
 }
 
- export {
-	takePath,
-	ReadPackageJson,
-	generateInviteFor
-};
+export { takePath, ReadPackageJson, generateInviteFor };
