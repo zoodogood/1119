cf9a6ddaf2bc00e400d2385632ba176ead2260c2
zoodogood <zoodogood@gmail.com>
2023-11-02 23:02:02 +0200

    Change interval to 5 minutes

--- folder/events/TimeEvents/autosave.js
+++ folder/events/TimeEvents/autosave.js
@@ -1,18 +1,22 @@
-import {TimeEventsManager, DataManager} from '#lib/modules/mod.js';
-
+import {
+  TimeEventsManager,
+  DataManager,
+  CounterManager,
+} from "#lib/modules/mod.js";
 
 class Event {
-	static INTERVAL = 7_200_000;
+  static INTERVAL = 60_000 * 5;
 
-	run(){
-		DataManager.file.write();
-    	return TimeEventsManager.create("autosave", this.constructor.INTERVAL);
-	}
+  run() {
+    DataManager.file.write();
+    TimeEventsManager.file.write();
+    CounterManager.file.write();
+    return TimeEventsManager.create("autosave", this.constructor.INTERVAL);
+  }
 
-
-	options = {
-		name: "TimeEvent/autosave"
-	}
+  options = {
+    name: "TimeEvent/autosave",
+  };
 }
 
 export default Event;
