d5d68e5aad7b8dea018313784ab41fc89a7f2cd9
zoodogood <zoodogood@gmail.com>
2023-11-02 23:02:22 +0200

    Improve

--- src/lib/MongoDBDriver.js
+++ src/lib/MongoDBDriver.js
@@ -1,29 +1,32 @@
 import config from "#config";
-import { getPackageJSON } from "#lib/safe-utils.js";
+import { ReadPackageJson } from "#lib/util.js";
 
 class MongoDBDriver {
-
-
-  async init(){
+  async init() {
     const { MongoClient, ...mongo } = await import("mongodb");
     this.mongo = mongo;
     this.MongoClient = MongoClient;
     this.client = new MongoClient(config.database.mongoURL);
-    const { name } = getPackageJSON();
+    const { name } = await ReadPackageJson();
     this.db = this.client.db(name);
     this.files = this.db.collection("files");
 
     return this;
   }
 
-  async writeFile(name, content){
-    const options = {upsert: true};
-    return await this.files.updateOne({name}, {$set: {name, content}}, options);
+  async writeFile(name, content) {
+    const options = { upsert: true };
+    return await this.files.updateOne(
+      { name },
+      { $set: { name, content } },
+      options,
+    );
   }
 
-  async readFile(name){
-    return await this.files.findOne({name});
+  async readFile(name) {
+    return await this.files.findOne({ name });
   }
 }
 
 export { MongoDBDriver };
+export default MongoDBDriver;
