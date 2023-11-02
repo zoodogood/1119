4870550219fa87c9d94e5b196ecf84d7b2350cb2
zoodogood <zoodogood@gmail.com>
2023-11-02 23:02:59 +0200

    Working on a module

--- src/lib/modules/StorageManager.js
+++ src/lib/modules/StorageManager.js
@@ -1,7 +1,28 @@
-
-
 class StorageManager {
-  
+  static async setDriver(driverId) {
+    const module = await (() => {
+      switch (driverId) {
+        case "localdb":
+          return import("#lib/FileDBDriver.js");
+        case "mongodb":
+          return import("#lib/MongoDBDriver.js");
+        default:
+          throw new Error(
+            "Maybe. Storage manager driver not selected in config file at database.driver; Incorrect driverId",
+          );
+      }
+    })();
+
+    this.driver = new module.default();
+
+    await this.driver.init();
+  }
+  static async write(name, content) {
+    await this.driver.writeFile(name, content);
+  }
+  static async read(name) {
+    await this.driver.readFile(name);
+  }
 }
 
 export default StorageManager;
