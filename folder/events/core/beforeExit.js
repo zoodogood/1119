d48e0a27e5e5269ff7919863e465efd4784e279d
zoodogood <zoodogood@gmail.com>
2023-11-02 23:01:18 +0200

    Call CounterManager.file.write()

--- folder/events/core/beforeExit.js
+++ folder/events/core/beforeExit.js
@@ -1,25 +1,29 @@
 import EventsManager, { BaseEvent } from "#lib/modules/EventsManager.js";
-import { DataManager, TimeEventsManager, ErrorsHandler } from "#lib/modules/mod.js";
-
+import {
+  DataManager,
+  TimeEventsManager,
+  ErrorsHandler,
+  CounterManager,
+} from "#lib/modules/mod.js";
 
 class Event extends BaseEvent {
-  constructor(){
+  constructor() {
     const EVENT = "beforeExit";
     super(EventsManager.emitter, EVENT);
   }
 
-  async run(){
-    DataManager.file.write();
-    TimeEventsManager.file.write();
-    ErrorsHandler.Audit.createLog();
+  async run() {
+    await DataManager.file.write();
+    await TimeEventsManager.file.write();
+    await CounterManager.file.write();
+    await ErrorsHandler.Audit.createLog();
 
-		
     process.exit();
   }
 
   options = {
     name: "process/beforeExit",
-    once: true
+    once: true,
   };
 }
 
