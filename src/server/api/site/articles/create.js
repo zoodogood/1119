4c1bba366c878b0b2d9283418c004adec327cb1c
zoodogood <zoodogood@gmail.com>
2023-11-07 16:45:57 +0200

    Clean a code

--- src/server/api/site/articles/create.js
+++ src/server/api/site/articles/create.js
@@ -1,38 +1,39 @@
 import { BaseRoute } from "#server/router.js";
-import { ArticlesManager } from './.mod.js';
+import { ArticlesManager } from "./.mod.js";
 import { omit } from "#lib/safe-utils.js";
 import bodyParser from "body-parser";
 import { authorizationProtocol } from "#lib/modules/APIPointAuthorization.js";
 
-
 const PREFIX = "/site/articles/create";
 
-
 class Route extends BaseRoute {
-	prefix = PREFIX;
+  prefix = PREFIX;
 
-	constructor(express){
-		super();
-	}
+  constructor(express) {
+    super();
+  }
 
-	async post(request, response){
-		const {data: user} = authorizationProtocol(request, response);
+  async post(request, response) {
+    const { data: user } = authorizationProtocol(request, response);
 
-		await new Promise(resolve => 
-			bodyParser.raw()(request, response, resolve)
-		);
+    await new Promise((resolve) =>
+      bodyParser.raw()(request, response, resolve),
+    );
 
-		const author = omit(
-			user,
-			(key) => ["id", "avatarURL", "username", "discriminator"].includes(key)
-		);
+    const author = omit(user, (key) =>
+      ["id", "avatarURL", "username", "discriminator"].includes(key),
+    );
 
-		const filename = request.headers.filename;
-		const content = String(request.body);
+    const filename = request.headers.filename;
+    const content = String(request.body);
 
-		const data = await ArticlesManager.createArticle({content, author, id: `${ user.id }/${ filename }`});
-		response.json( data );
-	}
+    const data = await ArticlesManager.createArticle({
+      content,
+      author,
+      id: `${user.id}/${filename}`,
+    });
+    response.json(data);
+  }
 }
 
 export default Route;
