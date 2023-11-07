7ec2b1753fcb2142f1b9a6cb5554e16680109500
zoodogood <zoodogood@gmail.com>
2023-11-07 17:45:50 +0200

    Clean a code

--- folder/events/process/uncaughtException.js
+++ folder/events/process/uncaughtException.js
@@ -1,21 +1,19 @@
 import ErrorsHandler from "#lib/modules/ErrorsHandler.js";
 import EventsManager, { BaseEvent } from "#lib/modules/EventsManager.js";
 
-
-
 class Event extends BaseEvent {
-  constructor(){
+  constructor() {
     const EVENT = "uncaughtException";
     super(process, EVENT);
   }
 
-  async run(error){
-    ErrorsHandler.Audit.push(error, {uncaughtException: true});
+  async run(error) {
+    ErrorsHandler.Audit.push(error, { uncaughtException: true });
     EventsManager.emitter.emit("beforeExit");
   }
 
   options = {
-    name: "process/uncaughtException"
+    name: "process/uncaughtException",
   };
 }
 
