3fcffeeb7af7ff545df7ea46242c4b6d28aa2fc4
zoodogood <zoodogood@gmail.com>
2023-11-10 22:59:32 +0200

    Clean a code; Improve

--- folder/events/users/levelIncrease.js
+++ folder/events/users/levelIncrease.js
@@ -1,53 +1,51 @@
 import { BaseEvent, EventsManager } from "#lib/modules/EventsManager.js";
 
-
 import { LEVELINCREASE_EXPERIENCE_PER_LEVEL as EXPERIENCE_PER_LEVEL } from "#lib/constants/users/events.js";
 import { sleep } from "#lib/safe-utils.js";
 
-
-
 class Event extends BaseEvent {
-	constructor(){
-		const EVENT = "users/levelIncrease";
-		super(EventsManager.emitter, EVENT);
-	}
+  constructor() {
+    const EVENT = "users/levelIncrease";
+    super(EventsManager.emitter, EVENT);
+  }
 
-	async onLevelIncrease(user, message){
-		const initialLevel = user.level;
+  async onLevelIncrease(user, message) {
+    const initialLevel = user.level;
 
-		while (user.exp >= user.level * EXPERIENCE_PER_LEVEL){
-			const expSummary = user.level * EXPERIENCE_PER_LEVEL;
-			const coefficient = Math.max(0.97716 ** user.voidRituals, 0.01);
-			user.exp -= Math.ceil(expSummary * coefficient);
-			user.level++;
-		 }
-		 
-		
+    while (user.exp >= user.level * EXPERIENCE_PER_LEVEL) {
+      const expSummary = user.level * EXPERIENCE_PER_LEVEL;
+      const coefficient = Math.max(0.97716 ** user.voidRituals, 0.01);
+      user.exp -= Math.ceil(expSummary * coefficient);
+      user.level++;
+    }
 
-		(async (originalMessage) => {
-			const author = originalMessage.author;
+    (async (originalMessage) => {
+      const author = originalMessage.author;
 
-			const textContent = user.level - initialLevel > 2 ?
-				`**${ author.username } повышает уровень с ${ initialLevel } до ${ user.level }!**` :
-				`**${ author.username } получает ${ user.level } уровень!**`;
+      const textContent =
+        user.level - initialLevel > 2
+          ? `**${author.username} повышает уровень с ${initialLevel} до ${user.level}!**`
+          : `**${author.username} получает ${user.level} уровень!**`;
 
-			const message = await originalMessage.msg({content: textContent});
-			
-			if (message.channel.id !== message.guild.data.chatChannel) {
-				await sleep(5000);
-				message.delete();
-			}
-		})(message);
-	}
+      const message = await originalMessage.msg({ content: textContent });
 
-	async run({user, message}){
-		this.onLevelIncrease(user, message);
-	}
+      if (
+        !message.guild ||
+        message.channel.id !== message.guild.data.chatChannel
+      ) {
+        await sleep(5000);
+        message.delete();
+      }
+    })(message);
+  }
 
-	options = {
-		name: "users/levelIncrease"
-	}
+  async run({ user, message }) {
+    this.onLevelIncrease(user, message);
+  }
+
+  options = {
+    name: "users/levelIncrease",
+  };
 }
-
 
 export default Event;
