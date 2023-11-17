9d6aaed6a22575e2f854763273af16ecce976a20
zoodogood <zoodogood@gmail.com>
2023-11-17 11:35:07 +0200

    Move code to #constants/actionsMap

---
+++ src/app/constants/actionsMap.js
@@ -0,0 +1,45 @@
+const ActionsMap = {
+  // Client
+  messageCreate: "messageCreate",
+
+  // User
+  beforeProfileDisplay: "beforeProfileDisplay",
+  casinoSession: "casinoSession",
+  coinFromMessage: "coinFromMessage",
+  likedTheUser: "likedTheUser",
+  buyFromGrempen: "buyFromGrempen",
+  callBot: "callBot",
+  berryBarter: "berryBarter",
+  openChest: "openChest",
+  chilliBooh: "chilliBooh",
+  praiseUser: "praiseUser",
+  userPraiseMe: "userPraiseMe",
+  anonTaskResolve: "anonTaskResolve",
+  beforeBagInteracted: "beforeBagInteracted",
+  // Quest
+  dailyQuestSkiped: "dailyQuestSkiped",
+  dailyQuestComplete: "dailyQuestComplete",
+  globalQuest: "globalQuest",
+  dailyQuestInit: "dailyQuestInit",
+  beforeDailyQuestInit: "beforeDailyQuestInit",
+  // Boss
+  bossAfterAttack: "bossAfterAttack",
+  bossBeforeAttack: "bossBeforeAttack",
+  bossMakeDamage: "bossMakeDamage",
+  bossBeforeEffectInit: "bossBeforeEffectInit",
+  bossEffectInit: "bossEffectInit",
+  bossEffectTimeoutEnd: "bossEffectTimeoutEnd",
+  bossEffectEnd: "bossEffectEnd",
+  // Curse
+  curseInit: "curseInit",
+  curseEnd: "curseEnd",
+  curseTimeEnd: "curseTimeEnd",
+  curseBeforeSetProgress: "curseBeforeSetProgress",
+  // Other
+  resourceChange: "resourceChange",
+  callCommand: "callCommand",
+  inputCommandParsed: "inputCommandParsed",
+  any: "_any",
+};
+
+export { ActionsMap };
--- src/lib/modules/ActionManager.js
+++ src/lib/modules/ActionManager.js
@@ -1,50 +1,7 @@
 import { User as DiscordUser } from "discord.js";
 import { CurseManager, QuestManager, ErrorsHandler } from "#lib/modules/mod.js";
 import { BossEffects } from "#lib/modules/BossManager.js";
-
-const ActionsMap = {
-  // Client
-  messageCreate: "messageCreate",
-
-  // User
-  beforeProfileDisplay: "beforeProfileDisplay",
-  casinoSession: "casinoSession",
-  coinFromMessage: "coinFromMessage",
-  likedTheUser: "likedTheUser",
-  buyFromGrempen: "buyFromGrempen",
-  callBot: "callBot",
-  berryBarter: "berryBarter",
-  openChest: "openChest",
-  chilliBooh: "chilliBooh",
-  praiseUser: "praiseUser",
-  userPraiseMe: "userPraiseMe",
-  anonTaskResolve: "anonTaskResolve",
-  beforeBagInteracted: "beforeBagInteracted",
-  // Quest
-  dailyQuestSkiped: "dailyQuestSkiped",
-  dailyQuestComplete: "dailyQuestComplete",
-  globalQuest: "globalQuest",
-  dailyQuestInit: "dailyQuestInit",
-  beforeDailyQuestInit: "beforeDailyQuestInit",
-  // Boss
-  bossAfterAttack: "bossAfterAttack",
-  bossBeforeAttack: "bossBeforeAttack",
-  bossMakeDamage: "bossMakeDamage",
-  bossBeforeEffectInit: "bossBeforeEffectInit",
-  bossEffectInit: "bossEffectInit",
-  bossEffectTimeoutEnd: "bossEffectTimeoutEnd",
-  bossEffectEnd: "bossEffectEnd",
-  // Curse
-  curseInit: "curseInit",
-  curseEnd: "curseEnd",
-  curseTimeEnd: "curseTimeEnd",
-  curseBeforeSetProgress: "curseBeforeSetProgress",
-  // Other
-  resourceChange: "resourceChange",
-  callCommand: "callCommand",
-  inputCommandParsed: "inputCommandParsed",
-  any: "_any",
-};
+import { ActionsMap } from "#constants/actionsMap.js";
 
 class ActionManager {
   static Actions = ActionsMap;
