394288e889cfc925862d2841cdfdfc3e1598afc4
zoodogood <zoodogood@gmail.com>
2023-11-19 15:42:28 +0200

    Export object with constants

---
+++ src/app/constants/mod.js
@@ -0,0 +1,10 @@
+import users from "./users/mod.js";
+import enums from "./enums/mod.js";
+import { Schema } from "./Schema.js";
+const Constants = {
+  users,
+  enums,
+  Schema,
+};
+
+export { Constants };
