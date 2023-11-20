ccd4a50c9de7fab6d6868a4596722151667139b9
zoodogood <zoodogood@gmail.com>
2023-11-20 09:35:37 +0200

    Clean a code

--- src/lib/MarkdownMetadata.js
+++ src/lib/MarkdownMetadata.js
@@ -1,46 +1,46 @@
-import yaml from 'yaml';
+import yaml from "yaml";
 
 class MarkdownMetadata {
-	static regex = /(?<open>(?:\s\t)*^---(?:\s\t)*\n)(?<content>.*?)(?<close>(?:\s\t)*^---(?:\s\t)*\n)/sm;
-	static FIELD_CONTENT = "---\n\n---\n";
+  static regex =
+    /(?<open>(?:\s\t)*^---(?:\s\t)*\n)(?<content>.*?)(?<close>(?:\s\t)*^---(?:\s\t)*\n)/ms;
+  static FIELD_CONTENT = "---\n\n---\n";
 
-	static yaml = yaml;
+  static yaml = yaml;
 
-	static replace(content, data){
-		if (!content.match(this.regex)){
-			content = this.createMetadataFieldIn(content);
-		}
+  static replace(content, data) {
+    if (!content.match(this.regex)) {
+      content = this.createMetadataFieldIn(content);
+    }
 
+    data = yaml.stringify(data);
+    return content.replace(
+      this.regex,
+      (_full, open, _content, close) => `${open}${data}${close}`,
+    );
+  }
 
-		data = yaml.stringify(data);
-		return content.replace(
-			this.regex,
-			(_full, open, _content, close) => `${ open }${ data }${ close }`
-		);
-	}
+  static parse(content) {
+    const metadata = content.match(this.regex);
+    if (!metadata) {
+      return null;
+    }
 
-	static parse(content){
-		const metadata = content.match(this.regex);
-		if (!metadata){
-			return null;
-		}
+    return yaml.parse(metadata.groups.content);
+  }
 
-		return yaml.parse(metadata.groups.content);
-	}
+  static assign(content, data) {
+    data = Object.assign(data, this.parse(content));
+    return this.replace(content, data);
+  }
 
-	static assign(content, data){
-		data = Object.assign(data, this.parse(content));
-		return this.replace(content, data);
-	}
+  static createMetadataFieldIn(content) {
+    const field = this.FIELD_CONTENT;
+    return `${field}${content}`;
+  }
 
-	static createMetadataFieldIn(content){
-		const field = this.FIELD_CONTENT;
-		return `${ field }${ content }`;
-	}
-
-	static removeMetadataFieldIn(content){
-		return content.replace(this.regex, "");
-	}
+  static removeMetadataFieldIn(content) {
+    return content.replace(this.regex, "");
+  }
 }
 
 export { MarkdownMetadata };
