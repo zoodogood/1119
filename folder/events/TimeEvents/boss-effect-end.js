eeb1149889c3a5807d2bdae37dcc03cce35714f0
zoodogood <zoodogood@gmail.com>
2023-11-27 22:40:11 +0200

    Fix; Clean a code

--- folder/events/TimeEvents/boss-effect-end.js
+++ folder/events/TimeEvents/boss-effect-end.js
@@ -1,27 +1,24 @@
-import { Actions } from '#lib/modules/ActionManager.js';
-
-
+import { client } from "#bot/client.js";
+import { Actions } from "#lib/modules/ActionManager.js";
 
 class Event {
+  run(isLost, userId, timestamp) {
+    const user = client.users.cache.get(userId);
+    const effects = user.data.bossEffects;
 
-	run(isLost, userId, timestamp){
-		const user = client.users.cache.get(userId);
-		const effects = user.data.bossEffects;
-		
-		const compare = effect => effect.timestamp === timestamp;
-		const effect = effects.find(compare);
+    const compare = (effect) => effect.timestamp === timestamp;
+    const effect = effects.find(compare);
 
-		if (!effect){
-			return;
-		}
-		
-		user.action(Actions.bossEffectTimeoutEnd, effect);
-	}
+    if (!effect) {
+      return;
+    }
 
+    user.action(Actions.bossEffectTimeoutEnd, effect);
+  }
 
-	options = {
-		name: "TimeEvent/boss-effect-timeout-end"
-	}
+  options = {
+    name: "TimeEvent/boss-effect-timeout-end",
+  };
 }
 
 export default Event;
