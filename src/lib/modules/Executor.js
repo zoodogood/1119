b60944952ccdea6fb2984096275e875f76f56f8f
zoodogood <zoodogood@gmail.com>
2023-12-02 13:52:25 +0200

    Clean a code

--- src/lib/modules/Executor.js
+++ src/lib/modules/Executor.js
@@ -1,29 +1,29 @@
-import EventsEmitter from 'events';
+import EventsEmitter from "events";
 
 class Executor {
-	static emitter = new EventsEmitter();
+  static emitter = new EventsEmitter();
 
-	static parseCustomId(customId){
-		// input example: @command/snippet/example:1:2
-		const [_full, key, target, parameters] = customId.match(/^@(.+?)\/(.+?)\/(.+?)$/) ?? [];
-		if (!key){
-			return null;
-		}
+  static parseCustomId(customId) {
+    // input example: @command/snippet/example:1:2
+    const [_full, key, target, parameters] =
+      customId.match(/^@(.+?)\/(.+?)\/(.+?)$/) ?? [];
+    if (!key) {
+      return null;
+    }
 
-		return [key, target, parameters];
-	}
+    return [key, target, parameters];
+  }
 
-	static emit(key, target, parameters){
-		const callback = this.#constructors[key];
-		return callback.call(null, target, parameters);
-	}
+  static emit(key, target, parameters) {
+    const callback = this.#constructors[key];
+    return callback.call(null, target, parameters);
+  }
 
-	static bind(key, callback){
-		this.#constructors[key] = callback;
+  static bind(key, callback) {
+    this.#constructors[key] = callback;
+  }
 
-	}
-
-	static #constructors = {};
+  static #constructors = {};
 }
 
 export { Executor };
