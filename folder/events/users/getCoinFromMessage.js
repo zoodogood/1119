8f8336b85340e58c79f982e06dbe5d73641e1ccd
zoodogood <zoodogood@gmail.com>
2023-12-03 17:49:25 +0200

    Clean a code
    Co-authored-by: Yakyzzza163 <Yakyzzza163@users.noreply.github.com>

--- folder/events/users/getCoinFromMessage.js
+++ folder/events/users/getCoinFromMessage.js
@@ -3,29 +3,31 @@ import DataManager from "#lib/modules/DataManager.js";
 import { BaseEvent, EventsManager } from "#lib/modules/EventsManager.js";
 import * as Util from "#lib/util.js";
 
-
-
-
 class Event extends BaseEvent {
-  constructor(){
+  constructor() {
     const EVENT = "users/getCoinsFromMessage";
     super(EventsManager.emitter, EVENT);
   }
 
-  async onGetCoinsFromMessage({userData, message}){
-    message.author.action(Actions.coinFromMessage, {channel: message.channel});
+  async onGetCoinsFromMessage({ userData, message }) {
+    message.author.action(Actions.coinFromMessage, {
+      channel: message.channel,
+    });
 
     let reaction = "637533074879414272";
     let k = 1;
 
-    if (DataManager.data.bot.dayDate === "31.12"){
+    if (DataManager.data.bot.dayDate === "31.12") {
       reaction = "❄️";
       k += 0.2;
     }
 
     if (message.guild && "cloverEffect" in message.guild.data) {
       reaction = "☘️";
-      let multiplier = 0.08 + (0.07 * ((1 - 0.9242 ** message.guild.data.cloverEffect.uses) / (1 - 0.9242)));
+      let multiplier =
+        0.08 +
+        0.07 *
+          ((1 - 0.9242 ** message.guild.data.cloverEffect.uses) / (1 - 0.9242));
       multiplier *= 2 ** (userData.voidMysticClover ?? 0);
       k += multiplier;
       message.guild.data.cloverEffect.coins++;
@@ -35,24 +37,34 @@ class Event extends BaseEvent {
     userData.coins += coins;
     userData.chestBonus = (userData.chestBonus ?? 0) + 5;
 
-    const react = await message.awaitReact({user: message.author, removeType: "full", time: 20000}, reaction);
+    const react = await message.awaitReact(
+      { user: message.author, removeType: "full", time: 20000 },
+      reaction,
+    );
 
     if (!react) {
       return;
     }
 
-    const messageContent = `> У вас ${ Util.ending(userData.coins, "коин", "ов", "", "а")} <:coin:637533074879414272>!\n> Получено ${coins}\n> Бонус сундука: ${userData.chestBonus || 0}`;
-    message.msg({content: messageContent, delete: 2500});
+    const messageContent = `> У вас ${Util.ending(
+      userData.coins,
+      "коин",
+      "ов",
+      "",
+      "а",
+    )} <:coin:637533074879414272>!\n> Получено ${coins}\n> Бонус сундука: ${
+      userData.chestBonus || 0
+    }`;
+    message.msg({ content: messageContent, delete: 2500 });
   }
 
-  async run({userData, message}){
-    this.onGetCoinsFromMessage({userData, message});
+  async run({ userData, message }) {
+    this.onGetCoinsFromMessage({ userData, message });
   }
 
   options = {
-    name: "users/getCoinsFromMessage"
+    name: "users/getCoinsFromMessage",
   };
 }
-
 
 export default Event;
