209d030529c00ecc5f45b8da6027da6fee8b31b0
zoodogood <zoodogood@gmail.com>
2023-12-14 16:14:05 +0200

    Fix

--- folder/commands/iq.js
+++ folder/commands/iq.js
@@ -8,6 +8,9 @@ class Command {
       client.users.cache.get(interaction.params) ||
       msg.author;
 
+    const { content } = interaction.message;
+    const commandName = content.match(/[a-zа-яїъё]+/i)?.[0] ?? "IQ";
+
     let first = true;
     if ("iq" in memb.data) {
       first = false;
@@ -20,12 +23,12 @@ class Command {
 
     let description;
     if (Util.random(18)) {
-      description = `У ${name}${!first ? " всё так же" : ""} ${iq} ${(
-        interaction.message.content.match(/[a-zа-яїъё]+/i)?.[0] ?? "IQ"
-      ).toUpperCase()}`;
+      description = `У ${name}${
+        !first ? " всё так же" : ""
+      } ${iq} ${commandName.toUpperCase()}`;
     } else {
       iq = ++memb.data.iq;
-      description = `Удивительно у ${name} айкью вырос на одну единицу! Сейчас ${interaction.command.toUpperCase()} === ${iq}`;
+      description = `Удивительно у ${name} айкью вырос на одну единицу! Сейчас ${commandName.toUpperCase()} === ${iq}`;
     }
     msg.msg({
       title: "<a:iq:768047041053196319> + <a:iq:768047041053196319> = ICQ²",
@@ -41,7 +44,7 @@ class Command {
       description:
         "\n\nХотя мы не знаем ваш настоящий IQ, можем предложить наш собственный..\nВозможно, когда-то у нас появится тест на ICQ\n\n✏️\n```python\n!iq <memb>\n```\n\n",
     },
-    allias: "iqmeme icq айкю айкью iqbanana",
+    allias: "iqmeme icq айкю айкью iqbanana iqmonkey",
     allowDM: true,
     cooldown: 15_000,
     type: "user",
