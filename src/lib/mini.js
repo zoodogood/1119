d91a4f21951752dbba070f9e5dc90826769a308c
zoodogood <zoodogood@gmail.com>
2023-12-16 19:51:10 +0200

    Add factoryGetPropertyValue, mapGetOrInsert

--- src/lib/mini.js
+++ src/lib/mini.js
@@ -1,3 +1,12 @@
 export function factorySummarizeBy(property) {
   return (acc, current) => acc + current[property] ?? 0;
 }
+
+export function factoryGetPropertyValue(property) {
+  return (x) => x[property];
+}
+
+export function mapGetOrInsert(map, key, defaults) {
+  !map.has(key) && map.set(key, defaults);
+  return map.get(key);
+}
