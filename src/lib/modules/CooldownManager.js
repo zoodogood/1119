27a87b960fa60001443b1afbde81433ff8f49eae
zoodogood <zoodogood@gmail.com>
2023-12-16 19:57:22 +0200

    Clean a code

--- src/lib/modules/CooldownManager.js
+++ src/lib/modules/CooldownManager.js
@@ -1,48 +1,45 @@
 class Cooldown {
-	constructor(target, key, {heat = 1, perCall = null}){
-		this.key = key;
-		this.target = target;
-		this.perCall = perCall;
-		this.heat = heat;
-	}
+  constructor(target, key, { heat = 1, perCall = null }) {
+    this.key = key;
+    this.target = target;
+    this.perCall = perCall;
+    this.heat = heat;
+  }
 
+  getCurrentCooldownEnd() {
+    return this.target[this.key];
+  }
 
-	getCurrentCooldownEnd(){
-		return this.target[this.key];
-	}
+  getCooldownThreshold() {
+    const current = this.getCurrentCooldownEnd();
+    const threshold = current - this.perCall * (this.heat - 1);
+    return threshold || 0;
+  }
 
-	getCooldownThreshold(){
-		const current = this.getCurrentCooldownEnd();
-		const threshold = current - this.perCall * (this.heat - 1);
-		return threshold || 0;
-	}
+  setCooldownThreshold(timestamp) {
+    this.target[this.key] = timestamp;
+    return this;
+  }
 
-	setCooldownThreshold(timestamp){
-		this.target[this.key] = timestamp;
-		return this;
-	}
+  call() {
+    const now = Math.max(Date.now(), this.getCurrentCooldownEnd() ?? 0);
+    this.setCooldownThreshold(now + this.perCall);
+    return this;
+  }
 
-	call(){
-		const now = Math.max(Date.now(), this.getCurrentCooldownEnd() ?? 0);
-		this.setCooldownThreshold(now + this.perCall);
-		return this;
-	}
+  checkYet() {
+    return this.diff() > 0;
+  }
 
-	checkYet(){
-		return this.diff() > 0;
-	}
-
-	diff(){
-		return this.getCooldownThreshold() - Date.now();
-	}
+  diff() {
+    return this.getCooldownThreshold() - Date.now();
+  }
 }
-
 
 class CooldownManager {
-	static api(target, key, {heat = 1, perCall = null} = {}){
-		return new Cooldown(target, key, {heat, perCall});
-	}
+  static api(target, key, { heat = 1, perCall = null } = {}) {
+    return new Cooldown(target, key, { heat, perCall });
+  }
 }
-
 
 export default CooldownManager;
