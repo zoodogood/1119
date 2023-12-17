3dd3f41c88f9729770f97f94b9e716aceb44421d
zoodogood <zoodogood@gmail.com>
2023-12-17 08:42:19 +0200

    Clean a code

--- src/server/api/_catchError.js
+++ src/server/api/_catchError.js
@@ -4,25 +4,29 @@ import { BaseRoute } from "#server/router.js";
 
 const PREFIX = "/";
 
-
 class Route extends BaseRoute {
-	constructor(express){
-		super();
+  constructor(express) {
+    super();
 
-		this.start();
-		this.express = express;
-	}
+    this.start();
+    this.express = express;
+  }
 
-	async catch(error, request, response, next){
-		ErrorsHandler.Audit.push(error, {path: request.originalUrl, source: "API"});
-		response.status(500);
-		response.json(`Ошибка сервера переданная от сервера: "${ error.message }". Информация об ошибке записана и может быть найдена по адресу /pages/errors/select`);
-	}
+  async catch(error, request, response, next) {
+    ErrorsHandler.Audit.push(error, {
+      path: request.originalUrl,
+      source: "API",
+    });
+    response.status(500);
+    response.json(
+      `Ошибка сервера переданная от сервера: "${error.message}". Информация об ошибке записана и может быть найдена по адресу /pages/errors/select`,
+    );
+  }
 
-	async start(){
-		await sleep(1);
-		this.express.use(PREFIX, this.catch);
-	}
+  async start() {
+    await sleep(1);
+    this.express.use(PREFIX, this.catch);
+  }
 }
 
 export default Route;
