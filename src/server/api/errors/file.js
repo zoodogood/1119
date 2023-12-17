a4a2b7f24f3105699e7527ed90afa72a4fb1256f
zoodogood <zoodogood@gmail.com>
2023-12-17 08:48:52 +0200

    Clean a code; Alterfix

--- src/server/api/errors/file.js
+++ src/server/api/errors/file.js
@@ -1,26 +1,25 @@
 const PREFIX = "/errors/files/:name";
-import ErrorsHandler from '#lib/modules/ErrorsHandler.js';
-import { BaseRoute } from '#server/router.js';
-
+import ErrorsHandler from "#lib/modules/ErrorsHandler.js";
+import { BaseRoute } from "#server/router.js";
 
 class Route extends BaseRoute {
-	prefix = PREFIX;
+  prefix = PREFIX;
 
-	constructor(express){
-		super();
-	}
+  constructor(express) {
+    super();
+  }
 
-	async get(request, response){
-		const fileName = request.params.name;
-		const json = await ErrorsHandler.Audit.readFile(fileName);
-		if (!json){
-			response.sendStatus(404);
-			return;
-		}
-		
-		const list = JSON.parse(json);
-		response.json(list);
-	}
+  async get(request, response) {
+    const fileName = request.params.name;
+    const json = await ErrorsHandler.File.readFile(fileName);
+    if (!json) {
+      response.sendStatus(404);
+      return;
+    }
+
+    const list = JSON.parse(json);
+    response.json(list);
+  }
 }
 
 export default Route;
