c993ce95424394bd4e9b4f3ec1cbebe13e4da975
zoodogood <zoodogood@gmail.com>
2023-12-19 14:08:27 +0200

    Import constants/globals

--- src/app/constants/mod.js
+++ src/app/constants/mod.js
@@ -1,9 +1,11 @@
 import * as users from "./users/mod.js";
 import * as enums from "./enums/mod.js";
+import * as globals from "./globals/mod.js";
 import { Schema } from "./Schema.js";
 const Constants = {
   users,
   enums,
+  globals,
   Schema,
 };
 
