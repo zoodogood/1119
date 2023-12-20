96e3c540f38ecb01ab828f22de4cd2eac464ae46
zoodogood <zoodogood@gmail.com>
2023-12-20 09:53:48 +0200

    Clean a code

--- src/bot/client.js
+++ src/bot/client.js
@@ -1,11 +1,10 @@
-import { Partials, GatewayIntentBits } from 'discord.js';
-import { Client } from 'discord.js';
-
+import { Partials, GatewayIntentBits } from "discord.js";
+import { Client } from "discord.js";
 
 const client = new Client({
-	messageCacheMaxSize: 110,
-	intents: [...Object.values(GatewayIntentBits)],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+  messageCacheMaxSize: 110,
+  intents: [...Object.values(GatewayIntentBits)],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 export default client;
