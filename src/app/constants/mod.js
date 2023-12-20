84ecd1a42b559136d101389f29aba7ec5f640c6a
zoodogood <zoodogood@gmail.com>
2023-12-20 08:51:38 +0200

    Add Emojis

--- src/app/constants/mod.js
+++ src/app/constants/mod.js
@@ -2,11 +2,13 @@ import * as users from "./users/mod.js";
 import * as enums from "./enums/mod.js";
 import * as globals from "./globals/mod.js";
 import { Schema } from "./Schema.js";
+import { Emoji } from "./emojis.js";
 const Constants = {
   users,
   enums,
   globals,
   Schema,
+  Emoji,
 };
 
 export { Constants };
