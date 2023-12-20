199da61922dbb3597efe5762d8cfbdce31494c1c
zoodogood <zoodogood@gmail.com>
2023-12-20 09:35:34 +0200

    Refact

--- folder/events/TimeEvents/new-day.js
+++ folder/events/TimeEvents/new-day.js
@@ -8,6 +8,8 @@ import {
 } from "#lib/modules/mod.js";
 import { dayjs } from "#lib/util.js";
 import { PropertiesEnum } from "#lib/modules/Properties.js";
+import { NEW_YEAR_DAY_DATE } from "#constants/globals/time.js";
+import config from "#config";
 
 class DailyAudit {
   static AUDIT_LINIT_IN_DAYS = 365;
@@ -60,26 +62,150 @@ class DailyAudit {
 }
 
 class Event {
+  static setTheClock() {
+    const Data = DataManager.data;
+    const today = Util.toDayDate(Date.now());
+    const currentDay = Util.timestampDay(Date.now());
+    Data.bot.dayDate = today;
+    Data.bot.currentDay = currentDay;
+    return { today, currentDay };
+  }
+
+  static calculateTimeForNextCall() {
+    return dayjs().endOf("date").add(1, "second") - Date.now();
+  }
+
+  static createNextCall() {
+    const next = this.calculateTimeForNextCall();
+    TimeEventsManager.create("new-day", next);
+  }
+
   todayIsAlreadyLaunched() {
     return DataManager.data.bot.dayDate === Util.toDayDate(Date.now());
   }
 
   async run(isLost) {
-    const Data = DataManager.data;
     if (this.todayIsAlreadyLaunched()) {
       return;
     }
-
-    const next = dayjs().endOf("date").add(1, "second") - Date.now();
-    TimeEventsManager.create("new-day", next);
+    const context = {
+      isLost,
+      today: null,
+      currentDay: null,
+    };
 
     DailyAudit.update();
+    Object.assign(context, this.setTheClock());
+    DailyEvents.updateGrempenProducts();
+    DailyEvents.updateBerrysPrice();
+    await DailyEvents.checkDayStatsEvent();
 
-    const today = Util.toDayDate(Date.now());
-    Data.bot.dayDate = today;
-    Data.bot.currentDay = Util.timestampDay(Date.now());
+    if (isLost) {
+      return;
+    }
 
-    Data.bot.grempenItems = "";
+    DailyEvents.distributePresents(context);
+    DailyEvents.askTheBoss();
+    DailyEvents.checkBirthDays();
+    DailyEvents.updateCommandsLaunchedData();
+  }
+
+  options = {
+    name: "TimeEvent/new-day",
+  };
+}
+
+class DailyEvents {
+  static updateCommandsLaunchedData() {
+    const { guilds, bot } = DataManager.data;
+    for (const guildData of guilds) {
+      const total = Object.values(guildData.commandsUsed).reduce(
+        Util.factorySummarize,
+        0,
+      );
+      guildData.commandsLaunched = total;
+    }
+
+    const total = Object.values(bot.commandsUsed).reduce(
+      Util.factorySummarize,
+      0,
+    );
+    bot.commandsLaunched = total;
+  }
+  static async checkDayStatsEvent() {
+    const botData = DataManager.data.bot;
+    const dayStatsEventIsExists = TimeEventsManager.findEventInRange(
+      ({ name }) => name === "day-stats",
+      [botData.currentDay, botData.currentDay + 1],
+    );
+    if (!dayStatsEventIsExists) {
+      await EventsManager.collection.get("TimeEvent/day-stats").run(true);
+    }
+  }
+
+  static checkBirthDays({ today }) {
+    const birthdaysToday = client.users.cache.filter(
+      (memb) => !memb.bot && memb.data.BDay === today,
+    ).length;
+
+    if (!birthdaysToday) {
+      return;
+    }
+    client.channels.cache.get(config.guild.logChannelId).msg({
+      content: `Сегодня день рождения у ${birthdaysToday} пользователя(ей)`,
+    });
+  }
+
+  static askTheBoss() {
+    client.guilds.cache.each((guild) => BossManager.bossApparance(guild));
+  }
+
+  static updateBerrysPrice() {
+    const botData = DataManager.data.bot;
+    const berryRandom = [
+      { _weight: 10, price: 1 },
+      { _weight: 1, price: -7 },
+      { _weight: 5, price: 3 },
+    ].random({ weights: true }).price;
+    const berryTarget = Math.sqrt(client.users.cache.size / 3) * 7 + 200;
+    botData.berrysPrice += Math.round(
+      (berryTarget - botData.berrysPrice) / 30 + berryRandom,
+    );
+  }
+
+  static distributePresents(context) {
+    if (DataManager.data.bot.dayDate !== NEW_YEAR_DAY_DATE) {
+      return;
+    }
+
+    const usersCache = client.users.cache;
+
+    for (const user of usersCache.values()) {
+      Util.addResource({
+        user,
+        value: 1,
+        executor: null,
+        source: "timeEventsManager.event.new-day.happySnowy",
+        resource: PropertiesEnum.presents,
+        context,
+      });
+    }
+
+    const users = DataManager.data.users;
+    client.channels.cache.get(config.guild.logChannelId).msg({
+      content: `${Util.ending(
+        usersCache.size,
+        "пользовател",
+        "ей получило",
+        "ь получил",
+        "ля получило",
+      )} подарки! из ${users.length} возможных*`,
+    });
+  }
+
+  static updateGrempenProducts() {
+    const botData = DataManager.data.bot;
+    botData.grempenItems = "";
     const arr = [
       "0",
       "1",
@@ -98,74 +224,9 @@ class Event {
       "e",
     ]; //0123456789abcdef
     for (let i = 1; i < 7; i++) {
-      DataManager.data.bot.grempenItems += arr.random({ pop: true });
-    }
-
-    const berryRandom = [
-      { _weight: 10, price: 1 },
-      { _weight: 1, price: -7 },
-      { _weight: 5, price: 3 },
-    ].random({ weights: true }).price;
-    const berryTarget = Math.sqrt(client.users.cache.size / 3) * 7 + 200;
-    Data.bot.berrysPrice += Math.round(
-      (berryTarget - Data.bot.berrysPrice) / 30 + berryRandom,
-    );
-
-    if (isLost) {
-      return;
-    }
-
-    client.guilds.cache.each((guild) => BossManager.bossApparance(guild));
-
-    const birthdaysToday = client.users.cache.filter(
-      (memb) => !memb.bot && memb.data.BDay === today,
-    ).length;
-
-    if (birthdaysToday) {
-      console.info(
-        `Сегодня день рождения у ${birthdaysToday} пользователя(ей)`,
-      );
-    }
-
-    Data.guilds.forEach(
-      (e) =>
-        (e.commandsLaunched = Object.values(e.commandsUsed).reduce(
-          (acc, e) => acc + e,
-          0,
-        )),
-    );
-
-    const commandsLaunched = Object.values(Data.bot.commandsUsed).reduce(
-      (acc, e) => acc + e,
-      0,
-    );
-    Data.bot.commandsLaunched = commandsLaunched;
-
-    if (today === "31.12") {
-      for (const user of client.users.cache().values()) {
-        Util.addResource({
-          user,
-          value: 1,
-          executor: null,
-          source: "timeEventsManager.event.new-day.happySnowy",
-          resource: PropertiesEnum.presents,
-          context: { isLost },
-        });
-      }
-    }
-
-    const dayStatsEventIsExists = TimeEventsManager.findEventInRange(
-      ({ name }) => name === "day-stats",
-      [Data.bot.currentDay, Data.bot.currentDay + 1],
-    );
-    if (!dayStatsEventIsExists) {
-      await EventsManager.collection.get("TimeEvent/day-stats").run(true);
+      botData.grempenItems += arr.random({ pop: true });
     }
   }
-
-  options = {
-    name: "TimeEvent/new-day",
-  };
 }
 
 export default Event;
