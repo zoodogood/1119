d12b5bb83d64cba5d21e9431f4bf7d778f98638f
zoodogood <zoodogood@gmail.com>
2023-12-20 09:44:10 +0200

    Add resolveLocale

--- src/lib/modules/I18nManager.js
+++ src/lib/modules/I18nManager.js
@@ -1,7 +1,22 @@
-
-
 class I18nManager {
- 
+  static DEFAULT_LOCALE = "ru";
+  static resolveLocale(locale) {
+    locale ||= this.DEFAULT_LOCALE;
+    return (
+      {
+        ru: "ru",
+        "ru-ru": "ru",
+        uk: "ua",
+        ua: "ua",
+        en: "en",
+        uk_ua: "ua",
+        "ua-ua": "ua",
+        en_us: "en",
+        en_gb: "en",
+        "en-en": "en",
+      }[locale.toLowerCase()] ?? this.DEFAULT_LOCALE
+    );
+  }
 }
 
 export default I18nManager;
