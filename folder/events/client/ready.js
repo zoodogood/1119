140f4e7d8d257654d09938e4f4bd70db82e44222
zoodogood <zoodogood@gmail.com>
2023-12-21 15:27:54 +0200

    Improve; Add code from event/start

--- folder/events/client/ready.js
+++ folder/events/client/ready.js
@@ -1,7 +1,11 @@
-import { BaseEvent } from "#lib/modules/EventsManager.js";
-import { CounterManager } from "#lib/modules/mod.js";
+import EventsManager, { BaseEvent } from "#lib/modules/EventsManager.js";
+import {
+  CounterManager,
+  DataManager,
+  TimeEventsManager,
+} from "#lib/modules/mod.js";
 import { client } from "#bot/client.js";
-import { ReadPackageJson } from "#lib/util.js";
+import { ReadPackageJson, timestampDay } from "#lib/util.js";
 import app from "#app";
 
 class Event extends BaseEvent {
@@ -10,13 +14,22 @@ class Event extends BaseEvent {
     super(client, EVENT);
   }
 
-  async run() {
-    CounterManager.handle();
+  async postLoading() {
     app.version = (await ReadPackageJson()).version;
-
     const { default: server } = await import("#server/start.js");
     app.server = server;
+    CounterManager.handle();
+    TimeEventsManager.handle();
 
+    const needUpdate =
+      DataManager.data.bot.currentDay !== timestampDay(Date.now());
+    if (needUpdate) {
+      await EventsManager.collection.get("TimeEvent/new-day").run(true);
+    }
+  }
+
+  async run() {
+    await this.postLoading();
     console.info("\n\n\n     Ready...\n\n");
 
     if (process.env.IN_CONTAINER) {
