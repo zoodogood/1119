a2af73cc7d011a30414e97b9eae1ef6afa96e475
zoodogood <zoodogood@gmail.com>
2023-12-21 15:46:51 +0200

    Alterfix

--- folder/commands/bot.js
+++ folder/commands/bot.js
@@ -81,9 +81,9 @@ class Command {
       description: `${contents.ping} ${contents.version} ${contents.season}, что сюда ещё запихнуть?\n${contents.guilds}(?) ${contents.commands}\n${contents.performance}\n${contents.time}${contents.address}\n${contents.errors};\n${contents.uniqueErrors}`,
       footer: {
         text: `Укушу! Прошло времени с момента добавления бота на новый сервер: ${
-          DataManager.data.bot.newGuildTimestamp
+          DataManager.data.bot.addToNewGuildAt
             ? timestampToDate(
-                Date.now() - DataManager.data.bot.newGuildTimestamp,
+                Date.now() - DataManager.data.bot.addToNewGuildAt,
                 2,
               )
             : "Вечность"
