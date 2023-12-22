633a2134bd850b6b1d1b097d2720809a99c0e971
zoodogood <zoodogood@gmail.com>
2023-12-22 15:08:04 +0200

    Working on a module

--- src/lib/modules/I18nManager.js
+++ src/lib/modules/I18nManager.js
@@ -1,4 +1,25 @@
 class I18nManager {
+  f(params) {
+    return this.format(...params);
+  }
+
+  format(key, { locale, values }) {
+    locale = I18nManager.resolveLocale(locale);
+    const string =
+      this.getRaw(key, locale) || this.getRaw(key, I18nManager.DEFAULT_LOCALE);
+
+    if (!string) {
+      throw new Error(`Cannot find ${key}`);
+    }
+
+    return string;
+  }
+
+  getRaw(key, locale) {
+    // to-do: developer crunch
+    return `${key} | ${locale}, not implemented`;
+  }
+
   static DEFAULT_LOCALE = "ru";
   static resolveLocale(locale) {
     locale ||= this.DEFAULT_LOCALE;
