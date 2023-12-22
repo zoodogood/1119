c0bfef2ed22bb1c9022d988d9ea4564eccb608a9
zoodogood <zoodogood@gmail.com>
2023-12-22 18:25:38 +0200

    Improve; Add capitalize

--- src/lib/mini.js
+++ src/lib/mini.js
@@ -2,11 +2,25 @@ export function factorySummarizeBy(property) {
   return (acc, current) => acc + current[property] ?? 0;
 }
 
-export function factoryGetPropertyValue(property) {
-  return (x) => x[property];
+export function factorySummarize() {
+  return (acc, current) => acc + current;
+}
+
+export function factoryGetPropertyValue(...targets) {
+  return (x) => {
+    let base = x;
+    for (const property of targets) {
+      base = base[property];
+    }
+    return base;
+  };
 }
 
 export function mapGetOrInsert(map, key, defaults) {
   !map.has(key) && map.set(key, defaults);
   return map.get(key);
+}
+
+export function capitalize(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
 }
