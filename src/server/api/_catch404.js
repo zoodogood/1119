119e579da90bb4c3f6904009f74a952f6fb0ddc0
zoodogood <zoodogood@gmail.com>
2023-12-22 18:49:44 +0200

    Clean a code

--- src/server/api/_catch404.js
+++ src/server/api/_catch404.js
@@ -1,25 +1,24 @@
-import ErrorsHandler from "#src/lib/modules/ErrorsHandler.js";
+
 import { sleep } from "#lib/util.js";
 import { BaseRoute } from "#server/router.js";
 
-
 class Route extends BaseRoute {
-	constructor(express){
-		super();
+  constructor(express) {
+    super();
 
-		this.start();
-		this.express = express;
-	}
+    this.start();
+    this.express = express;
+  }
 
-	async catch(request, response){
-		response.status(404);
-		response.send("404");
-	}
+  async catch(request, response) {
+    response.status(404);
+    response.send("404");
+  }
 
-	async start(){
-		await sleep(1);
-		this.express.use(this.catch);
-	}
+  async start() {
+    await sleep(1);
+    this.express.use(this.catch);
+  }
 }
 
 export default Route;
