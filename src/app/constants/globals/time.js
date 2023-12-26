95d0b2f428da934b640a00a9baef8e0b2f1755b7
zoodogood <zoodogood@gmail.com>
2023-12-26 21:15:20 +0200

    Add constants

--- src/app/constants/globals/time.js
+++ src/app/constants/globals/time.js
@@ -2,4 +2,7 @@ export const SECOND = 1_000;
 export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
 export const DAY = HOUR * 24;
+export const WEEK = DAY * 7;
+export const MONTH = DAY * 30;
+export const YEAR = DAY * 365;
 export const NEW_YEAR_DAY_DATE = "31.12";
