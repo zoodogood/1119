69ca3338c91a549b4289da4bf4872aaf3bdcd1de
zoodogood <zoodogood@gmail.com>
2024-01-07 07:59:44 +0200

    Clean a code

--- folder/events/TimeEvents/day-stats.js
+++ folder/events/TimeEvents/day-stats.js
@@ -1,49 +1,46 @@
-import {TimeEventsManager, BossManager} from "#lib/modules/mod.js";
+import { TimeEventsManager, BossManager } from "#lib/modules/mod.js";
 import { client } from "#bot/client.js";
-
 
 import TreeCommand from "#folder/commands/seed.js";
 import BankCommand from "#folder/commands/bank.js";
 import { ending, NumberFormatLetterize } from "#src/lib/util.js";
 
 class Event {
-  run(isLost){
-    let next = new Date(Date.now() + 14500000).setHours(20, 0, 0) - Date.now();
-    if (isLost){
+  run(isLost) {
+    const next =
+      new Date(Date.now() + 14500000).setHours(20, 0, 0) - Date.now();
+    if (isLost) {
       return TimeEventsManager.create("day-stats", next);
     }
 
     const context = {
       treeCommand: new TreeCommand(),
       bankCommand: new BankCommand(),
-      guilds: {}
+      guilds: {},
     };
 
     client.guilds.cache
-      .filter(guild => guild.data.treeLevel)
-      .each(guild => context.treeCommand.onDayStats(guild, context));
+      .filter((guild) => guild.data.treeLevel)
+      .each((guild) => context.treeCommand.onDayStats(guild, context));
 
-    client.guilds.cache.forEach(guild => {
+    client.guilds.cache.forEach((guild) => {
       this.sendStats(guild, context);
     });
 
-
-
-    client.guilds.cache.filter(guild => guild.data.professions)
-      .each(guild => {
+    client.guilds.cache
+      .filter((guild) => guild.data.professions)
+      .each((guild) => {
         context.bankCommand.onDayStats(guild, context);
       });
 
-    client.guilds.cache
-      .each((guild) => BossManager.beforeApparance(guild));
-
+    client.guilds.cache.each((guild) => BossManager.beforeApparance(guild));
 
     return TimeEventsManager.create("day-stats", next);
   }
 
-  sendStats(guild, context){
-    let data = guild.data;
-    let msgs = data.day_msg || 0;
+  sendStats(guild, context) {
+    const data = guild.data;
+    const msgs = data.day_msg || 0;
 
     const misstake = context.guilds[guild.id]?.messagesNeed;
 
@@ -52,34 +49,54 @@ class Event {
     data.days = data.days + 1 || 1;
     data.msg_total = data.msg_total + msgs || msgs;
 
-
-    let description = `За этот день было отправлено ${ ending(msgs, "сообщени", "й", "е", "я") }\nРекордное количество: ${data.day_max || (data.day_max = 0)}`;
+    let description = `За этот день было отправлено ${ending(
+      msgs,
+      "сообщени",
+      "й",
+      "е",
+      "я",
+    )}\nРекордное количество: ${data.day_max || (data.day_max = 0)}`;
 
     if (data.days > 3) {
-      description += `\nВсего сообщений: ${ NumberFormatLetterize(data.msg_total) }\nВ среднем за день: ${Math.round(data.msg_total / data.days)}`;
+      description += `\nВсего сообщений: ${NumberFormatLetterize(
+        data.msg_total,
+      )}\nВ среднем за день: ${Math.round(data.msg_total / data.days)}`;
     }
 
     if (data.day_max < msgs) {
       data.day_max = msgs;
-      description += `\nГильдия ${["<a:jeqery:768047102503944202>", "<a:jeqeryBlue:806176327223738409>", "<a:jeqeryPurple:806176181140848660>", "<a:jeqeryGreen:806176083757105162>", "<a:jeqeryRed:806175947447205958>", "<a:blockPink:794615199361400874>", "<a:blockAqua:794166748085223475>"].random()} установила свой рекорд по сообщениям!`;
+      description += `\nГильдия ${[
+        "<a:jeqery:768047102503944202>",
+        "<a:jeqeryBlue:806176327223738409>",
+        "<a:jeqeryPurple:806176181140848660>",
+        "<a:jeqeryGreen:806176083757105162>",
+        "<a:jeqeryRed:806175947447205958>",
+        "<a:blockPink:794615199361400874>",
+        "<a:blockAqua:794166748085223475>",
+      ].random()} установила свой рекорд по сообщениям!`;
     }
-
 
     data.day_msg = 0;
 
-    if (!msgs){
+    if (!msgs) {
       return;
       // description = ["Сегодня не было отправленно ни одно сообщение", "Сегодня на сервере пусто", "За целый день ни один смертный не проявил активность", "Похоже, тишина — второе имя этого сервера"].random();
     }
 
     if (misstake)
-      description += `\n\nДерево засыхает! Ему необходимо на ${ ending(misstake - msgs, "сообщени", "й", "е", "я") } больше 💧`;
+      description += `\n\nДерево засыхает! Ему необходимо на ${ending(
+        misstake - msgs,
+        "сообщени",
+        "й",
+        "е",
+        "я",
+      )} больше 💧`;
 
     guild.chatSend({ title: "Статистика сервера", description });
   }
 
   options = {
-    name: "TimeEvent/day-stats"
+    name: "TimeEvent/day-stats",
   };
 }
 
