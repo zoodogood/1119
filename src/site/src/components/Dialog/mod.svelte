90e9cc8a249a020763fbc300447c5303a75f6d74
zoodogood <zoodogood@gmail.com>
2024-01-13 08:51:44 +0200

    Clean a code

--- src/site/src/components/Dialog/mod.svelte
+++ src/site/src/components/Dialog/mod.svelte
@@ -1,194 +1,168 @@
+<script>
+  import { onDestroy, createEventDispatcher } from "svelte";
+  import Icon from "#site-component/iconic";
+  import { get_current_component as getCurrentComponent } from "svelte/internal";
 
+  export let useClassic;
+  export let title;
+  export let description;
+  export let isHTMLAccepted;
+  export let preventDestroy = false;
+  export let hide = false;
 
+  const self = getCurrentComponent();
+
+  let ref;
+
+  const dispatch = createEventDispatcher();
+  const close = () => {
+    if (!ref.open) {
+      return;
+    }
+
+    ref.close();
+    history.state.isModal && history.go(-1);
+    dispatch.call(ref, "close");
+  };
+  const open = () => {
+    if (ref.open) {
+      return;
+    }
+
+    ref.showModal();
+    globalThis.history.pushState({ isModal: true }, "Open modal");
+    addEventListener("popstate", close);
+  };
+  const destroy = () => !preventDestroy && self.$destroy();
+
+  $: if (ref && !hide) open();
+
+  onDestroy(() => {
+    removeEventListener("popstate", close);
+  });
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <dialog
-	bind:this = { ref }
-	on:click = { close }
-	on:close = { destroy }
-	on:keydown = { ({key}) => key === "Esc" && close() }
+  bind:this={ref}
+  on:click={close}
+  on:close={destroy}
+  on:keydown={({ key }) => key === "Esc" && close()}
 >
-	<main on:click|stopPropagation on:keydown|stopPropagation>
-		{#if useClassic}
-			<article class = "classic-dialog">
-				<p>{ title }</p>
-				<span>
-					{#if isHTMLAccepted}
-						{@html description}
-					{:else}
-						{ description }
-					{/if}
-				</span>
-				<button on:click = { close }><Icon code = ""/> или кликните вне диалогового окна</button>
-			</article>
-		{/if}
-		<slot/>
-	</main>
+  <main on:click|stopPropagation on:keydown|stopPropagation>
+    {#if useClassic}
+      <article class="classic-dialog">
+        <p>{title}</p>
+        <span>
+          {#if isHTMLAccepted}
+            {@html description}
+          {:else}
+            {description}
+          {/if}
+        </span>
+        <button on:click={close}
+          ><Icon code="" /> или кликните вне диалогового окна</button
+        >
+      </article>
+    {/if}
+    <slot />
+  </main>
 </dialog>
 
-
-
-
 <style>
-	dialog::backdrop
-	{
-		 background-color: #0009;
-		 backdrop-filter: blur(2px) saturate(5%);
-		 cursor: zoom-out;
-	}
+  dialog::backdrop {
+    background-color: #0009;
+    backdrop-filter: blur(2px) saturate(5%);
+    cursor: zoom-out;
+  }
 
-	dialog
-	{
-		 top: 50%;
-		 left: 50%;
-		 transform: translate(-50%, -50%);
-		 max-width: 85vw;
-		 max-height: 80vh;
+  dialog {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 85vw;
+    max-height: 80vh;
 
+    background-color: transparent;
+    border: none;
 
-		 background-color: transparent;
-		 border: none;
+    overflow: auto;
+    animation: apparance 1s;
+    box-shadow: 0px 3px 30px -15px var(--main-color);
+  }
 
-		 overflow: auto;
-		 animation: apparance 1s;
-		 box-shadow: 0px 3px 30px -15px var( --main-color );
-	}
+  @keyframes apparance {
+    0% {
+      opacity: 0.5;
+      transform: translate(-50%, -100vh);
+    }
 
-	@keyframes apparance
-	{
-		0% {
-			opacity: 0.5;
-			transform: translate(-50%, -100vh);
-		}
+    100% {
+      opacity: 1;
+      transform: translate(-50%, -50%);
+    }
+  }
 
-		100% {
-			opacity: 1;
-			transform: translate(-50%, -50%);
-		}
-	}
+  :global(html):has(dialog) {
+    overflow: hidden;
+  }
 
-	:global(html):has(dialog)
-	{
-		overflow: hidden;
-	}
+  .classic-dialog {
+    background-color: var(--background-theme-accent);
+    padding: calc(10px + 1vw);
+    border-radius: 15px;
+    min-width: 400px;
 
-	
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
 
-	.classic-dialog
-	{
-		background-color: var( --background-theme-accent );
-		padding: calc(10px + 1vw);
-		border-radius: 15px;
-		min-width: 400px;
+    color: var(--text-theme-accent);
+    border: 1px solid var(--main-color);
 
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 1em;
+    white-space: pre-wrap;
+    overflow: auto;
+    overflow: overlay;
+  }
 
-		color: var( --text-theme-accent );
-		border: 1px solid var( --main-color );		
+  @media (max-width: 980px) {
+    dialog {
+      max-width: 95vw;
+    }
 
-		white-space: pre-wrap;
-		overflow: auto;
-		overflow: overlay;
-	}
+    .classic-dialog {
+      width: 95vw;
+      min-width: 0;
+      padding: 10px;
+    }
 
-	@media (max-width: 980px){
-		dialog
-		{
-			max-width: 95vw;
-		}
+    .classic-dialog span {
+      padding: 0.5em;
+    }
+  }
 
-		.classic-dialog
-		{
-			width: 95vw;
-			min-width: 0;
-			padding: 10px;
-		}
+  .classic-dialog p {
+    font-family: "PTMono", sans-serif;
+    text-transform: uppercase;
+    opacity: 0.8;
+    font-size: 0.7em;
+  }
 
-		.classic-dialog span
-		{
-			padding: 0.5em;
-		}
-	}
+  .classic-dialog span {
+    position: relative;
+    padding: calc(3px + 0.3vw);
+    align-self: baseline;
+    max-width: 100%;
+  }
 
+  .classic-dialog::before {
+    content: "";
+    display: block;
+    height: 2px;
+    width: 70%;
 
-
-	.classic-dialog p 
-	{
-		font-family: 'PTMono', sans-serif;
-		text-transform: uppercase;
-		opacity: 0.8;
-		font-size: 0.7em;
-	}
-
-	.classic-dialog span
-	{
-		position: relative;
-		padding: calc(3px + 0.3vw);
-		align-self: baseline;
-		max-width: 100%;
-	}
-
-	
-
-	.classic-dialog::before
-	{
-		content: '';
-		display: block;
-		height: 2px;
-		width: 70%;
-
-		background-color: var( --main-color );
-		opacity: 0.2;
-	}
+    background-color: var(--main-color);
+    opacity: 0.2;
+  }
 </style>
-
-
-
-
-
-<script>
-	import { onDestroy, createEventDispatcher } from "svelte";
-	import Icon from '#site-component/iconic';
-  	import { get_current_component as getCurrentComponent } from "svelte/internal";
-
-	export let useClassic;
-	export let title;
-	export let description;
-	export let isHTMLAccepted;
-	export let preventDestroy = false;
-	export let hide = false;
-
-	const self = getCurrentComponent();
-	
-	let ref;
-	
-
-	const dispatch = createEventDispatcher();
-	const close = () => {
-		if (!ref.open){
-			return;
-		}
-
-		ref.close();
-		history.state.isModal && history.go(-1);
-		dispatch.call(ref, "close");
-	};
-	const open = () => {
-		if (ref.open){
-			return;
-		}
-
-		ref.showModal();
-		globalThis.history.pushState({isModal: true}, "Open modal");
-		addEventListener("popstate", close);
-	};
-	const destroy = () => !preventDestroy && self.$destroy();
-
-	$: 
-		if (ref && !hide) open();
-
-	onDestroy(() => {
-		removeEventListener("popstate", close);
-	})
-</script>
