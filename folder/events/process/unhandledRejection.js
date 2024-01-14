873daa46138c9ee8a929b2f877d2162f59d8c046
zoodogood <zoodogood@gmail.com>
2024-01-14 11:09:33 +0200

    Don't show some errors

--- folder/events/process/unhandledRejection.js
+++ folder/events/process/unhandledRejection.js
@@ -8,15 +8,16 @@ class Event extends BaseEvent {
   }
 
   async run(error) {
-    ErrorsHandler.onErrorReceive(error, { uncaughtException: true });
-
     const ignoreMessages = [
       "Cannot execute action on a DM channel",
       "Unknown Message",
+      "Missing Permissions",
     ];
     if (ignoreMessages.includes(error.message)) {
       return;
     }
+
+    ErrorsHandler.onErrorReceive(error, { uncaughtException: true });
   }
 
   options = {
