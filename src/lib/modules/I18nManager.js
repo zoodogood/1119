1f06d3c8c0adb109250e9f2451f43b0e3ae07177
zoodogood <zoodogood@gmail.com>
2024-01-17 22:12:21 +0200

    Read i18n.jsonc file

--- src/lib/modules/I18nManager.js
+++ src/lib/modules/I18nManager.js
@@ -1,4 +1,18 @@
+import StorageManager from "#lib/modules/StorageManager.js";
+import JSON5 from "json5";
+class StorageUtils {
+  static async readLocales() {
+    return JSON5.parse(
+      (await StorageManager.read("i18n.jsonc")) || `{"ru": {}}`,
+    );
+  }
+}
 class I18nManager {
+  async load() {
+    this.data = await StorageUtils.readLocales();
+    console.log(this.data);
+  }
+
   f(...params) {
     return this.format(...params);
   }
@@ -16,8 +30,7 @@ class I18nManager {
   }
 
   getRaw(key, locale) {
-    // to-do: developer crunch
-    return `${key} | ${locale}, not implemented`;
+    return this.data[locale]?.[key];
   }
 
   static DEFAULT_LOCALE = "ru";
@@ -40,4 +53,5 @@ class I18nManager {
   }
 }
 
+export { I18nManager };
 export default I18nManager;
