7f4d2fb50089f2fc8141f5c4731393aa4a62e532
zoodogood <zoodogood@gmail.com>
2024-01-31 20:37:41 +0200

    Clean a code

--- folder/events/TimeEvents/clover-end.js
+++ folder/events/TimeEvents/clover-end.js
@@ -1,30 +1,33 @@
-import { client } from '#bot/client.js';
+import { client } from "#bot/client.js";
+import { CALCULATE_CLOVER_MULTIPLAYER } from "#constants/users/commands.js";
 
 class Event {
-	/**
-	 * @param {boolean} isLost
-	 * @param {string} guildId
-	 * @param {string} channelId
-	 */
-	async run(isLost, guildId, channelId){
-		const guild = client.guilds.cache.get(guildId);
-   	if (!guild){
-     		 return;
-    	}
-		const channel = guild.channels.cache.get(channelId);
-		const effect = guild.data.cloverEffect;
-		delete guild.data.cloverEffect;
+  /**
+   * @param {boolean} isLost
+   * @param {string} guildId
+   * @param {string} channelId
+   */
+  async run(isLost, guildId, channelId) {
+    const guild = client.guilds.cache.get(guildId);
+    if (!guild) {
+      return;
+    }
+    const channel = guild.channels.cache.get(channelId);
+    const { cloverEffect } = guild.data;
+    delete guild.data.cloverEffect;
 
-		
-		const multiplier = 1.08 + (0.07 * ((1 - 0.9242 ** effect.uses) / (1 - 0.9242)));
-		channel?.msg({title: "☘️ Ивент Клевера завершился", color: "#21c96c", description: `Получено наград во время действия эффекта: ${effect.coins}\nМаксимальный множитель: X${multiplier.toFixed(2)}\nКуплено клеверов: ${effect.uses}\nКлевер длился ${((Date.now() - effect.timestamp) / 3600000).toFixed(1)}ч.`});	
-	}
+    const multiplier = CALCULATE_CLOVER_MULTIPLAYER(cloverEffect.uses);
 
-	
+    channel?.msg({
+      title: "☘️ Ивент Клевера завершился",
+      color: "#21c96c",
+      description: `Получено наград во время действия эффекта: ${cloverEffect.coins}\nМаксимальный множитель: X${multiplier.toFixed(2)}\nКуплено клеверов: ${cloverEffect.uses}\nКлевер длился ${((Date.now() - cloverEffect.timestamp) / 3600000).toFixed(1)}ч.`,
+    });
+  }
 
-	options = {
-		name: "TimeEvent/clover-end"
-	}
+  options = {
+    name: "TimeEvent/clover-end",
+  };
 }
 
 export default Event;
