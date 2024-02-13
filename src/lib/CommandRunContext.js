b20c6f0a0429417f8c404d6df1afffcca1a08a65
zoodogood <zoodogood@gmail.com>
2024-02-13 07:58:28 +0200

    Improve

--- src/lib/CommandRunContext.js
+++ src/lib/CommandRunContext.js
@@ -2,13 +2,26 @@
 import EventsManager from "#lib/modules/EventsManager.js";
 
 class BaseCommandRunContext {
+  command = null;
+  interaction = null;
   emitter = new EventsManager();
+
+  setCliParsed(parsed, values) {
+    this.cliParsed = [parsed, values];
+  }
+  getCliParsed() {
+    return this.cliParsed || [null, null];
+  }
+  cliParsed = null;
 
   end() {
     this.isEnded = true;
   }
+  isEnded = false;
 
-  static new(interaction, command) {}
+  static async new(interaction, command) {
+    return new this(interaction, command);
+  }
   constructor(interaction, command) {
     this.interaction = interaction;
     this.command = command;
