da7c663e8cf718576abf87c940a36c6bb21e1cc8
zoodogood <zoodogood@gmail.com>
2024-02-26 21:07:39 +0200

    Alterfix

--- folder/events/TimeEvents/postpone.js
+++ folder/events/TimeEvents/postpone.js
@@ -1,25 +1,28 @@
-import { client } from '#bot/client.js';
-
+import { client } from "#bot/client.js";
 
 class Event {
-	async run(isLost, authorId, channelId, content){
-		if (isLost) client.users.cache.get(authorId).msg({title: "Ваше сообщение не было доставлено вовремя", description: content});
-		const channel = client.channels.cache.get(channelId);
+  async run(timeEventData, authorId, channelId, content) {
+    if (timeEventData.isLost)
+      client.users.cache.get(authorId).msg({
+        title: "Ваше сообщение не было доставлено вовремя",
+        description: content,
+      });
+    const channel = client.channels.cache.get(channelId);
 
-		if (!channel){
-			return;
-		}
-		const author = channel.guild.members.cache.get(authorId);
-		const webhook = await channel.createWebhook(author.displayName, {avatar: author.user.avatarURL()});
-		await webhook.msg({content});
-		webhook.delete();
-	}
+    if (!channel) {
+      return;
+    }
+    const author = channel.guild.members.cache.get(authorId);
+    const webhook = await channel.createWebhook(author.displayName, {
+      avatar: author.user.avatarURL(),
+    });
+    await webhook.msg({ content });
+    webhook.delete();
+  }
 
-	
-
-	options = {
-		name: "TimeEvent/postpone"
-	}
+  options = {
+    name: "TimeEvent/postpone",
+  };
 }
 
 export default Event;
