b32a7d85d7c1bdb7ea54a1f5f4145e8cee5ae7c6
zoodogood <zoodogood@gmail.com>
2024-03-18 20:59:43 +0200

    Add toObject method

--- src/lib/dayjs.js
+++ src/lib/dayjs.js
@@ -1,4 +1,6 @@
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration.js";
+import toObject from "dayjs/plugin/toObject.js";
 dayjs.extend(duration);
+dayjs.extend(toObject);
 export { dayjs };
