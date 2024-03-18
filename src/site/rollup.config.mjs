09056ad2f44884506e34ccc5c9d2c8d8be417870
zoodogood <zoodogood@gmail.com>
2024-03-18 20:59:07 +0200

    Fix

--- src/site/rollup.config.mjs
+++ src/site/rollup.config.mjs
@@ -1,7 +1,7 @@
 import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import livereload from "rollup-plugin-livereload";
 import css from "rollup-plugin-css-only";
 import nodePolyfills from "rollup-plugin-polyfill-node";
