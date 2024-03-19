df207e8f6121a3bde477d796f8b74d7f52bc3def
zoodogood <zoodogood@gmail.com>
2024-03-19 21:11:50 +0200

    Alterfix

--- src/server/mod.js
+++ src/server/mod.js
@@ -1,77 +1,70 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import config from '#config';
-import express from './express.js';
-import FileSystem from 'fs/promises';
-import { setMiddleware } from './middleware.js';
+import config from "#config";
+import express from "./express.js";
+import FileSystem from "fs/promises";
+import { setMiddleware } from "./middleware.js";
 
+import { checkPort, getAddress } from "./util.js";
+import { sleep } from "#lib/util.js";
 
-import { checkPort, getAddress } from './util.js';
-import { sleep } from '#lib/util.js';
+const SSLSecret =
+  config.server.hasSSLCertificate &&
+  config.server.isAvailable &&
+  (
+    await Promise.all([
+      FileSystem.readFile("./folder/SSLSecret/privkey.pem"),
+      FileSystem.readFile("./folder/SSLSecret/cert.pem"),
+    ])
+  ).map(String);
 
-const SSLSecret = config.server.hasSSLCertificate && config.server.isAvailable
-	&& (await Promise.all([
-		FileSystem.readFile("./folder/SSLSecret/private.key"),
-		FileSystem.readFile("./folder/SSLSecret/certificate.crt"),
-		FileSystem.readFile("./folder/SSLSecret/ca_bundle.crt")
-	]))
-	.map(String);
+async function raiseServer(port) {
+  while (true) {
+    const isOpen = await checkPort(port);
+    if (isOpen) {
+      break;
+    }
 
+    port++;
+  }
 
-async function raiseServer(port){
-	while (true){
-		const isOpen = await checkPort(port);
-		if (isOpen){
-			break;
-		}
+  return await new Promise(async (resolve, reject) => {
+    const HTTPBase = config.server.hasSSLCertificate
+      ? (await import("https")).default
+      : (await import("http")).default;
+    const options = {
+      port,
+      host: config.server.hostname,
 
-		port++;
-	}
+      key: SSLSecret && SSLSecret.at(0),
+      cert: SSLSecret && SSLSecret.at(1),
+    };
 
-	
-	return await new Promise(async (resolve, reject) => {
-		const HTTPBase = (config.server.hasSSLCertificate ? (await import("https")).default : (await import("http")).default);
-		const options = {
-			port,
-			host: config.server.hostname,
+    const server = HTTPBase.createServer(options, express);
+    server.listen(options, () => resolve(server));
 
-			key: SSLSecret && SSLSecret.at(0),
-  			cert: SSLSecret && SSLSecret.at(1),
-			ca: SSLSecret && SSLSecret.at(2),
-		};
-		
-		const server = HTTPBase.createServer(options, express);
-		server.listen(options, () => resolve(server));
-
-		
-		await sleep(3_000);
-		reject( new Error("TIMEOUT ERROR") );
-	})
+    await sleep(3_000);
+    reject(new Error("TIMEOUT ERROR"));
+  });
 }
 
-function logger(server){
-	const address = getAddress(server);
-	console.info(`Listen on ${ address }`);
+function logger(server) {
+  const address = getAddress(server);
+  console.info(`Listen on ${address}`);
 }
 
 export default async () => {
-	if (!config.server.isAvailable){
-		return null;
-	}
+  if (!config.server.isAvailable) {
+    return null;
+  }
 
-	
-	const { router } = await setMiddleware( express );
+  const { router } = await setMiddleware(express);
 
+  const port = config.server.port ?? 8001;
+  const server = await raiseServer(port);
 
-	const port = config.server.port ?? 8001;
-	const server = await raiseServer(port);
-	
-	server.router = router;
-	
-	logger(server);
-	return server;
-}
+  server.router = router;
 
-
-
-
+  logger(server);
+  return server;
+};
