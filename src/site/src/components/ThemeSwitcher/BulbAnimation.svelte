154e9f68e956e2ea62dfdb78c62b55b6da4acf2f
zoodogood <zoodogood@gmail.com>
2024-03-19 21:10:13 +0200

    Improve; Clean a code

--- src/site/src/components/ThemeSwitcher/BulbAnimation.svelte
+++ src/site/src/components/ThemeSwitcher/BulbAnimation.svelte
@@ -1,55 +1,52 @@
-<article bind:this = {node}></article>
-
 <script>
   import { sleep } from "#lib/safe-utils.js";
 
-
-	let node = null;
-	(async () => {
-		await sleep(1000);
-		node.parentNode.removeChild(node);
-	})();
-	
+  let node = null;
+  (async () => {
+    await sleep(1_000);
+    node.parentNode.removeChild(node);
+  })();
 </script>
 
+<article bind:this={node}></article>
+
 <style>
-	@keyframes apparance
-	{
-		0% {
-			opacity: 0.35;
-			width: 0;
-			height: 0;
-			border-radius: 50%;
-		}
+  @keyframes apparance {
+    0% {
+      opacity: 0.35;
+      width: 0;
+      height: 0;
+      border-radius: 50%;
+    }
 
-		100% {
-			opacity: 0;
-			width: 100vw;
-			height: 100vh;
-			border-radius: 0%;
-		}
-	}
+    100% {
+      opacity: 0;
+      width: 100vw;
+      height: 100vh;
+      border-radius: 0%;
+    }
+  }
 
-	article
-	{
-		position: fixed;
-		width: 100%;
-		height: 100vh;
+  article {
+    position: fixed;
+    width: 100%;
+    height: 100vh;
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		top: 0;
-		left: 0;
-	}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    pointer-events: none;
+  }
 
-	article::after
-	{
-		content: '';
-		display: block;
+  article::after {
+    content: "";
+    display: block;
 
-		border-radius: 50%;
-		background-color: var( --main-color );
-		animation: apparance 1s;
-	}
+    border-radius: 50%;
+    background-color: var(--main-color);
+    animation: apparance 1s;
+  }
 </style>
