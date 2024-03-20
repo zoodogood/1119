5b94e0d3570d86f7942ef14ead0523a0efa671aa
zoodogood <zoodogood@gmail.com>
2024-03-20 14:27:26 +0200

    Clean a code

--- src/site/src/core/index.js
+++ src/site/src/core/index.js
@@ -1,15 +1,15 @@
-import {route} from '#site/core/router.js';
-import svelteApp from './svelte-app.js';
-import PageWrapper from '#site-component/PageWrapper';
+import { route } from "#site/core/router.js";
+import svelteApp from "./svelte-app.js";
+import PageWrapper from "#site-component/PageWrapper";
 
-import app from '#app';
+import app from "#app";
 app.svelte = svelteApp;
 app.launch();
 
 const page = route(svelteApp);
 new PageWrapper({
-	target: svelteApp.document.body,
-	props: {
-		page
-	}
+  target: svelteApp.document.body,
+  props: {
+    page,
+  },
 });
