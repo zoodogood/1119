356fcba2bd27f5b17090898d15d6aa84fe5345bd
zoodogood <zoodogood@gmail.com>
2024-03-22 15:24:51 +0200

    Clean a code

--- src/site/src/core/svelte-app.js
+++ src/site/src/core/svelte-app.js
@@ -1,168 +1,173 @@
-import HashController from '#site/lib/HashController.js';
-import { parseDocumentLocate, omit, fetchFromInnerApi, ReplaceTemplate } from '#lib/safe-utils.js';
-import { createDialog } from '#site/lib/createDialog.js';
+import HashController from "#site/lib/HashController.js";
+import {
+  parseDocumentLocate,
+  omit,
+  fetchFromInnerApi,
+  ReplaceTemplate,
+} from "#lib/safe-utils.js";
+import { createDialog } from "#site/lib/createDialog.js";
 
-import { whenDocumentReadyStateIsComplete } from '#site/lib/util.js';
-import enviroment from '#site/enviroment/mod.js';
+import { whenDocumentReadyStateIsComplete } from "#site/lib/util.js";
+import enviroment from "#site/enviroment/mod.js";
 
-import PagesURLs from '#static/build/svelte-pages/enum[builded].mjs';
-import config from '#config';
-import PagesRouter from '#site/lib/Router.js';
-
+import PagesURLs from "#static/build/svelte-pages/enum[builded].mjs";
+import config from "#config";
+import PagesRouter from "#site/lib/Router.js";
 
 class StorageManager {
-	getToken(){
-		return localStorage.getItem("access_token");
-	}
+  getToken() {
+    return localStorage.getItem("access_token");
+  }
 
-	setToken(token){
-		if (token === null){
-			localStorage.removeItem("access_token");
-			return;
-		}
+  setToken(token) {
+    if (token === null) {
+      localStorage.removeItem("access_token");
+      return;
+    }
 
-		return localStorage.setItem("access_token", token);
-	}
+    return localStorage.setItem("access_token", token);
+  }
 
-	getUserData(){
-		return JSON.parse(localStorage.getItem("user") ?? null);
-	}
+  getUserData() {
+    return JSON.parse(localStorage.getItem("user") ?? null);
+  }
 
-	setUserData(user){
-		if (user === null){
-			localStorage.removeItem("user");
-			return;
-		}
+  setUserData(user) {
+    if (user === null) {
+      localStorage.removeItem("user");
+      return;
+    }
 
-		const ignoreKeysList = ["guilds"];
-		user = omit(user, (key) => !ignoreKeysList.includes(key));
-		return localStorage.setItem("user", JSON.stringify(user));
-	}
+    const ignoreKeysList = ["guilds"];
+    user = omit(user, (key) => !ignoreKeysList.includes(key));
+    return localStorage.setItem("user", JSON.stringify(user));
+  }
 
-	getSelectedLocale(){
-		return localStorage.getItem("selected_locale") ?? null;
-	}
+  getSelectedLocale() {
+    return localStorage.getItem("selected_locale") ?? null;
+  }
 
-	setLocale(locale){
-		if (locale === null){
-			localStorage.removeItem("selected_locale");
-			return;
-		}
+  setLocale(locale) {
+    if (locale === null) {
+      localStorage.removeItem("selected_locale");
+      return;
+    }
 
-		localStorage.setItem("selected_locale", locale);
-	}
+    localStorage.setItem("selected_locale", locale);
+  }
 
+  yetTokenHeat() {
+    if (sessionStorage.tokenHeat > Date.now()) {
+      return true;
+    }
 
-	yetTokenHeat(){
-		if (sessionStorage.tokenHeat > Date.now()){
-			return true;
-		}
-
-		const HEAT_DELAY = 60_000 * 5;
-		sessionStorage.tokenHeat = Date.now() + HEAT_DELAY;
-		return false;
-	}
+    const HEAT_DELAY = 60_000 * 5;
+    sessionStorage.tokenHeat = Date.now() + HEAT_DELAY;
+    return false;
+  }
 }
 
 class SvelteApp {
+  document = document;
+  Date = new Date();
+  HashData;
+  Hash = this.#createHashController();
+  enviroment = enviroment;
+  url = parseDocumentLocate(this.document.location);
+  PagesURLs = PagesURLs;
+  storage = new StorageManager();
+  user = this.storage.getUserData();
+  i18n = null;
 
-	document = document;
-	Date = new Date();
-	HashData;
-	Hash = this.#createHashController();
-	enviroment = enviroment;
-	url = parseDocumentLocate(this.document.location);
-	PagesURLs = PagesURLs;
-	storage = new StorageManager();
-	user = this.storage.getUserData();
-	i18n = null;
+  constructor() {
+    this.lang = this.url.base.lang ?? "ru";
+    this.i18n = this.enviroment.i18n[this.lang];
 
-	constructor(){
-		this.lang = this.url.base.lang ?? "ru";
-		this.i18n = this.enviroment.i18n[this.lang];
-		
-		this.#checkOrigin();
-		this.#checkExternalUserDataByToken();
-		this.#checkURLLocaleProtocol();
-		console.info(this);
-	}
+    this.#checkOrigin();
+    this.#checkExternalUserDataByToken();
+    this.#checkURLLocaleProtocol();
+    console.info(this);
+  }
 
-	get href(){
-		return this.document.location.href;
-	}
+  get href() {
+    return this.document.location.href;
+  }
 
-	getBot(){
-		const bot = this.enviroment.bot ?? {
-			id: null,
-			username: "Призрак",
-			discriminator: "1119",
-			displayAvatarURL: `${ config.server.origin }/static/favicon.ico`,
-			invite: null
-		};
+  getBot() {
+    const bot = this.enviroment.bot ?? {
+      id: null,
+      username: "Призрак",
+      discriminator: "1119",
+      displayAvatarURL: `${config.server.origin}/static/favicon.ico`,
+      invite: null,
+    };
 
-		return bot;
-	}
+    return bot;
+  }
 
-	#checkOrigin(){
-		if (config.server.origin !== this.document.location.origin){
-			throw new Error(`You need set in config server.origin equal to ${ this.document.location.origin }\nCurrent: ${ this.document.location.origin }`);
-		}
-	}
+  #checkOrigin() {
+    if (config.server.origin !== this.document.location.origin) {
+      throw new Error(
+        `You need set in config server.origin equal to ${this.document.location.origin}\nCurrent: ${this.document.location.origin}`,
+      );
+    }
+  }
 
+  #createHashController() {
+    const controller = new HashController().subscribe();
+    controller.store.subscribe(this.#onHashUpdate.bind(this));
+    return controller;
+  }
 
-	#createHashController(){
-		const controller = new HashController().subscribe();
-		controller.store.subscribe(this.#onHashUpdate.bind(this));
-		return controller;
-	}
+  #onHashUpdate(hash) {
+    const data = (this.HashData ||= { hash: {} });
+    data.currentHash = hash;
+    Object.assign(data.hash, hash);
+  }
 
-	#onHashUpdate(hash){
-		const data = (this.HashData ||= {hash: {}});
-		data.currentHash = hash;
-		Object.assign(data.hash, hash);
-	}
-	
-	async #checkExternalUserDataByToken(){
-		await whenDocumentReadyStateIsComplete(this.document);
-		
-		const token = this.storage.getToken();
-		if (!token){
-			return;
-		}
-		
-		const yet = this.storage.yetTokenHeat();
-		if (yet){
-			return;
-		}
+  async #checkExternalUserDataByToken() {
+    await whenDocumentReadyStateIsComplete(this.document);
 
+    const token = this.storage.getToken();
+    if (!token) {
+      return;
+    }
 
-		const headers = {Authorization: token};
-		const user = await fetchFromInnerApi("oauth2/user", {headers})
-			.catch(() => {});
+    const yet = this.storage.yetTokenHeat();
+    if (yet) {
+      return;
+    }
 
-		if (!user || typeof user === "string"){
-			const _key = PagesRouter.getPageBy("oauth").key;
-			const link = PagesRouter.relativeToPage(_key);
-			createDialog(svelteApp, {
-				title: this.i18n.general.app.externalTokenDialog.title,
-				description: ReplaceTemplate(this.i18n.general.app.externalTokenDialog.description, {link, _key}),
-				isHTMLAccepted: true
-			});
-			return;
-		}
+    const headers = { Authorization: token };
+    const user = await fetchFromInnerApi("oauth2/user", { headers }).catch(
+      () => {},
+    );
 
-		this.storage.setUserData(user);
-	}
+    if (!user || typeof user === "string") {
+      const _key = PagesRouter.getPageBy("oauth").key;
+      const link = PagesRouter.relativeToPage(_key);
+      createDialog(svelteApp, {
+        title: this.i18n.general.app.externalTokenDialog.title,
+        description: ReplaceTemplate(
+          this.i18n.general.app.externalTokenDialog.description,
+          { link, _key },
+        ),
+        isHTMLAccepted: true,
+      });
+      return;
+    }
 
+    this.storage.setUserData(user);
+  }
 
-	#checkURLLocaleProtocol(){
-		const locale = this.url.base.lang;
-		if (!locale){
-			return;
-		}
+  #checkURLLocaleProtocol() {
+    const locale = this.url.base.lang;
+    if (!locale) {
+      return;
+    }
 
-		this.storage.setLocale(locale);
-	}
+    this.storage.setLocale(locale);
+  }
 }
 
 const svelteApp = new SvelteApp();
