2e933cc22d62b1dfdfe3f69cbea378bb111dccfc
zoodogood <zoodogood@gmail.com>
2024-03-22 12:44:19 +0200

    Clean a code

--- src/site/src/components/ThemeSwitcher/themes.js
+++ src/site/src/components/ThemeSwitcher/themes.js
@@ -1,101 +1,101 @@
-export default new Map(Object.entries({
-	brightGreen: {
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#3b7c4c",
-		"--theme-accent-name": "dark"
-	},
+export default new Map(
+  Object.entries({
+    brightGreen: {
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--main-color": "#3b7c4c",
+      "--theme-accent-name": "dark",
+    },
 
-	darkGreen: {
-		"--main-color": "#2b5b37",
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--theme-accent-name": "dark"
-	},
+    darkGreen: {
+      "--main-color": "#2b5b37",
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--theme-accent-name": "dark",
+    },
 
-	lightGreen: {
-		"--main-color": "#2b5b37",
-		"--text-theme-accent": "var(--dark)",
-		"--background-theme-accent": "var(--white)",
-		"--theme-accent-name": "light"
-	},
+    lightGreen: {
+      "--main-color": "#2b5b37",
+      "--text-theme-accent": "var(--dark)",
+      "--background-theme-accent": "var(--white)",
+      "--theme-accent-name": "light",
+    },
 
-	brightPurple: {
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#3b37a9",
-		"--theme-accent-name": "dark"
-	},
+    brightPurple: {
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--main-color": "#3b37a9",
+      "--theme-accent-name": "dark",
+    },
 
-	darkPurple: {
-		"--main-color": "#3f3c8d",
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--theme-accent-name": "dark"
-	},
+    darkPurple: {
+      "--main-color": "#3f3c8d",
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--theme-accent-name": "dark",
+    },
 
-	lightPurple: {
-		"--main-color": "#3f3c8d",
-		"--text-theme-accent": "var(--dark)",
-		"--background-theme-accent": "var(--white)",
-		"--theme-accent-name": "light"
-	},
+    lightPurple: {
+      "--main-color": "#3f3c8d",
+      "--text-theme-accent": "var(--dark)",
+      "--background-theme-accent": "var(--white)",
+      "--theme-accent-name": "light",
+    },
 
-	brightBlue: {
-		"--main-color": "#409ff6",
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--theme-accent-name": "dark"
-	},
+    brightBlue: {
+      "--main-color": "#409ff6",
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--theme-accent-name": "dark",
+    },
 
-	darkBlue: {
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#00529d",
-		"--theme-accent-name": "dark"
-	},
+    darkBlue: {
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--main-color": "#00529d",
+      "--theme-accent-name": "dark",
+    },
 
-	lightBlue: {
-		"--main-color": "#409ff6",
-		"--text-theme-accent": "var(--dark)",
-		"--background-theme-accent": "var(--white)",
-		"--theme-accent-name": "light"
-	},
+    lightBlue: {
+      "--main-color": "#409ff6",
+      "--text-theme-accent": "var(--dark)",
+      "--background-theme-accent": "var(--white)",
+      "--theme-accent-name": "light",
+    },
 
-	brightOrange: {
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--main-color": "#ff9f64",
-		"--theme-accent-name": "dark"
-	},
+    brightOrange: {
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--main-color": "#ff9f64",
+      "--theme-accent-name": "dark",
+    },
 
-	darkOrange: {
-		"--main-color": "#e1864e",
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "var(--dark)",
-		"--theme-accent-name": "dark"
-	},
+    darkOrange: {
+      "--main-color": "#e1864e",
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "var(--dark)",
+      "--theme-accent-name": "dark",
+    },
 
-	lightOrange: {
-		"--main-color": "#e1864e",
-		"--text-theme-accent": "var(--dark)",
-		"--background-theme-accent": "var(--white)",
-		"--theme-accent-name": "light"
-	},
+    lightOrange: {
+      "--main-color": "#e1864e",
+      "--text-theme-accent": "var(--dark)",
+      "--background-theme-accent": "var(--white)",
+      "--theme-accent-name": "light",
+    },
 
-	
+    contrast: {
+      "--text-theme-accent": "var(--dark)",
+      "--background-theme-accent": "var(--white)",
+      "--main-color": "#000000",
+      "--theme-accent-name": "light",
+    },
 
-	contrast: {
-		"--text-theme-accent": "var(--dark)",
-		"--background-theme-accent": "var(--white)",
-		"--main-color": "#000000",
-		"--theme-accent-name": "light"
-	},
-
-	gray: {
-		"--text-theme-accent": "var(--white)",
-		"--background-theme-accent": "#1b1d21",
-		"--main-color": "#414d6b",
-		"--theme-accent-name": "dark"
-	}
-}))
+    gray: {
+      "--text-theme-accent": "var(--white)",
+      "--background-theme-accent": "#1b1d21",
+      "--main-color": "#414d6b",
+      "--theme-accent-name": "dark",
+    },
+  }),
+);
