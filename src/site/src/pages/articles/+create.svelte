e8c486b121c8f5f8555fad0f3764e34013306de1
zoodogood <zoodogood@gmail.com>
2024-03-22 12:43:29 +0200

    Add preview editor; Clean a code

--- src/site/src/pages/articles/+create.svelte
+++ src/site/src/pages/articles/+create.svelte
@@ -1,89 +1,124 @@
+<script>
+  import Layout from "#site-component/Layout";
+  import Dialog from "#site-component/Dialog";
+
+  import PagesRouter from "#site/lib/Router.js";
+  import {
+    fetchFromInnerApi,
+    MarkdownMetadata,
+    sleep,
+  } from "#lib/safe-utils.js";
+  import { svelteApp } from "#site/core/svelte-app.js";
+
+  const i18n = svelteApp.i18n.pages.articlesCreate;
+  const Contents = {
+    filename: null,
+  };
+  let labelNode;
+
+  const onFileUpload = async (inputEvent) => {
+    const button = labelNode.querySelector("button");
+    button.setAttribute("disabled", true);
+
+    const files = inputEvent.target.files;
+    const file = files[0];
+
+    const headers = {
+      Authorization: svelteApp.storage.getToken(),
+      "Content-Type": "application/octet-stream",
+      FileName: file.name,
+    };
+
+    const answer = await fetchFromInnerApi("site/articles/create", {
+      method: "POST",
+      body: file,
+      headers,
+    });
+
+    new Dialog({
+      target: document.body,
+      props: {
+        title: i18n.serverResponse,
+        description: MarkdownMetadata.yaml.stringify(answer),
+        useClassic: true,
+      },
+    });
+    button.removeAttribute("disabled");
+
+    Contents.filename = file.name;
+  };
+</script>
+
 <Layout>
-	{#if !svelteApp.user}
-		<p>{ i18n.atFirstAuthorize }</p>
-	{/if}
-	<h1>{ i18n.label }</h1>
+  {#if !svelteApp.user}
+    <p>{i18n.atFirstAuthorize}</p>
+  {/if}
+  <h1>{i18n.label}</h1>
 
-	<main>
-		<section class = "main-buttons">
-			<label for="load-file" bind:this = { labelNode } on:click={(pointerEvent) => !svelteApp.user && pointerEvent.preventDefault()} on:keydown={(keyEvent) => !svelteApp.user && keyEvent.preventDefault()}>
-				<input type="file" id="load-file" accept=".md" on:change={onFileUpload}>
-				<button on:click = {() => labelNode.click()} disabled = {!svelteApp.user || null}>{ i18n.uploadMarkdown }</button>
-			</label>
-			{#if Contents.filename}
-				<a href = "{ PagesRouter.relativeToPage(PagesRouter.getPageBy("articles/item").key) }?id={ svelteApp.user.id }/{ Contents.filename }">
-					<button>{ i18n.getDownToPage }</button>
-				</a>
-			{/if}
-		</section>
-		
-	</main>
-	
-	<p>{ i18n.generalCharacters.public }</p>
-	<p>{ i18n.generalCharacters.editable }</p>
-	<p>{ i18n.generalCharacters.author }</p>
-	<p>{ i18n.generalCharacters.noWarnings }</p>
-	
+  <main>
+    <section class="main-buttons">
+      <label
+        for="load-file"
+        bind:this={labelNode}
+        on:click={(pointerEvent) =>
+          !svelteApp.user && pointerEvent.preventDefault()}
+        on:keydown={(keyEvent) => !svelteApp.user && keyEvent.preventDefault()}
+      >
+        <input
+          type="file"
+          id="load-file"
+          accept=".md"
+          on:change={onFileUpload}
+        />
+        <button
+          on:click={() => labelNode.click()}
+          disabled={!svelteApp.user || null}>{i18n.uploadMarkdown}</button
+        >
+      </label>
+      {#if Contents.filename}
+        <a
+          href="{PagesRouter.relativeToPage(
+            PagesRouter.getPageBy('articles/item').key,
+          )}?id={svelteApp.user.id}/{Contents.filename}"
+        >
+          <button>{i18n.getDownToPage}</button>
+        </a>
+      {/if}
+    </section>
+  </main>
 
+  <p>{i18n.generalCharacters.public}</p>
+  <p>{i18n.generalCharacters.editable}</p>
+  <p>{i18n.generalCharacters.author}</p>
+  <p>{i18n.generalCharacters.noWarnings}</p>
+
+  <hr />
+  <section>
+    <h4>Внешнй редактор:</h4>
+    <p>
+      Это лишь редактор с возможностью предварительного просмотра (Хотя его
+      возможности и значительно отличаются от классического Markdown
+      синтаксиса). Сохраните содержимое в файл с расширением .md и загрузите
+      через кнопку выше.
+    </p>
+    <br />
+    <iframe
+      loading="lazy"
+      title="editor-preview"
+      src="https://marked.hyoo.ru/#!preview=view"
+      style="width: 100%; height: 100vh;"
+    />
+    <hr />
+  </section>
 </Layout>
 
-
 <style>
-	input
-	{
-		display: none;
-	}
+  input {
+    display: none;
+  }
 
-	.main-buttons
-	{
-		display: flex;
-		gap: 1em;
-	}
+  .main-buttons {
+    display: flex;
+    gap: 1em;
+  }
 </style>
-
-
-<script>
-	import Layout from '#site-component/Layout';
-	import Dialog from '#site-component/Dialog';
-
-	import PagesRouter from '#site/lib/Router.js';
-  	import { fetchFromInnerApi, MarkdownMetadata, sleep } from '#lib/safe-utils.js';
-  	import { svelteApp } from '#site/core/svelte-app.js';
-
-	const i18n = svelteApp.i18n.pages.articlesCreate;
-	const Contents = {
-		filename: null
-	}
-	let labelNode;
-
-	const onFileUpload = (async (inputEvent) => {
-		const button = labelNode.querySelector("button");
-		button.setAttribute("disabled", true);
-
-		const files = inputEvent.target.files;
-		const file = files[0];
-
-		const headers = {
-			Authorization: svelteApp.storage.getToken(),
-			"Content-Type": "application/octet-stream",
-			"FileName": file.name
-		};
-		
-
-		const answer = await fetchFromInnerApi(
-			"site/articles/create",
-			{method: "POST", body: file, headers}
-		);
-
-		new Dialog({
-			target: document.body,
-			props: {title: i18n.serverResponse, description: MarkdownMetadata.yaml.stringify(answer), useClassic: true}
-		});
-		button.removeAttribute("disabled");
-
-		Contents.filename = file.name;
-	});
-	
-
-
-</script>
