cedfe92a6b43db82e36e35e314bebc2f9fe6e5e8
zoodogood <zoodogood@gmail.com>
2024-03-26 06:25:58 +0200

    Clean a code

--- src/server/router.js
+++ src/server/router.js
@@ -1,79 +1,76 @@
-import { ImportDirectory } from '@zoodogood/import-directory';
-import path from 'path';
-
+import { ImportDirectory } from "@zoodogood/import-directory";
+import path from "path";
 
 const ROOT = "src/server/api";
 const directory = path.join(process.cwd(), ROOT);
 
-
-
 class Router {
+  routes = [];
 
-	routes = [];
+  async fetch() {
+    const modules = await new ImportDirectory({ subfolders: true }).import(
+      directory,
+    );
 
+    this.fetchedRoutes = modules;
 
-	async fetch(){
-		const modules = await new ImportDirectory({subfolders: true})
-			.import(directory);
+    return this;
+  }
 
+  bindAll(express) {
+    for (const { default: Route } of this.fetchedRoutes) {
+      const route = new Route(express);
+      this.handle(route, express);
 
-		this.fetchedRoutes = modules;
+      this.routes.push(route);
+    }
+  }
 
-		return this;
-	}
+  getParsedRoutesList() {
+    const isRegex = (prefix) => prefix instanceof RegExp;
+    const isSimple = (prefix) =>
+      typeof prefix === "string" && !prefix.match(/:|\*|\\/);
+    // to-do: complete a list
+    const getMethods = (router) =>
+      [
+        "get" in router ? "get" : null,
+        "post" in router ? "post" : null,
+        "put" in router ? "put" : null,
+      ].filter(Boolean);
 
-	bindAll(express){
-		for (const {default: Route} of this.fetchedRoutes){
-			const route = new Route(express);
-			this.handle(route, express);
+    const parse = (router) => ({
+      prefix: String(router.prefix),
+      isRegex: isRegex(router.prefix),
+      isSimple: isSimple(router.prefix) && router.isSimple !== false,
+      methods: getMethods(router),
+    });
+    const isAvailable = (router) => router.isHidden !== true;
+    return this.routes.filter(isAvailable).map(parse);
+  }
 
-			this.routes.push(route);
-		} 
-	}
+  handle(route, express) {
+    route.get &&
+      express.get(route.prefix, (...params) =>
+        this.decorate({ params, route, callback: route.get }),
+      );
 
-	getParsedRoutesList(){
-		const isRegex = (prefix) => prefix instanceof RegExp;
-		const isSimple = (prefix) => typeof prefix === "string" && !prefix.match(/:|\*|\\/);
-		// to-do: complete a list
-		const getMethods = (router) => [
-			"get" in router ?  "get" : null,
-			"post" in router ? "post" : null,
-			"put" in router ?  "put" : null
-		].filter(Boolean);
+    route.post &&
+      express.post(route.prefix, (...params) =>
+        this.decorate({ params, route, callback: route.post }),
+      );
+  }
 
-		const parse = (router => ({
-			prefix: String(router.prefix),
-			isRegex: isRegex(router.prefix),
-			isSimple: isSimple(router.prefix) && router.isSimple !== false,
-			methods: getMethods(router)
-		}));
-		const isAvailable = (router) => router.isHidden !== true;
-		return this.routes.filter(isAvailable).map(parse);
-	}
-
-	handle(route, express){
-		route.get && express.get(
-			route.prefix,
-			(...params) => this.decorate({params, route, callback: route.get})
-		);
-
-		route.post && express.post(
-			route.prefix,
-			(...params) => this.decorate({params, route, callback: route.post})
-		);
-	}
-
-	async decorate({params, route, callback}){
-		try {
-			await callback.apply(route, params);
-		} catch (error) {
-			const next = params.at(-1);
-			next(error);
-		}
-	}
+  async decorate({ params, route, callback }) {
+    try {
+      await callback.apply(route, params);
+    } catch (error) {
+      const next = params.at(-1);
+      next(error);
+    }
+  }
 }
 
-class BaseRoute {};
+class BaseRoute {}
 
 export default Router;
 export { Router, BaseRoute };
