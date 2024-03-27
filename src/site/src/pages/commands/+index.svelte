df3ff3b076eaaaa98887e1b9c5820732bce54e1f
zoodogood <zoodogood@gmail.com>
2024-03-27 07:28:15 +0200

    Clean a code

--- src/site/src/pages/commands/+index.svelte
+++ src/site/src/pages/commands/+index.svelte
@@ -1,124 +1,118 @@
+<script>
+  import { fetchFromInnerApi } from "#lib/safe-utils.js";
+  import Layout from "#site-component/Layout";
+  import EditableMarkdown from "#site-component/EditableMarkdown";
+  import svelteApp from "#site/core/svelte-app.js";
+
+  const i18n = svelteApp.i18n.pages.commandsIndex;
+
+  const CategoryEnum = {
+    delete: i18n.category.delete,
+    dev: i18n.category.dev,
+    user: i18n.category.user,
+    guild: i18n.category.guild,
+    bot: i18n.category.bot,
+    other: i18n.category.other,
+  };
+
+  let commands = [];
+  (async () => {
+    const _commands = await fetchFromInnerApi("client/commands/list");
+    console.log({ _commands });
+    commands = _commands;
+  })();
+</script>
+
 <Layout>
-	<h3>TO-DO: Задача. Полностью изменить внешний вид этой страницы</h3>
-	<section>
-		<ul>
-			{#each commands as command}
-			<li>
-				<h2>{ command.name }</h2>
-				<EditableMarkdown source = { command.media.description.trim() }/>
-				<footer>
-					<span class = "category">{ CategoryEnum[ command.type ]}</span>
-					{#if command.media.poster}
-						<img src = { command.media.poster } alt="poster" fetchpriority = "low" loading = "lazy">
-					{/if}
-					<h6>
-						{#each command.alias.split(" ") as alias}
-							<span>!{ alias }</span>
-						{/each}
-					</h6>
-				</footer>
-			</li>
-			{/each}
-		</ul>
-		
-	</section>
-	
+  <h3>TO-DO: Задача. Полностью изменить внешний вид этой страницы</h3>
+  <small>Текстовые команды могут быть отправлены в чат. Бот слушает..</small>
+  <section>
+    <ul>
+      {#each commands as command}
+        <li>
+          <h2>{command.name}</h2>
+          <EditableMarkdown source={command.media.description.trim()} />
+          <footer>
+            <span class="category">{CategoryEnum[command.type]}</span>
+            {#if command.media.poster}
+              <img
+                src={command.media.poster}
+                alt="poster"
+                fetchpriority="low"
+                loading="lazy"
+              />
+            {/if}
+            <h6>
+              {#each command.alias.split(" ") as alias}
+                <span>!{alias}</span>
+              {/each}
+            </h6>
+          </footer>
+        </li>
+      {/each}
+    </ul>
+  </section>
 </Layout>
 
-
 <style>
-	ul
-	{
-		display: flex;
-		list-style: none;
-		flex-wrap: wrap;
-		gap: 30px;
-	}
+  ul {
+    display: flex;
+    list-style: none;
+    flex-wrap: wrap;
+    gap: 30px;
+  }
 
+  li {
+    width: min(100%, 1000px);
 
-	li
-	{
-		width: min(100%, 1000px);
+    display: flex;
+    gap: 1em;
 
-		display: flex;
-		gap: 1em;
+    flex-direction: column;
+    background-color: #88888811;
+    flex-grow: 1;
+    padding: 30px;
+    border-radius: 15px;
+    white-space: pre-line;
 
-		flex-direction: column;
-		background-color: #88888811;
-		flex-grow: 1;
-		padding: 30px;
-		border-radius: 15px;
-		white-space: pre-line;
+    font-size: 0.9em;
+  }
 
-		font-size: 0.9em;
-	}
+  h2 {
+    text-transform: uppercase;
+  }
 
-	h2
-	{
-		text-transform: uppercase;
-	}
+  img {
+    opacity: 0.8;
+    transition: opacity 1s;
+  }
 
-	img
-	{
-		opacity: 0.8;
-		transition: opacity 1s;
-	}
+  img:hover {
+    opacity: 1;
+  }
 
-	img:hover
-	{
-		opacity: 1;
-	}
+  .category {
+    background-color: #88888811;
+    display: inline-block;
+    width: fit-content;
+    padding: 0.2em;
 
-	.category 
-	{
-		background-color: #88888811;
-		display: inline-block;
-		width: fit-content;
-		padding: 0.2em;
+    margin-top: 1.5em;
+  }
 
-		margin-top: 1.5em;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    margin-top: auto;
+  }
 
-	footer
-	{
-		display: flex;
-		flex-direction: column;
-		gap: 1em;
-		margin-top: auto;
-	}
-
-	h6
-	{
-		margin-top: 0.75em;
-		opacity: 0.65;
-		display: flex;
-		gap: 0.5em;
-		font-weight: 0.5em;
-		flex-wrap: wrap;
-	}
+  h6 {
+    margin-top: 0.75em;
+    opacity: 0.65;
+    display: flex;
+    gap: 0.5em;
+    font-weight: 0.5em;
+    flex-wrap: wrap;
+  }
 </style>
-
-<script>
-  	import { fetchFromInnerApi } from '#lib/safe-utils.js';
-	import Layout from '#site-component/Layout';
-	import EditableMarkdown from '#site-component/EditableMarkdown';
-  	import svelteApp from '#site/core/svelte-app.js';
-
-	const i18n = svelteApp.i18n.pages.commandsIndex;
-	
-	const CategoryEnum = {
-		delete: i18n.category.delete,
-		dev: i18n.category.dev,
-		user: i18n.category.user,
-		guild: i18n.category.guild,
-		bot: i18n.category.bot,
-		other: i18n.category.other
-	}
-	
-	let commands = [];
-	(async () => {
-		const _commands = await fetchFromInnerApi("client/commands/list");
-		console.log({_commands});
-		commands = _commands;
-	})();
-</script>
