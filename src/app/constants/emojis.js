fe5f0a30e9fe32cf9153a764cc68a5b829fa7ea1
Alexsandr1x <zoodogood@gmail.com>
2024-04-04 14:59:57 +0300

    Add animation_tick_block, animation_cross_block emojis

--- src/app/constants/emojis.js
+++ src/app/constants/emojis.js
@@ -5,6 +5,18 @@ const Emoji = {
       return "📜";
     },
   },
+  animation_tick_block: {
+    id: "763371572073201714",
+    toString() {
+      return "<a:yes:763371572073201714>";
+    }
+  },
+  animation_cross_block: {
+    id: "763371626908876830",
+    toString() {
+      return "<a:Yno:763371626908876830>"
+    }
+  },
   green_arrow_left: {
     id: "640449848050712587",
   },
