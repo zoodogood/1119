61916910e544ba9fca2093e004bbe2841d5af886
zoodogood <zoodogood@gmail.com>
2024-04-10 10:59:28 +0300

    Fix

--- src/lib/createStopPromise.js
+++ src/lib/createStopPromise.js
@@ -3,4 +3,5 @@ export function createStopPromise() {
   const credentials = Promise.withResolvers();
   this._createStopPromise_stoppers ||= [];
   this._createStopPromise_stoppers.push(credentials.promise);
+  return credentials;
 }
