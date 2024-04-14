5db0560382b18a7bb2603b54ef96bb6ccba8a779
zoodogood <zoodogood@gmail.com>
2024-04-14 09:28:11 +0300

    Refact

--- folder/commands/restart.js
+++ folder/commands/restart.js
@@ -1,52 +1,89 @@
 import { BaseCommand } from "#lib/BaseCommand.js";
 import config from "#config";
 import get from "#lib/child-process-utils.js";
-import { ending } from "@zoodogood/utils/primitives";
+import { Pager } from "#lib/DiscordPager.js";
+import { BaseCommandRunContext } from "#lib/CommandRunContext.js";
 const { run } = get({ root: process.cwd() });
+import { CliParser } from "@zoodogood/utils/primitives";
+import { MINUTE } from "#constants/globals/time.js";
 
 const toANSIBlock = (content) => `\`\`\`ansi\n${content}\`\`\``;
 
+class CommandRunContext extends BaseCommandRunContext {
+  needPull;
+  needBuild;
+  parseCli(input) {
+    const parsed = new CliParser()
+      .setText(input)
+      .captureFlags(this.command.options.cliParser.flags)
+      .collect();
+
+    const values = parsed.resolveValues((capture) => capture?.toString());
+    this.setCliParsed(parsed, values);
+    this.resolveFlags();
+  }
+
+  resolveFlags() {
+    const values = this.cliParsed.at(1);
+    this.needPull = values.get("--pull");
+    this.needBuild = values.get("--build");
+  }
+}
+
 class Command extends BaseCommand {
-  async onChatInput(_msg, interaction) {
-    const COMMANDS = [
-      "git pull",
-      "pnpm run build",
-      config.pm2.id
+  COMMANDS = [
+    {
+      run: "git pull",
+      filter: (context) => context.needPull,
+    },
+    {
+      run: "pnpm run build && pnpm prune --prod",
+      filter: (context) => context.needBuild,
+    },
+    {
+      run: config.pm2.id
         ? `pnpm run pm2-please-restart ${config.pm2.id}`
         : "echo pm2 not setted",
-    ];
+      filter: () => true,
+    },
+  ];
 
+  async run(context) {
+    context.parseCli(context.interaction.params);
+    const { COMMANDS } = this;
+    const { channel } = context;
     const embed = {
       title: "<:emoji_50:753916145177722941>",
       color: "#2c2f33",
       description: "**RESTARTING...**",
     };
 
-    const message = await interaction.channel.msg(embed);
-    embed.edit = true;
+    const pager = new Pager(channel);
+    pager.setDefaultMessageState(embed);
 
-    const updateDescription = (content) => {
-      embed.description += `\n${content}`;
-      embed.description >= 2000 &&
-        (embed.description = `Очищено ${ending(
-          embed.description.length,
-          "символ",
-          "ов",
-          "а",
-        )}\n${content}`);
-      message.msg(embed);
-    };
-
-    for (const string of COMMANDS) {
-      const [command, ...params] = string.split(" ");
-      updateDescription(`\n> ${string}`);
+    for (const commandData of COMMANDS) {
+      if (!commandData.filter(context)) {
+        continue;
+      }
+      const [command, ...params] = commandData.split(" ");
+      pager.addPages({
+        title: `> ${commandData.run}`,
+      });
+      pager.updateMessage();
 
       const result = await run(command, params).catch(
         (error) => `Error: ${error.message}`,
       );
 
-      updateDescription(toANSIBlock(result));
+      pager.pages.at(-1).description = toANSIBlock(result);
+      pager.updateMessage();
     }
+  }
+
+  async onChatInput(_msg, interaction) {
+    const context = CommandRunContext.new(interaction, this);
+    context.setWhenRunExecuted(this.run(context));
+    return context;
   }
 
   options = {
@@ -55,9 +92,23 @@ class Command extends BaseCommand {
     media: {
       description: "Перезапускает процесс",
     },
+    cliParser: {
+      flags: [
+        {
+          name: "--pull",
+          capture: ["--pull"],
+          description: "Найти и получить обновления из репозитория",
+        },
+        {
+          name: "--build",
+          capture: ["--build"],
+          description: "Применить команду сборки сайта",
+        },
+      ],
+    },
     alias: "перезапустить перезапуск рестарт",
     allowDM: true,
-    cooldown: 100_000,
+    cooldown: MINUTE,
     cooldownTry: 5,
     type: "dev",
   };
