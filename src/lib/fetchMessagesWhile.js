e069bc1fb9e6d265e05eacdef6b44748094fbac7
zoodogood <zoodogood@gmail.com>
2024-04-16 08:24:19 +0300

    Add fetchMessagesWhile

---
+++ src/lib/fetchMessagesWhile.js
@@ -0,0 +1,23 @@
+export async function* fetchMessagesWhile({
+  fromMessage = null,
+  while: checkWhile,
+  direction = "before",
+  channel,
+}) {
+  const { messages } = channel;
+  const MAX_AVAILABLE_LIMIT = 50;
+  let bulk;
+  do {
+    bulk = await messages.fetch({
+      limit: MAX_AVAILABLE_LIMIT,
+      [direction]: fromMessage,
+    });
+    for (const message of bulk.values()) {
+      yield message;
+    }
+    if (!checkWhile()) {
+      return;
+    }
+    fromMessage = bulk.at(-1)?.id;
+  } while (bulk.length === MAX_AVAILABLE_LIMIT);
+}
