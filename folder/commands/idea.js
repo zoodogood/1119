1b8c5ffbaae93af7a4f272c68bd01d4c3f598575
zoodogood <zoodogood@gmail.com>
2024-04-16 08:44:08 +0300

    Refact; Init flags

--- folder/commands/idea.js
+++ folder/commands/idea.js
@@ -1,12 +1,35 @@
+// @ts-check
 import { BaseCommand } from "#lib/BaseCommand.js";
 import * as Util from "#lib/util.js";
 import { client } from "#bot/client.js";
 import { BaseCommandRunContext } from "#lib/CommandRunContext.js";
 import { MINUTE } from "#constants/globals/time.js";
+import { fetchMessagesWhile } from "#lib/fetchMessagesWhile.js";
+import { CliParser } from "@zoodogood/utils/primitives";
 
-class CommandRunContext extends BaseCommandRunContext {}
+class CommandRunContext extends BaseCommandRunContext {
+  parseCli(input) {
+    const parsed = new CliParser()
+      .setText(input)
+      .captureFlags(this.command.options.cliParser.flags)
+      .captureResidue({ name: "phrase" })
+      .collect();
+
+    const values = parsed.resolveValues((capture) => capture?.toString());
+    this.setCliParsed(parsed, values);
+  }
+}
 
 class Command extends BaseCommand {
+  getChannel() {
+    const IDEAS_CHANNEL_ID = "753587805195862058";
+    return client.channels.cache.get(IDEAS_CHANNEL_ID);
+  }
+  fetchIdeaMessages() {
+    const channel = this.getChannel();
+    const generator = fetchMessagesWhile({ while: () => true, channel });
+    return generator;
+  }
   async processAggree(context) {
     const { channel, user, interaction } = context;
     const heAccpet = await Util.awaitUserAccept({
@@ -39,6 +62,7 @@ class Command extends BaseCommand {
     return +Util.match(authorField.name, /#\d+$/).slice(1);
   }
   async run(context) {
+    context.parseCli(context.intearction.params);
     this.processDefaultBehaviour(context);
   }
 
@@ -46,16 +70,14 @@ class Command extends BaseCommand {
     if (!(await this.processAggree(context))) {
       return;
     }
-
     const { interaction, user } = context;
-
-    const channel = client.channels.cache.get("753587805195862058");
-
+    const channel = this.getChannel();
     const increasedIdeaNumber = (await this.fetchLastIdeaNumber(channel)) + 1;
+    const phrase = context.cliParsed.at(1).get("phrase");
 
     channel.msg({
       title: "<:meow:637290387655884800> Какая классная идея!",
-      description: `**Идея:**\n${interaction.params}`,
+      description: `**Идея:**\n${phrase}`,
       color: interaction.userData.profile_color || "#00ffaf",
       author: {
         name: `${user.username} #${increasedIdeaNumber}`,
@@ -65,13 +87,13 @@ class Command extends BaseCommand {
     });
     channel.msg({
       title: "<:meow:637290387655884800> Вы отправили нам свою идею! Спасибо!",
-      description: `А что, идея «${interaction.params}» весьма не плоха...`,
+      description: `А что, идея «${phrase}» весьма не плоха...`,
       color: "#00ffaf",
       author: { name: user.username, iconURL: user.avatarURL() },
     });
   }
   async onChatInput(msg, interaction) {
-    const context = CommandRunContext.new(interaction, this);
+    const context = await CommandRunContext.new(interaction, this);
     context.setWhenRunExecuted(this.run(context));
     return context;
   }
@@ -84,7 +106,26 @@ class Command extends BaseCommand {
         "\n\nЕсли у вас есть идеи как можно улучшить бота — с помощью этой команды отправьте её на сервер.\nНе забудьте позже обсудить её в чате, подробно расписывая особенности вы повышаете вероятность того, что она будет реализована.\n\n\n✏️\n```python\n!idea {content}\n```\n\n",
     },
     cliParser: {
-      flags: [],
+      flags: [
+        {
+          name: "--edit",
+          capture: ["--edit", "-e"],
+          expectValue: true,
+          description:
+            "Автор идеи может переписать её содержание за её номером",
+        },
+        {
+          name: "--at",
+          capture: ["--at"],
+          expectValue: true,
+          description: "Возвращает информацию об идее за её номером",
+        },
+        {
+          name: "--json",
+          capture: ["--json", "-j"],
+          description: "Возвращает список идей как *.json файл",
+        },
+      ],
     },
     alias: "идея innovation новвоведение ідея proposal предложение",
     allowDM: true,
