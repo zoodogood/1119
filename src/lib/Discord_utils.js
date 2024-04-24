c8c47f81e7d87c2ffa5a6b72d5093e7275a4e96c
zoodogood <zoodogood@gmail.com>
2024-04-24 06:50:39 +0300

    Add ReactionInteraction

---
+++ src/lib/Discord_utils.js
@@ -0,0 +1,19 @@
+import { pushMessage } from "#lib/DiscordPushMessage.js";
+
+export class ReactionInteraction {
+  constructor(reaction, user) {
+    const { message, emoji } = reaction;
+    const { channel, guild } = message;
+    const customId = emoji.code;
+    Object.assign(this, {
+      user,
+      message,
+      channel,
+      guild,
+      reaction,
+      emoji,
+      customId,
+    });
+  }
+  msg = pushMessage;
+}
