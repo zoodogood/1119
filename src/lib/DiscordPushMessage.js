da3b938c0d18fdb703a6b283305a455bd6dc33c8
zoodogood <zoodogood@gmail.com>
2024-04-24 06:17:17 +0300

    Add pushMessage

---
+++ src/lib/DiscordPushMessage.js
@@ -0,0 +1,38 @@
+import { justSendMessage } from "@zoodogood/utils/discordjs";
+import { sendErrorInfo } from "#lib/sendErrorInfo.js";
+import { Message, InteractionResponse } from "discord.js";
+import config from "#config";
+
+export async function pushMessage(options, ..._devFixParams) {
+  if (_devFixParams.length > 0) {
+    throw new Error("Incorrect message input. Need to fix!");
+  }
+
+  options.color ||= config.development ? "#000100" : "#23ee23";
+
+  const target =
+    this instanceof InteractionResponse
+      ? this.interaction
+      : this instanceof Message && !options.edit
+        ? this.channel
+        : this;
+
+  const message = (async () => {
+    try {
+      return await justSendMessage(target, options);
+    } catch (error) {
+      if (!error.message.includes("Invalid Form Body")) {
+        console.error(options);
+        throw error;
+      }
+      await sendErrorInfo({
+        description: "Оригинальное сообщение не было доставлено",
+        channel: target,
+        error,
+      });
+      throw new Error(error.message, { cause: error });
+    }
+  })();
+
+  return message;
+}
