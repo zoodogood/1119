bda7d188c6811498e432cd0c2fb93ab949db54c8
zoodogood <zoodogood@gmail.com>
2024-04-26 11:41:17 +0300

    Add getValuesByIndexes

--- src/lib/safe-utils.js
+++ src/lib/safe-utils.js
@@ -376,6 +376,18 @@ class TimeAuditor {
   }
 }
 
+export function getValuesByIndexes(array, indexes) {
+  const targets = new Set();
+  for (const index of indexes) {
+    if (index === "+") {
+      array.forEach((target) => targets.add(target));
+      break;
+    }
+    targets.add(array.at(index));
+  }
+  return [...targets.values()].filter(Boolean);
+}
+
 export {
   ending,
   omit,
