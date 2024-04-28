41019f503a86791c51fbb045f8686fcd081b2b91
zoodogood <zoodogood@gmail.com>
2024-04-28 10:57:35 +0300

    Init

---
+++ folder/events/TimeEvents/partner-daemon.js
@@ -0,0 +1,12 @@
+import { PartnersDaemon } from "#folder/commands/partners.js";
+import { TimeEventsManager } from "#lib/modules/mod.js";
+
+class Event {
+  async run(timeEventData) {}
+
+  options = {
+    name: "TimeEvent/partner-daemon",
+  };
+}
+
+export default Event;
