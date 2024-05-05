9617991eb87e3c1dcf2639a0e3614b9c507ff07e
zoodogood <zoodogood@gmail.com>
2024-05-05 10:43:46 +0300

    Move code to #lib/features/primitives

--- src/lib/safe-utils.js
+++ src/lib/safe-utils.js
@@ -394,18 +394,6 @@ class TimeAuditor {
   }
 }
 
-export function getValuesByIndexes(array, indexes) {
-  const targets = new Set();
-  for (const index of indexes) {
-    if (index === "+") {
-      array.forEach((target) => targets.add(target));
-      break;
-    }
-    targets.add(array.at(index));
-  }
-  return [...targets.values()].filter(Boolean);
-}
-
 export function* zeroCenteredSequence() {
   let i = 0;
   while (true) {
