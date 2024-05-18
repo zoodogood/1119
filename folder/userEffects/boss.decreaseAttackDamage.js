63dddc8c8ac7d6f9f28e49bedb1945a56ecbf624
zoodogood <zoodogood@gmail.com>
2024-05-18 14:06:40 +0300

    Add decreaseAttackDamage effect

---
+++ folder/userEffects/boss.decreaseAttackDamage.js
@@ -0,0 +1,32 @@
+import { BossEffects } from "#lib/modules/BossManager.js";
+import { EffectInfluenceEnum } from "#lib/modules/EffectsManager.js";
+
+export default {
+  id: "boss.increaseAttackDamage",
+  callback: {
+    bossBeforeAttack: (user, effect, data) => {
+      const { guild, attackContext } = data;
+      const {
+        values: { guildId },
+      } = effect;
+
+      if (guild.id !== guildId) {
+        return;
+      }
+      const { power, multiplayer } = effect.values;
+      attackContext.damageMultiplayer *= 1 - (power * multiplayer);
+
+      effect.values.duration--;
+      if (!effect.values.duration) {
+        BossEffects.removeEffect({ user, effect });
+      }
+    },
+  },
+  values: {
+    power: () => 2,
+    duration: () => 1,
+    multiplayer: () => 1,
+    guildId: (user, effect, { guild }) => guild?.id,
+  },
+  influence: EffectInfluenceEnum.Negative,
+};
