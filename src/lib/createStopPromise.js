d3a414aa77274b8a95db71b47d4574147dd59b47
zoodogood <zoodogood@gmail.com>
2024-05-19 10:27:51 +0300

    Refact

--- src/lib/createStopPromise.js
+++ src/lib/createStopPromise.js
@@ -1,7 +1,12 @@
-// Use it with await Promise.all(event,_createStopPromise_stoppers);
+// Use it with await event.whenStopPromises();
 export function createStopPromise() {
-  const credentials = Promise.withResolvers();
-  this._createStopPromise_stoppers ||= [];
-  this._createStopPromise_stoppers.push(credentials.promise);
-  return credentials;
+  const stoppers = [];
+  const addStopPromise = () => {
+    const credentials = Promise.withResolvers();
+    stoppers.push(credentials.promise);
+    return credentials;
+  };
+  const stopPromises = () => stoppers;
+  const whenStopPromises = () => Promise.all(stopPromises());
+  return { addStopPromise, stopPromises, whenStopPromises };
 }
