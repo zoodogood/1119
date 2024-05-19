c3679d6cc9ba2fff52d9748c0cb1e2c21ab811cc
zoodogood <zoodogood@gmail.com>
2024-05-19 17:46:07 +0300

    Improve

--- src/site/src/lib/init_pwa.js
+++ src/site/src/lib/init_pwa.js
@@ -1,10 +1,16 @@
 import { createElement } from "#site/lib/dom_utils.js";
 
 export async function init_pwa_worker() {
+  const unique = "init_pwa_worker__active_pwa_manifest";
+  if (document.getElementById(unique)) {
+    return;
+  }
   const manifestNode = createElement("link", {
     rel: "manifest",
     href: "manifest.json",
+    id: unique,
   });
+  console.log(manifestNode);
   document.head.appendChild(manifestNode);
   await navigator.serviceWorker.register("/pwa_worker_up");
 }
