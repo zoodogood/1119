1547030582b4e4e5a8c67359e77c4678f6a1c03e
zoodogood <zoodogood@gmail.com>
2024-05-24 19:00:38 +0300

    Clean a code

--- src/lib/CommandRunContext.js
+++ src/lib/CommandRunContext.js
@@ -2,38 +2,38 @@
 import { BaseContext } from "#lib/BaseContext.js";
 
 class BaseCommandRunContext extends BaseContext {
-  options = {};
+  cliParsed = null;
   command = null;
   interaction = null;
-  whenRunExecuted = null;
-
-  setCliParsed(parsed, values) {
-    this.cliParsed = [parsed, values];
-    return this;
-  }
-  getCliParsed() {
-    return this.cliParsed || [null, null];
-  }
-  cliParsed = null;
-
-  setWhenRunExecuted(whenRunExecuted) {
-    this.whenRunExecuted = whenRunExecuted;
-    return this;
-  }
-
-  end() {
-    this.isEnded = true;
-  }
   isEnded = false;
 
-  static async new(interaction, command) {
-    return new this(interaction, command);
-  }
+  options = {};
+  whenRunExecuted = null;
   constructor(interaction, command) {
     super(`command.${command.options.name}`, interaction);
     this.command = command;
     this.interaction = interaction;
     interaction.extend && Object.assign(this.options, interaction.extend);
+  }
+
+  end() {
+    this.isEnded = true;
+  }
+
+  getCliParsed() {
+    return this.cliParsed || [null, null];
+  }
+  static async new(interaction, command) {
+    return new this(interaction, command);
+  }
+
+  setCliParsed(parsed, values) {
+    this.cliParsed = [parsed, values];
+    return this;
+  }
+  setWhenRunExecuted(whenRunExecuted) {
+    this.whenRunExecuted = whenRunExecuted;
+    return this;
   }
 
   toJSON() {
