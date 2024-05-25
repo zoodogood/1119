009141855842232111eb0537495c0486cada67e6
zoodogood <zoodogood@gmail.com>
2024-05-25 10:13:06 +0300

    Add writeError

---
+++ src/site/src/lib/writeErrorToServer.js
@@ -0,0 +1,24 @@
+import { fetchFromInnerApi } from "#lib/safe-utils.js";
+
+export async function writeError(error) {
+  if (typeof error === "string") {
+    error = new Error(error);
+  }
+
+  if (error.cause) {
+    error.stack ||= error.cause.stack;
+    error.stack += `\nCause:\n${error.cause.stack}`;
+  }
+
+  await fetchFromInnerApi("toys/throw", {
+    body: JSON.stringify({
+      message: error.message,
+      stack: error.stack,
+      cause: error?.cause.message,
+    }),
+    headers: { "Content-Type": "text/plain" },
+    method: "POST",
+  }).catch(() => {});
+}
+
+export default writeError;
