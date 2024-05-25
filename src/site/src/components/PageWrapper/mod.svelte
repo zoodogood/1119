dbc3dd6fd9b0b0049e9af139ab6d844e66359db5
zoodogood <zoodogood@gmail.com>
2024-05-25 10:20:11 +0300

    Clean a code; Handle errors

--- src/site/src/components/PageWrapper/mod.svelte
+++ src/site/src/components/PageWrapper/mod.svelte
@@ -1,47 +1,55 @@
-<Notifications>
-	<svelte:component this = { page }/>
-</Notifications>
-
-
-
-<MetaTags
-	title = { i18n.title }
-	{languageAlternates}
-	canonical = { relativeSiteRoot(svelteApp) }
-	additionalMetaTags = {[
-		{
-			name: "theme-color",
-			content: Theme.collection.get( getStoreValue(Theme.current ) )["--main-color"]
-		}
-	]}
-/>
-
 <script>
-	export let page;
+  export let page;
 
-	
-	import Notifications from '#site-component-lib/Notifications/wrap.svelte';
-	import { MetaTags } from 'svelte-meta-tags';
-	
-	import svelteApp from '#site/core/svelte-app.js';
-  	import config from '#config';
-  	import { relativeSiteRoot } from '#lib/safe-utils.js';
-	import { Theme } from '#site-component/ThemeSwitcher';
-	import { get as getStoreValue } from 'svelte/store';
-	
+  import Notifications from "#site-component-lib/Notifications/wrap.svelte";
+  import { MetaTags } from "svelte-meta-tags";
 
-	const _currentURLLang = svelteApp.url.base.lang;
-	const languageAlternates = config.i18n.availableLanguages
-		.map(locale => {
-			svelteApp.url.base.lang = locale;
-			const href = relativeSiteRoot(svelteApp, svelteApp.url.subpath.join("/"));
-			return {hrefLang: locale, href};
-		});
+  import config from "#config";
+  import { relativeSiteRoot } from "#lib/safe-utils.js";
+  import { Theme } from "#site-component/ThemeSwitcher";
+  import svelteApp from "#site/core/svelte-app.js";
+  import { writeError } from "#site/lib/writeErrorToServer.js";
+  import { onMount } from "svelte";
+  import { get as getStoreValue } from "svelte/store";
 
-	svelteApp.url.base.lang = _currentURLLang;
+  const _currentURLLang = svelteApp.url.base.lang;
+  const languageAlternates = config.i18n.availableLanguages.map((locale) => {
+    svelteApp.url.base.lang = locale;
+    const href = relativeSiteRoot(svelteApp, svelteApp.url.subpath.join("/"));
+    return { hrefLang: locale, href };
+  });
 
+  svelteApp.url.base.lang = _currentURLLang;
 
-	const i18n = svelteApp.i18n.general;
-	document.documentElement.setAttribute("lang", svelteApp.lang);
+  const i18n = svelteApp.i18n.general;
+  document.documentElement.setAttribute("lang", svelteApp.lang);
+
+  onMount(() => {
+    addEventListener("unhandledrejection", async (event) => {
+      const { reason } = event;
+      writeError(reason);
+    });
+
+    addEventListener("error", (event) => {
+      writeError(event);
+    });
+  });
 </script>
 
+<Notifications>
+  <svelte:component this={page} />
+</Notifications>
+
+<MetaTags
+  title={i18n.title}
+  {languageAlternates}
+  canonical={relativeSiteRoot(svelteApp)}
+  additionalMetaTags={[
+    {
+      name: "theme-color",
+      content: Theme.collection.get(getStoreValue(Theme.current))[
+        "--main-color"
+      ],
+    },
+  ]}
+/>
