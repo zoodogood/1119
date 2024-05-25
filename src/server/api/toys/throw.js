1de19b42aab5fa77d97aa9e733ce0c8ed4ff1e0c
zoodogood <zoodogood@gmail.com>
2024-05-25 10:14:14 +0300

    Handle custom errors

--- src/server/api/toys/throw.js
+++ src/server/api/toys/throw.js
@@ -1,18 +1,29 @@
+import { parse_body } from "#lib/express_utils.js";
+import { ErrorsHandler } from "#lib/modules/ErrorsHandler.js";
 import { BaseRoute } from "#server/router.js";
 
 const PREFIX = "/toys/throw";
 
-
 class Route extends BaseRoute {
-	prefix = PREFIX;
+  prefix = PREFIX;
 
-	constructor(express){
-		super();
-	}
+  constructor(express) {
+    super();
+  }
 
-	async get(request, response, next){
-		throw new Error("Error caused automatically from toy api point: Need more coffe");
-	}
+  async get(request, response, next) {
+    throw new Error(
+      "Error caused automatically from toy api point: Need more coffe",
+    );
+  }
+
+  async post(request, response, next) {
+    const data = JSON.parse(await parse_body(request));
+    const { message, stack } = data;
+    const error = new Error(message);
+    error.stack = stack;
+    ErrorsHandler.onErrorReceive(error, { type: "site" });
+  }
 }
 
 export default Route;
