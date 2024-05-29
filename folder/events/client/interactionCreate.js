7c1f7fa105f11c9f9109ca0dee560c6427baa460
zoodogood <zoodogood@gmail.com>
2024-05-29 13:29:07 +0300

    Add cleanUnhandled

--- folder/events/client/interactionCreate.js
+++ folder/events/client/interactionCreate.js
@@ -1,11 +1,42 @@
+import { client } from "#bot/client.js";
+import { actionRowsToComponents } from "#lib/Discord_utils.js";
 import { BaseEvent } from "#lib/modules/EventsManager.js";
 import { CommandsManager, Executor } from "#lib/modules/mod.js";
-import { client } from "#bot/client.js";
+import { sleep } from "#lib/safe-utils.js";
+import { ButtonStyle } from "discord.js";
 
 class Event extends BaseEvent {
+  options = {
+    name: "client/interactionCreate",
+  };
+
   constructor() {
     const EVENT = "interactionCreate";
     super(client, EVENT);
+  }
+
+  async cleanUnhandled(interaction) {
+    if (!interaction.isButton()) {
+      return;
+    }
+
+    await sleep(1000);
+    if (interaction.replied || interaction.deffered) {
+      return;
+    }
+
+    const current_components = actionRowsToComponents(
+      interaction.message.components,
+    );
+    const component = current_components
+      .flat()
+      .find((component) => component.customId === interaction.customId);
+
+    component.style = ButtonStyle.Danger;
+    interaction.message.msg({
+      edit: true,
+      components: current_components,
+    });
   }
 
   async run(interaction) {
@@ -22,11 +53,9 @@ class Event extends BaseEvent {
       type && Executor.emit(type, target, { params, interaction });
       return;
     }
-  }
 
-  options = {
-    name: "client/interactionCreate",
-  };
+    this.cleanUnhandled(interaction);
+  }
 }
 
 export default Event;
