3e4302e797174e2eec9c97a5f8e91371662ab058
zoodogood <zoodogood@gmail.com>
2024-05-30 13:21:58 +0300

    Add accept_github_commit_webhook api point

---
+++ src/server/api/extenral/accept_github_webhook.js
@@ -0,0 +1,28 @@
+import client from "#bot/client.js";
+import config from "#config";
+import { parse_body } from "#lib/express_utils.js";
+import { BaseRoute } from "#server/router.js";
+
+const PREFIX = "/accept_github_commit_webhook";
+
+class Route extends BaseRoute {
+  isHidden = true;
+  prefix = PREFIX;
+
+  constructor(express) {
+    super();
+  }
+
+  async post(request, response) {
+    const eventName = request.headers["x-github-event"];
+    console.log(request, eventName);
+    response.sendStatus(202);
+    const body = await parse_body(request);
+    client.channels.cache.get(config.guild.logChannelId).msg({
+      title: "Github push webhook handler",
+      description: `${body.slice(0, 4000)}`,
+    });
+  }
+}
+
+export default Route;
