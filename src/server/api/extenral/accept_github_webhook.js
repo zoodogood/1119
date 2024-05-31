f247fa92d65ac07ec2e42cc81cbd8e1dc4b346ed
zoodogood <zoodogood@gmail.com>
2024-05-31 08:50:23 +0300

    Improve

--- src/server/api/extenral/accept_github_webhook.js
+++ src/server/api/extenral/accept_github_webhook.js
@@ -1,6 +1,7 @@
 import client from "#bot/client.js";
 import config from "#config";
 import { parse_body } from "#lib/express_utils.js";
+import { toLocaleDeveloperTypes } from "#lib/safe-utils.js";
 import { BaseRoute } from "#server/router.js";
 
 const PREFIX = "/accept_github_commit_webhook";
@@ -15,11 +16,12 @@ class Route extends BaseRoute {
 
   async post(request, response) {
     const eventName = request.headers["x-github-event"];
+
     await parse_body(request, response, { method: "json" });
     response.sendStatus(202);
     client.channels.cache.get(config.guild.logChannelId).msg({
       title: `Github \`${eventName}\` webhook handler`,
-      description: `${JSON.stringify(request.body).slice(0, 4000)}`,
+      description: `${toLocaleDeveloperTypes(request.body).slice(0, 4000)}`,
     });
   }
 }
