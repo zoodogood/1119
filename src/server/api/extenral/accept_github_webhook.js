49bb70397329ba90a693b407e9c3f038dc2f316b
zoodogood <zoodogood@gmail.com>
2024-05-31 16:26:34 +0300

    Improve
    + Improve: Message to logsChannel will not sended on github commit
    + The first changelog message

--- src/server/api/extenral/accept_github_webhook.js
+++ src/server/api/extenral/accept_github_webhook.js
@@ -1,8 +1,5 @@
-import client from "#bot/client.js";
-import config from "#config";
 import { parse_body } from "#lib/express_utils.js";
 import EventsManager from "#lib/modules/EventsManager.js";
-import { toLocaleDeveloperTypes } from "#lib/safe-utils.js";
 import { BaseRoute } from "#server/router.js";
 
 const PREFIX = "/accept_github_webhook";
@@ -19,20 +16,8 @@ class Route extends BaseRoute {
   }
 
   async post(request, response) {
-    const eventName = request.headers["x-github-event"];
-
-    const body = await parse_body(request, response, { method: "json" });
-    const { sender } = body ?? {};
+    await parse_body(request, response, { method: "json" });
     response.sendStatus(202);
-    const description = Object.entries(request.body)
-      .map(([key, value]) => `- ${key} => ${toLocaleDeveloperTypes(value)}`)
-      .join("\n")
-      .slice(0, 4000);
-    client.channels.cache.get(config.guild.logChannelId).msg({
-      title: `Github \`${eventName}\` webhook handler`,
-      description,
-      author: { iconURL: sender?.avatar_url, name: sender?.type },
-    });
 
     EventsManager.emitter.emit(Events.Commit, request.body);
   }
