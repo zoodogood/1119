989fae79f541437cab6dcccf450f13dd3003d219
zoodogood <zoodogood@gmail.com>
2024-05-31 13:21:17 +0300

    Use ChangelogDaemon

---
+++ src/lib/ChangelogDaemon/singleton.js
@@ -0,0 +1,12 @@
+import { Events as AppEvents } from "#constants/app/events.js";
+import { ChangelogDaemon } from "#lib/ChangelogDaemon/ChangelogDaemon.js";
+import EventsManager from "#lib/modules/EventsManager.js";
+import { Events } from "#server/api/extenral/accept_github_webhook.js";
+
+export const singleton = new ChangelogDaemon();
+
+EventsManager.emitter.once(AppEvents.BeforeLogin, async () => {
+  await singleton.file.load();
+});
+EventsManager.emitter.on(AppEvents.RequestSave, () => singleton.file.write());
+EventsManager.emitter.on(Events.Commit, (event) => singleton.onCommit(event));
