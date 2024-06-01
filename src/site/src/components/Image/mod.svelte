cdb54cc8fe3700f2fa276316275e34306aec70e3
zoodogood <zoodogood@gmail.com>
2024-06-01 09:24:15 +0300

    Add Image component
    + Improve: broken images on the site look better
    + Improve: the images on the site load lazily

---
+++ src/site/src/components/Image/mod.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let src = "";
+  export let alt = "";
+  export let className = "";
+  let broken = false;
+</script>
+
+<img
+  {alt}
+  {src}
+  loading="lazy"
+  decoding="async"
+  class={className}
+  class:broken
+  on:error={() => (broken = true)}
+/>
+
+<style>
+  img.broken {
+    overflow: visible;
+  }
+
+  img[alt]::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: var(--background-theme-accent);
+    filter: contrast(0.9);
+    top: 0;
+    left: 0;
+
+    display: flex;
+    align-items: end;
+    justify-content: end;
+
+    font-family: "Icon";
+    font-size: 1.2em;
+    color: color-mix(in srgb, currentColor, transparent 50%);
+  }
+</style>
