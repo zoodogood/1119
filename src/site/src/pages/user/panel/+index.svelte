2de1953ff7ea0d318f3393fcbb39b1e3ca431773
zoodogood <zoodogood@gmail.com>
2024-06-02 22:19:55 +0300

    Refact; Move code to UserGuildNav

--- src/site/src/pages/user/panel/+index.svelte
+++ src/site/src/pages/user/panel/+index.svelte
@@ -16,6 +16,7 @@
   import { onMount } from "svelte";
 
   import { init_pwa_worker } from "#site/lib/init_pwa.js";
+  import UserGuildsNav from "#site/pages/user/panel/UserGuildsNav.svelte";
   import { getNotificationsContext } from "svelte-notifications";
   const { addNotification } = getNotificationsContext();
   const hashStore = svelteApp.Hash.store;
@@ -110,56 +111,7 @@
 </script>
 
 <Layout>
-  <ul
-    style:--elements-size="{2.85 - State.guilds?.length * 0.15}em"
-    class="guilds-list"
-  >
-    {#each State.guilds as guild}
-      {@const onClick = () =>
-        State.target.id !== guild.id
-          ? svelteApp.Hash.include({ key: "guildId", value: guild.id }).apply()
-          : svelteApp.Hash.remove("guildId").apply()}
-      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-      <li
-        title="Сервер {guild.name}"
-        class:selected={State.target.id === guild.id}
-        data-id={guild.id}
-        on:click={onClick}
-        on:keydown={onClick}
-        on:contextmenu|preventDefault={() =>
-          navigator.clipboard
-            .writeText(guild.id)
-            .then(() =>
-              addNotification({
-                text: `ID скопирован`,
-                position: "bottom-center",
-                removeAfter: 5_000,
-              }),
-            )
-            .catch(() =>
-              addNotification({
-                text: `Неудалось скопировать ID`,
-                position: "bottom-center",
-                removeAfter: 5_000,
-              }),
-            )}
-      >
-        {#if guild.iconURL}
-          <Image src={guild.iconURL} alt="guild-icon" class="icon guild_icon" />
-        {:else}
-          <span class="icon">{guild.name.at(0)}</span>
-        {/if}
-      </li>
-    {/each}
-    <hr />
-    <li class="invite" title="Пригласить">
-      <a target="_blank" href={svelteApp.getBot().invite} rel="noreferrer">
-        <span class="icon">
-          {"+"}
-        </span>
-      </a>
-    </li>
-  </ul>
+  <UserGuildsNav {State} />
 
   <main class="page-main">
     <section>
@@ -168,7 +120,7 @@
           <Image
             src={State.target.icon}
             alt="guild-icon"
-            class="icon guild_icon"
+            className="icon guild_icon"
           />
         {:else}
           <span class="icon">{State.target.name?.at(0) ?? "/"}</span>
@@ -216,172 +168,26 @@
 </Layout>
 
 <style>
-  .guilds-list {
-    --offset: 0;
-    font-size: max(0.5em, var(--elements-size, 0em));
-    display: flex;
-    list-style: none;
-    gap: 0.15em;
-
-    background-color: #88888810;
-    border-radius: 0.2em;
-    padding: 0.075em;
-
-    align-items: center;
-    opacity: 0.8;
-    transition: opacity 1s;
-
-    scrollbar-width: none;
-  }
-
-  .guilds-list:hover {
-    opacity: 1;
-  }
-
-  @media (max-width: 980px) {
-    .guilds-list {
-      left: 50%;
-      transform: translateX(-50%);
-      position: absolute;
-      max-width: 90vw;
-      top: var(--offset);
-      flex-direction: row;
-
-      overflow-x: auto;
-      overscroll-behavior-x: contain;
-      scroll-snap-type: x mandatory;
-
-      padding-inline: 0.5em;
-      padding-bottom: 0.2em;
-      margin-top: 1vh;
-    }
-
-    .guilds-list > li {
-      margin-top: var(--margin, 0);
-    }
-
-    .guilds-list hr {
-      width: 3px;
-      height: 15px;
-    }
-
+  @media (width < 980px) {
     .page-main {
       padding-top: 10vh;
     }
   }
 
-  @media (min-width: 980px) {
-    .guilds-list {
-      top: 50%;
-      transform: translateY(-50%);
-      position: fixed;
-      max-height: 70vh;
-      left: var(--offset);
-      flex-direction: column;
-
-      overflow-y: auto;
-      overscroll-behavior-y: contain;
-      scroll-snap-type: y mandatory;
-
-      padding-block: 0.5em;
-      padding-right: 0.2em;
-    }
-
-    .guilds-list > li {
-      margin-left: var(--margin, 0);
-    }
-
-    .guilds-list hr {
-      width: 15px;
-      height: 3px;
-    }
-
+  @media (width >= 980px) {
     .page-main {
       padding-left: 5vw;
     }
-  }
-
-  .guilds-list > li {
-    --size: 0.5em + 0.5vw + 25px;
-    width: calc(var(--size));
-    font-size: calc(var(--size) / 2);
-    aspect-ratio: 1 / 1;
-    border-radius: 50%;
-    overflow: hidden;
-    cursor: pointer;
-
-    display: flex;
-    flex-shrink: 0;
-
-    scroll-snap-align: start;
-
-    position: relative;
-
-    filter: brightness(0.7);
-    transition: filter 300ms;
-  }
-
-  .guilds-list > li > * {
-    flex-grow: 1;
-  }
-
-  .guilds-list > hr:first-child {
-    display: none;
-  }
-
-  .icon {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: var(--dark);
-    color: var(--white);
-    width: 100%;
-    height: 100%;
-
-    font-size: 0.5em;
-    font-weight: 600;
-    font-family: monospace;
-    user-select: none;
-  }
-
-  span.icon {
-    filter: contrast(0.7);
-  }
-
-  .guilds-list > li:hover {
-    border-radius: 20%;
-    background: #88888833;
-    filter: brightness(1);
-  }
-
-  .guilds-list > li.selected {
-    filter: brightness(1);
-    border-radius: 40%;
-  }
-
-  .guilds-list > li.invite {
-    filter: brightness(1);
-  }
-
-  .guilds-list > li.invite span {
-    font-weight: 100;
-    color: var(--white);
-    transition:
-      transform 300ms,
-      background-color 1s,
-      color 1s;
-  }
-
-  .guilds-list > li.invite:hover span {
-    font-weight: 100;
-    background-color: var(--main-color);
-    transform: rotateZ(360deg);
   }
 
   .icon-container {
     display: flex;
     width: calc(3vw + 30px);
     aspect-ratio: 1 / 1;
+    border-radius: 15px;
+  }
+
+  .icon-container :global(.guild_icon) {
     border-radius: 15px;
   }
 </style>
