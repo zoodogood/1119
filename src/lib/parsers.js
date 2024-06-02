03661181f9be7e8acaa8c329613fb83e861ad916
zoodogood <zoodogood@gmail.com>
2024-06-02 20:22:37 +0300

    Clean a code

--- src/lib/parsers.js
+++ src/lib/parsers.js
@@ -2,13 +2,36 @@ import { DAY, HOUR, MINUTE, SECOND } from "#constants/globals/time.js";
 import dayjs from "dayjs";
 
 export class ParserTime {
-  regex =
-    /(?<time>\d\d:\d\d)|(?<date>\d\d\.\d\d)|(?<stamp>\d+\s?(д|d|ч|h|м|m|с|s)\.?\S*?)/i;
-
-  items = [];
-  time = 0;
   /**@type {ReturnType<dayjs>} */
   #date;
+
+  items = [];
+  regex =
+    /(?<time>\d\d:\d\d)|(?<date>\d\d\.\d\d)|(?<stamp>\d+\s?(д|d|ч|h|м|m|с|s)\.?\S*?)/i;
+  time = 0;
+
+  static _getActiveGroupName(groups) {
+    for (const group in groups) {
+      if (groups[group]) {
+        return group;
+      }
+    }
+    return null;
+  }
+
+  appendDate(date) {
+    const [day, month] = date.split(".").map(Number);
+    this.date = this.date.set("date", day).set("month", month - 1);
+  }
+
+  appendStamp(stamp) {
+    this.time += this.stampToNumber(stamp);
+  }
+
+  appendTime(time) {
+    const [hours, minutes] = time.split(":").map(Number);
+    this.date = this.date.set("hour", hours).set("minute", minutes);
+  }
 
   /**@type {ReturnType<dayjs>} */
   get date() {
@@ -20,16 +43,27 @@ export class ParserTime {
     this.#date = value;
   }
 
-  pushItem(item) {
-    this.items.push(item);
-  }
-
-  summarizeItems() {
-    for (const item of this.items) {
-      this.processItem(item);
+  diffDateTime(compare) {
+    if (!this.#date) {
+      return 0;
     }
 
-    return this.diffDateTime(Date.now()) + this.time;
+    return this.#date.toDate().getTime() - compare;
+  }
+
+  processItem(item) {
+    switch (item.key) {
+      case "time":
+        return this.appendTime(item.value);
+      case "date":
+        return this.appendDate(item.value);
+      case "stamp":
+        return this.appendStamp(item.value);
+    }
+  }
+
+  pushItem(item) {
+    this.items.push(item);
   }
 
   stampToNumber(stamp) {
@@ -47,37 +81,12 @@ export class ParserTime {
     return consts[key] * value;
   }
 
-  diffDateTime(compare) {
-    if (!this.#date) {
-      return 0;
+  summarizeItems() {
+    for (const item of this.items) {
+      this.processItem(item);
     }
 
-    return this.#date.toDate().getTime() - compare;
-  }
-
-  appendStamp(stamp) {
-    this.time += this.stampToNumber(stamp);
-  }
-
-  appendTime(time) {
-    const [hours, minutes] = time.split(":").map(Number);
-    this.date = this.date.set("hour", hours).set("minute", minutes);
-  }
-
-  appendDate(date) {
-    const [day, month] = date.split(".").map(Number);
-    this.date = this.date.set("date", day).set("month", month - 1);
-  }
-
-  processItem(item) {
-    switch (item.key) {
-      case "time":
-        return this.appendTime(item.value);
-      case "date":
-        return this.appendDate(item.value);
-      case "stamp":
-        return this.appendStamp(item.value);
-    }
+    return this.diffDateTime(Date.now()) + this.time;
   }
 
   static toNumber(string) {
@@ -90,14 +99,5 @@ export class ParserTime {
       parser.pushItem(item);
     }
     return parser.summarizeItems();
-  }
-
-  static _getActiveGroupName(groups) {
-    for (const group in groups) {
-      if (groups[group]) {
-        return group;
-      }
-    }
-    return null;
   }
 }
