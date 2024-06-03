bb0f653f551f042aa7fbd836b5bc1fd8be6f2891
zoodogood <zoodogood@gmail.com>
2024-06-03 07:50:15 +0300

    Clean a code

--- src/site/src/components/Notifications/wrap.svelte
+++ src/site/src/components/Notifications/wrap.svelte
@@ -1,39 +1,35 @@
+<script>
+  import Notifications from "svelte-notifications";
+</script>
+
 <Notifications>
-	<slot></slot>
+  <slot></slot>
 </Notifications>
 
 <style>
-	:global(.notification)
-	{
-		filter: brightness(0.7) contrast(0.9) opacity(0.9);
-		color: var( --text-theme-accent ) !important;
-		background-color: var( --background-theme-accent ) !important;
-		border-radius: 20px !important;
-		padding-left: 1em !important;
-		min-width: min(95vw, 400px) !important;
-		max-width: 95vw;
+  :global(.notification) {
+    filter: brightness(0.7) contrast(0.9) opacity(0.9);
+    color: var(--text-theme-accent) !important;
+    background-color: var(--background-theme-accent) !important;
+    border-radius: 20px !important;
+    padding-left: 1em !important;
+    min-width: min(95vw, 400px) !important;
+    max-width: 95vw;
 
-		transition: filter 200ms;
-	}
+    transition: filter 200ms;
+  }
 
-	:global(.notification:hover)
-	{
-		filter: none;
-	}
+  :global(.notification:hover) {
+    filter: none;
+  }
 
-	:global(.notification-content)
-	{
-		width: 350px !important;
-		font-size: 0.9em !important;
-	}
+  :global(.notification-content) {
+    width: 350px !important;
+    font-size: 0.9em !important;
+  }
 
-	:global(.notification-button)
-	{
-		color: var( --white ) !important;
-		border: none !important;
-	}
+  :global(.notification-button) {
+    color: var(--white) !important;
+    border: none !important;
+  }
 </style>
-
-<script>
-	import Notifications from "svelte-notifications";
-</script>
