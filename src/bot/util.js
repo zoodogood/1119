147d446a7c3889d69c4d101d5bd1bfc273f53584
zoodogood <zoodogood@gmail.com>
2024-06-09 09:54:43 +0300

    Add chunkBySize

--- src/bot/util.js
+++ src/bot/util.js
@@ -31,6 +31,12 @@ export async function awaitUserAccept({ name, message, channel, userData }) {
   return false;
 }
 
+export function chunkBySize(array, size) {
+  return Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
+    array.slice(index * size, index * size + size),
+  );
+}
+
 export function awaitReactOrMessage({
   target,
   user,
