f64626c4bc108e9bb2986fb9f01d72f63963df17
zoodogood <zoodogood@gmail.com>
2024-06-09 10:31:08 +0300

    Improve
    + Feature: provide flag for disable chat, logs channels at guild: `!setchat --remove, !setlogs --remove`

--- folder/commands/setchat.js
+++ folder/commands/setchat.js
@@ -1,4 +1,63 @@
-import { BaseCommand } from "#lib/BaseCommand.js";
+import { BaseCommand, BaseFlagSubcommand } from "#lib/BaseCommand.js";
+import { BaseCommandRunContext } from "#lib/CommandRunContext.js";
+import { CliParser } from "@zoodogood/utils/CliParser";
+class CommandRunContext extends BaseCommandRunContext {
+  parseCli(input) {
+    const parsed = new CliParser()
+      .setText(input)
+      .captureFlags(this.command.options.cliParser.flags)
+      .collect();
+    const values = parsed.resolveValues((capture) => capture?.toString());
+    this.setCliParsed(parsed, values);
+    return parsed;
+  }
+}
+
+class CommandDefaultBehaviour extends BaseFlagSubcommand {
+  async onProcess() {
+    const { interaction } = this.context;
+    const {
+      user,
+      guild,
+      message: { mentions },
+    } = interaction;
+    const type = "chatChannel";
+    const channel = mentions.channels.first() ?? interaction.channel;
+    guild.data[type] = channel.id;
+    interaction.msg({
+      title: `#${channel.name} канал стал чатом!`,
+      delete: 9000,
+    });
+
+    guild.logSend({
+      description: `Каналу #${channel.name} установили метку "чат"`,
+      author: { name: user.username, avatarURL: user.avatarURL() },
+    });
+  }
+}
+
+class Remove_FlagSubcommand extends BaseFlagSubcommand {
+  static FLAG_DATA = {
+    name: "--remove",
+    capture: ["--remove"],
+  };
+  async onProcess() {
+    const { guild, interaction } = this.context;
+    const type = "chatChannel";
+    await guild.logSend({
+      description: `Чат отключен`,
+      author: {
+        name: interaction.user.username,
+        avatarURL: interaction.user.avatarURL(),
+      },
+    });
+    delete guild.data[type];
+    interaction.msg({
+      title: "Чат канал отключен!",
+      delete: 9000,
+    });
+  }
+}
 class Command extends BaseCommand {
   options = {
     name: "setchat",
@@ -8,6 +67,9 @@ class Command extends BaseCommand {
         "Устанавливает для бота указанный канал, как чат, туда будет отправляться ежедневная статистика, а также не будут удалятся сообщения о повышении уровня.",
       example: `!setChat <channel>`,
     },
+    cliParser: {
+      flags: [Remove_FlagSubcommand.FLAG_DATA],
+    },
     alias: "установитьчат встановитичат",
     allowDM: true,
     type: "guild",
@@ -15,16 +77,26 @@ class Command extends BaseCommand {
   };
 
   async onChatInput(msg, interaction) {
-    const type = "chatChannel";
-    const guild = msg.guild;
-    const channel = msg.mentions.channels.first() ?? msg.channel;
-    guild.data[type] = channel.id;
-    msg.msg({ title: `#${channel.name} канал стал чатом!`, delete: 9000 });
+    const context = new CommandRunContext(interaction, this);
+    context.setWhenRunExecuted(this.run(context));
+    return context;
+  }
 
-    guild.logSend({
-      description: `Каналу #${channel.name} установили метку "чат"`,
-      author: { name: msg.author.username, avatarURL: msg.author.avatarURL() },
-    });
+  processRemoveFlag(context) {
+    const value = context.cliParsed.at(1).get("--remove");
+    if (!value) {
+      return false;
+    }
+    new Remove_FlagSubcommand(context).onProcess();
+    return true;
+  }
+
+  async run(context) {
+    context.parseCli(context.interaction.params);
+    if (await this.processRemoveFlag(context)) {
+      return;
+    }
+    await new CommandDefaultBehaviour(context).onProcess();
   }
 }
 
--- folder/commands/setlogs.js
+++ folder/commands/setlogs.js
@@ -1,4 +1,63 @@
-import { BaseCommand } from "#lib/BaseCommand.js";
+import { BaseCommand, BaseFlagSubcommand } from "#lib/BaseCommand.js";
+import { BaseCommandRunContext } from "#lib/CommandRunContext.js";
+import { CliParser } from "@zoodogood/utils/CliParser";
+class CommandRunContext extends BaseCommandRunContext {
+  parseCli(input) {
+    const parsed = new CliParser()
+      .setText(input)
+      .captureFlags(this.command.options.cliParser.flags)
+      .collect();
+    const values = parsed.resolveValues((capture) => capture?.toString());
+    this.setCliParsed(parsed, values);
+    return parsed;
+  }
+}
+
+class CommandDefaultBehaviour extends BaseFlagSubcommand {
+  async onProcess() {
+    const { interaction } = this.context;
+    const {
+      user,
+      guild,
+      message: { mentions },
+    } = interaction;
+    const type = "logChannel";
+    const channel = mentions.channels.first() ?? interaction.channel;
+    guild.data[type] = channel.id;
+    interaction.msg({
+      title: `Готово, в #${channel.name} будут отправляться логи сервера!`,
+      delete: 9000,
+    });
+
+    guild.logSend({
+      description: `Каналу #${channel.name} установили метку "логи"`,
+      author: { name: user.username, avatarURL: user.avatarURL() },
+    });
+  }
+}
+
+class Remove_FlagSubcommand extends BaseFlagSubcommand {
+  static FLAG_DATA = {
+    name: "--remove",
+    capture: ["--remove"],
+  };
+  async onProcess() {
+    const { guild, interaction } = this.context;
+    const type = "logChannel";
+    await guild.logSend({
+      description: `Логи отключены`,
+      author: {
+        name: interaction.user.username,
+        avatarURL: interaction.user.avatarURL(),
+      },
+    });
+    delete guild.data[type];
+    interaction.msg({
+      title: "Логи отключены!",
+      delete: 9000,
+    });
+  }
+}
 class Command extends BaseCommand {
   options = {
     name: "setlogs",
@@ -8,6 +67,9 @@ class Command extends BaseCommand {
         "Устанавливает для бота указанный канал, как логи, иначе говоря — журнал, туда будет записываться информация о применении пользователями небезопасных команд, статистике, важных изменениях и многом другом.\nНастоятельно рекомендуется создать такой канал, если его нет.",
       example: `!setLogs <channel>`,
     },
+    cliParser: {
+      flags: [Remove_FlagSubcommand.FLAG_DATA],
+    },
     alias: "установитьлоги встановитилоги",
     allowDM: true,
     type: "guild",
@@ -15,19 +77,26 @@ class Command extends BaseCommand {
   };
 
   async onChatInput(msg, interaction) {
-    const type = "logChannel";
-    const guild = msg.guild;
-    const channel = msg.mentions.channels.first() ?? msg.channel;
-    guild.data[type] = channel.id;
-    msg.msg({
-      title: `Готово. В #${channel.name} будут отправлятся логи сервера`,
-      delete: 9000,
-    });
+    const context = new CommandRunContext(interaction, this);
+    context.setWhenRunExecuted(this.run(context));
+    return context;
+  }
 
-    guild.logSend({
-      description: `Каналу #${channel.name} установили метку "логи"`,
-      author: { name: msg.author.username, avatarURL: msg.author.avatarURL() },
-    });
+  processRemoveFlag(context) {
+    const value = context.cliParsed.at(1).get("--remove");
+    if (!value) {
+      return false;
+    }
+    new Remove_FlagSubcommand(context).onProcess();
+    return true;
+  }
+
+  async run(context) {
+    context.parseCli(context.interaction.params);
+    if (await this.processRemoveFlag(context)) {
+      return;
+    }
+    await new CommandDefaultBehaviour(context).onProcess();
   }
 }
 
