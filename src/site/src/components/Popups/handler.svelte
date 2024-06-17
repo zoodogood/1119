2d33c3e592b65d395a0d3b464412188771c1411e
zoodogood <zoodogood@gmail.com>
2024-06-17 18:32:52 +0300

    Improve

--- src/site/src/components/Popups/handler.svelte
+++ src/site/src/components/Popups/handler.svelte
@@ -3,7 +3,7 @@
   export function createPopup(component, props = {}) {
     const popup = new component({ target: document.body, props });
     popups.push(popup);
-    popup.$on("request_destroy", () => removePopup(popup));
+    popup.$on("request_close", () => removePopup(popup));
     return popup;
   }
 
