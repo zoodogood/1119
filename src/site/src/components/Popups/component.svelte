d1a05fd49f6d0daf9edef40550183f35aeb91e4c
zoodogood <zoodogood@gmail.com>
2024-06-17 15:26:35 +0300

    Add popups handler

--- src/site/src/components/Popups/component.svelte
+++
@@ -1,6 +0,0 @@
-<script context="module">
-  const popups = [];
-  function createPopup(component, options) {
-    new component({ target: document.body, props: options });
-  }
-</script>
---
+++ src/site/src/components/Popups/handler.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  const popups = [];
+  export function createPopup(component, props = {}) {
+    const popup = new component({ target: document.body, props });
+    popups.push(popup);
+    popup.$on("request_destroy", () => removePopup(popup));
+    return popup;
+  }
+
+  export function removePopup(popup) {
+    popup.$destroy?.();
+    const index = popups.indexOf(popup);
+    index !== -1 && popups.splice(index, 1);
+  }
+</script>
