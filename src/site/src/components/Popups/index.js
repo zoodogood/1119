f07ed096fd1b11f066bc4d6f05dae45c635f9469
zoodogood <zoodogood@gmail.com>
2024-06-18 11:06:54 +0300

    Alterfix

--- src/site/src/components/Popups/index.js
+++ src/site/src/components/Popups/index.js
@@ -1,2 +1,2 @@
 export { default as PopupBox } from "./PopupBox.svelte";
-export { default as PopupsHandler } from "./component.svelte";
+export { default as PopupsHandler } from "./handler.svelte";
