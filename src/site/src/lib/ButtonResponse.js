dafe484d4b85e8b66111a6ed40ce9653dafd6e90
zoodogood <zoodogood@gmail.com>
2024-06-18 11:07:15 +0300

    Improve

--- src/site/src/lib/ButtonResponse.js
+++ src/site/src/lib/ButtonResponse.js
@@ -1,19 +1,20 @@
 import writeError from "#site/lib/writeErrorToServer.js";
 
 export async function ButtonResponse(source, clickEvent, callback) {
-  const label = clickEvent.target.innerText;
   const { target } = clickEvent;
   const LOADING_STATE = "Брмм";
   try {
-    target.innerText = LOADING_STATE;
+    target.append(LOADING_STATE);
     await callback(clickEvent);
   } catch (cause) {
-    target.innerText = `Информация об ошибке записана`;
-    target.title = cause.message;
+    target.append(`Err: ${cause.message}`);
     const error = new Error(`ButtonResponse with ${source}`, { cause });
     writeError(error);
     console.error(error);
     return;
+  } finally {
+    [...target.childNodes]
+      .find((text) => text.data === LOADING_STATE)
+      ?.remove();
   }
-  label !== LOADING_STATE && (clickEvent.target.innerText = label);
 }
