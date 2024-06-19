5b0d124975c213cff5ddb643ca8df5a2d2cd98f1
zoodogood <zoodogood@gmail.com>
2024-06-19 13:43:07 +0300

    Add command
    + Feature: add command: !execute thats allows execute multiple commands in one message

---
+++ folder/commands/execute.js
@@ -0,0 +1,117 @@
+import client from "#bot/client.js";
+import { BaseCommand, BaseFlagSubcommand } from "#lib/BaseCommand.js";
+import { BaseCommandRunContext } from "#lib/CommandRunContext.js";
+import CommandsManager, {
+  parseInputCommandFromMessage,
+} from "#lib/modules/CommandsManager.js";
+import { sleep } from "#lib/safe-utils.js";
+import { CliParser } from "@zoodogood/utils/CliParser";
+import { Message } from "discord.js";
+
+class CommandRunContext extends BaseCommandRunContext {
+  period_delay = 0;
+  separator = ";";
+  parseCli(input) {
+    const parsed = new CliParser()
+      .setText(input)
+      .processBrackets()
+      .captureFlags(this.command.options.cliParser.flags)
+      .captureResidue({ name: "residue" })
+      .collect();
+    this.captures = parsed.captures;
+
+    this.separator =
+      this.captures.get("--separator")?.valueOfFlag() || this.separator;
+
+    this.period = this.captures.get("--period")?.valueOfFlag() || this.period;
+    return parsed;
+  }
+}
+
+class CommandDefaultBehaviour extends BaseFlagSubcommand {
+  async onProcess() {
+    const { captures, separator, period } = this.context;
+    const residue = captures.get("residue").toString();
+    const executable = residue.split(separator);
+    const { message: original } = this.context.interaction;
+
+    for (const content of executable) {
+      const clone = Object.create(Message.prototype);
+      Object.assign(clone, {
+        ...original,
+        content,
+        client,
+      });
+      const commandContext = parseInputCommandFromMessage(clone);
+      const command = commandContext?.command;
+      if (
+        commandContext &&
+        CommandsManager.checkAvailable(command, commandContext)
+      ) {
+        CommandsManager.execute(command, commandContext);
+      }
+
+      period && (await sleep(period));
+    }
+  }
+}
+class Command extends BaseCommand {
+  daemon;
+  options = {
+    name: "execute",
+    id: 68,
+    media: {
+      description:
+        "Позволяет выполнить несколько команд, вызвав одно сообщение",
+      example: `!execute "!boss -a ; !boss -s"`,
+    },
+    alias: "выполнить",
+    allowDM: true,
+    cooldown: 10_000,
+    cooldownTry: 3,
+    type: "guild",
+    expectParams: true,
+    slash: {
+      name: "execute",
+      description: "Allow execute chat commands",
+    },
+    cliParser: {
+      flags: [
+        {
+          name: "--separator",
+          capture: ["--separator", "-s"],
+          expectValue: true,
+          description: "Символ разделитель между командами",
+        },
+        {
+          name: "--period",
+          capture: ["--period"],
+          expectValue: true,
+          description: "Вводит задержку между применениями",
+        },
+      ],
+    },
+    accessibility: {
+      publicized_on_level: 20,
+    },
+  };
+  async onChatInput(message, interaction) {
+    const context = await CommandRunContext.new(interaction, this);
+    context.setWhenRunExecuted(this.run(context));
+    return context;
+  }
+  onSlash() {}
+
+  /**
+   *
+   * @param {CommandRunContext} context
+   * @returns {CommandRunContext}
+   */
+  async run(context) {
+    context.parseCli(context.interaction.params);
+    await new CommandDefaultBehaviour(context).onProcess();
+    return;
+  }
+}
+
+export default Command;
