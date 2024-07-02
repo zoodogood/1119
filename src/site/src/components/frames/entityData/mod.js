075ab4d130432b7fb6ad11ec5af00f33179003c3
zoodogood <zoodogood@gmail.com>
2024-07-02 06:57:44 +0300

    Clean a code

---
+++ src/site/src/components/frames/entityData/mod.js
@@ -0,0 +1,4 @@
+export { default as GuildProgress } from "./GuildProgress.svelte";
+export { default as GuildSettings } from "./GuildSettings.svelte";
+export { default as UserProgress } from "./UserProgress.svelte";
+export { default as UserSettings } from "./UserSettings.svelte";
--- src/site/src/components/frames/entityData/mod.svelte
+++
@@ -1,9 +0,0 @@
-<script context = "module">
-  	import GuildProgress from "./GuildProgress.svelte";
-  	import UserProgress from "./UserProgress.svelte";
-	import GuildSettings from "./GuildSettings.svelte";
-	import UserSettings from "./UserSettings.svelte";
-
-	export {GuildSettings, UserSettings, GuildProgress, UserProgress};
-
-</script>
