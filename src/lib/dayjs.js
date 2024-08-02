b9f248b2f97dd02dc0092203a15bd408802f12d2
zoodogood <zoodogood@gmail.com>
2024-08-02 13:29:59 +0300

    Add dayjs_ensure_coming_year

--- src/lib/dayjs.js
+++ src/lib/dayjs.js
@@ -3,4 +3,15 @@ import duration from "dayjs/plugin/duration.js";
 import toObject from "dayjs/plugin/toObject.js";
 dayjs.extend(duration);
 dayjs.extend(toObject);
+
+/**
+ * @param {dayjs.Dayjs} date
+ */
+export function dayjs_ensure_coming_year(date) {
+  if (date.isAfter()) {
+    return date;
+  }
+  return date.add(1, "year");
+}
+
 export { dayjs };
