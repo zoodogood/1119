5a597ddfa7c5a5bb7346a700aa4e05f3dce05023
zoodogood <zoodogood@gmail.com>
2024-08-07 07:07:51 +0300

    Alterfix

--- folder/scripts/checkFiles.js
+++ folder/scripts/checkFiles.js
@@ -7,19 +7,13 @@ const resolve = (path) => Path.resolve(root, path);
 const isWindowsBased = process.platform === "win32";
 
 const FileExists = async (path) =>
-  !!(await FileSystem.stat(resolve(path)).catch((err) => null));
-
-const Write = async (path, content) =>
-  await FileSystem.writeFile(resolve(path), content);
+  !!(await FileSystem.stat(resolve(path)).catch(() => null));
 
 const Paths = {
   env: ".env",
   envExample: "folder/development/.env.example",
   config: "src/config.json.js",
   configExample: "folder/development/config.json.js.example",
-  main: "folder/data/main.json",
-  counter: "folder/data/counters.json",
-  time: "folder/data/time.json",
 
   firstBuildDocs: "static/special/first-build.html",
 };
@@ -41,31 +35,6 @@ const Paths = {
     const target = resolve(Paths.config);
     await FileSystem.copyFile(source, target);
     console.info(`CREATED: ${Paths.config}`);
-  })());
-
-!(await FileExists(Paths.main)) &&
-  (await (async () => {
-    const BOT_DEFAULT_DATA = { commandsUsed: {} };
-    const DEFAULT_DATA = { bot: BOT_DEFAULT_DATA, users: [], guilds: [] };
-    const content = JSON.stringify(DEFAULT_DATA);
-    await Write(Paths.main, content);
-    console.info(`CREATED: ${Paths.main}`);
-  })());
-
-!(await FileExists(Paths.counter)) &&
-  (await (async () => {
-    const DEFAULT_DATA = [];
-    const content = JSON.stringify(DEFAULT_DATA);
-    await Write(Paths.counter, content);
-    console.info(`CREATED: ${Paths.counter}`);
-  })());
-
-!(await FileExists(Paths.time)) &&
-  (await (async () => {
-    const DEFAULT_DATA = {};
-    const content = JSON.stringify(DEFAULT_DATA);
-    await Write(Paths.time, content);
-    console.info(`CREATED: ${Paths.time}`);
   })());
 
 console.info("Next.\n");
