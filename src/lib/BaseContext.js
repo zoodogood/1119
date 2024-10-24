adcdccf3ad4419203d2ca6dab7428685d692114d
zoodogood <zoodogood@gmail.com>
2024-10-24 20:03:47 +0300

    Fix; Implement toSafeValues for BaseContext

--- src/lib/BaseContext.js
+++ src/lib/BaseContext.js
@@ -1,5 +1,24 @@
 import EventsEmitter from "events";
 
+// [Unsafe_primitives]: string[]
+export const Unsafe_primitives = Symbol("unsafe_primitives");
+
+function to_safe_values_default(values, transformer) {
+  const unsafe_primitives = values[Unsafe_primitives];
+  const map = ([key, value]) => {
+    const newValue =
+      value && typeof value === "object"
+        ? value.toSafeValues(transformer)
+        : !unsafe_primitives || !unsafe_primitives.includes(key);
+    return [key, newValue];
+  };
+  return Object.fromEntries(
+    Object.entries(values)
+      .map(map)
+      .filter(([_key, value]) => value),
+  );
+}
+
 export class BaseContext {
   channel = null;
   contextedAt = Date.now();
@@ -16,14 +35,16 @@ export class BaseContext {
 
   // Expected a toSafeValues will be overridden by the situation
   toSafeValues() {
-    return {
-      createdAt: this.createdAt,
+    const values = {
+      contextedAt: this.contextedAt,
       source: this._source,
       is: this.constructor.name,
       events: Object.entries(this.emitter._events).map(([key, value]) => [
         key,
         value.length,
       ]),
+      ...to_safe_values_default(this),
     };
+    return values;
   }
 }
