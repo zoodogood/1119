4c9b28811526d862a72f6b84e6b930ff79cefed3
zoodogood <zoodogood@gmail.com>
2024-10-24 20:02:23 +0300

    Implement toSafeValues for Guild, BaseChannel; Improve

--- src/lib/expand_prototype__to_safe_values.js
+++ src/lib/expand_prototype__to_safe_values.js
@@ -1,11 +1,22 @@
-import { BaseInteraction, User } from "discord.js";
+import { BaseChannel, BaseInteraction, Guild, User } from "discord.js";
 
 BaseInteraction.prototype.toSafeValues = function () {
   return {
-    ...this.user.toSafeValues(),
+    user: this.user.toSafeValues(),
   };
 };
 
 User.prototype.toSafeValues = function () {
-  return { ...this };
+  const { id, username } = this;
+  return { id, username };
+};
+
+Guild.prototype.toSafeValues = function () {
+  const { id, name } = this;
+  return { id, name };
+};
+
+BaseChannel.prototype.toSafeValues = function () {
+  const { id, name } = this;
+  return { id, name };
 };
