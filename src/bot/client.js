6a52e35caf0183a2e4657639e879a241e2c0754a
zoodogood <zoodogood@gmail.com>
2024-11-21 11:28:33 +0200

    Clean a code

--- src/bot/client.js
+++ src/bot/client.js
@@ -1,9 +1,8 @@
-import { Partials, GatewayIntentBits } from "discord.js";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
 
 const client = new Client({
   messageCacheMaxSize: 110,
-  intents: [...Object.values(GatewayIntentBits)],
+  intents: Object.values(GatewayIntentBits),
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
   allowedMentions: { repliedUser: true, parse: ["users"] },
 });
