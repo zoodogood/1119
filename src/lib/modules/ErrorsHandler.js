6522df370dbee02a7354ef13c3e17c5cf74444d0
zoodogood <zoodogood@gmail.com>
2024-12-07 12:53:25 +0200

    Clean a code

--- src/lib/modules/ErrorsHandler.js
+++ src/lib/modules/ErrorsHandler.js
@@ -5,345 +5,345 @@ import { dayjs, mapGetOrInsert } from "#lib/util.js";
 const { stringify, parse } = JSON;
 
 class Metadata {
-  #updateRequested = false;
+	static defaults = {};
 
-  static defaults = {};
+	#updateRequested = false;
 
-  constructor() {
-    const { defaults } = this.constructor;
-    Object.assign(this, defaults);
-  }
+	constructor() {
+		const { defaults } = this.constructor;
+		Object.assign(this, defaults);
+	}
 
-  appendMetadata() {
-    throw new Error("Must be implemented");
-  }
+	static from(props) {
+		return Object.assign(Object.create(this.prototype), props);
+	}
 
-  static from(props) {
-    return Object.assign(Object.create(this.prototype), props);
-  }
+	appendMetadata() {
+		throw new Error("Must be implemented");
+	}
 
-  requestUpdate() {
-    this.#updateRequested = true;
-  }
-  get updateRequested() {
-    return this.#updateRequested;
-  }
+	requestUpdate() {
+		this.#updateRequested = true;
+	}
+	get updateRequested() {
+		return this.#updateRequested;
+	}
 
-  set updateRequested(value) {
-    this.#updateRequested = value;
-  }
+	set updateRequested(value) {
+		this.#updateRequested = value;
+	}
 }
 
 class SessionMetadata extends Metadata {
-  static defaults = {
-    commentsCount: null,
-    uniqueTags: new Set(),
-    errorsCount: null,
-    uniqueErrors: new Set(),
-  };
+	static defaults = {
+		commentsCount: null,
+		uniqueTags: new Set(),
+		errorsCount: null,
+		uniqueErrors: new Set(),
+	};
 
-  appendCommentsCount(value) {
-    this.commentsCount = value;
-  }
+	appendCommentsCount(value) {
+		this.commentsCount = value;
+	}
 
-  appendErrorsCount(value) {
-    this.errorsCount = value;
-  }
+	appendErrorsCount(value) {
+		this.errorsCount = value;
+	}
 
-  appendMetadata({ commentsCount, uniqueTags, errorsCount, uniqueErrors }) {
-    commentsCount && this.appendCommentsCount(commentsCount);
-    uniqueTags && this.appendTags(uniqueTags);
-    errorsCount && this.appendErrorsCount(errorsCount);
-    uniqueErrors && this.appendUniqueErrors(uniqueErrors);
-  }
+	appendMetadata({ commentsCount, uniqueTags, errorsCount, uniqueErrors }) {
+		commentsCount && this.appendCommentsCount(commentsCount);
+		uniqueTags && this.appendTags(uniqueTags);
+		errorsCount && this.appendErrorsCount(errorsCount);
+		uniqueErrors && this.appendUniqueErrors(uniqueErrors);
+	}
 
-  appendTags(uniqueTags) {
-    for (const tag of uniqueTags) {
-      this.uniqueTags.add(tag);
-    }
-  }
+	appendTags(uniqueTags) {
+		for (const tag of uniqueTags) {
+			this.uniqueTags.add(tag);
+		}
+	}
 
-  appendUniqueErrors(messages) {
-    for (const message of messages) {
-      this.uniqueErrors.add(message);
-    }
-  }
+	appendUniqueErrors(messages) {
+		for (const message of messages) {
+			this.uniqueErrors.add(message);
+		}
+	}
 }
 
 class GroupMetadata extends Metadata {
-  appendComment(data) {
-    this.comments ||= [];
-    this.comments.push(data);
-  }
+	appendComment(data) {
+		this.comments ||= [];
+		this.comments.push(data);
+	}
 
-  appendErrorsCount(value) {
-    this.errorsCount = value;
-  }
+	appendErrorsCount(value) {
+		this.errorsCount = value;
+	}
 
-  appendMetadata({ comments, tags, errorsCount }) {
-    comments && this.appendComment(comments);
-    tags && this.appendTags(tags);
-    errorsCount && this.appendErrorsCount(errorsCount);
-  }
+	appendMetadata({ comments, tags, errorsCount }) {
+		comments && this.appendComment(comments);
+		tags && this.appendTags(tags);
+		errorsCount && this.appendErrorsCount(errorsCount);
+	}
 
-  appendTags(tags) {
-    this.uniqueTags ||= new Set();
-    for (const tag of tags) {
-      this.uniqueTags.add(tag);
-    }
-  }
+	appendTags(tags) {
+		this.uniqueTags ||= new Set();
+		for (const tag of tags) {
+			this.uniqueTags.add(tag);
+		}
+	}
 }
 
 class ErrorData {
-  constructor(error, context) {
-    this.tags = Object.keys(context ?? {});
-    context &&= stringify(context);
-    this.error = error;
-    try {
-      this.stackData = this.parseErrorStack();
-    } catch (error) {
-      console.error(error);
-    }
-    this.createdAt = Date.now();
-    this.context = context ?? null;
-  }
+	constructor(error, context) {
+		this.tags = Object.keys(context ?? {});
+		context &&= stringify(context);
+		this.error = error;
+		try {
+			this.stackData = this.parseErrorStack();
+		} catch (error) {
+			console.error(error);
+		}
+		this.createdAt = Date.now();
+		this.context = context ?? null;
+	}
 
-  static from(data) {
-    return Object.assign(Object.create(ErrorData.prototype), data);
-  }
+	static from(data) {
+		return Object.assign(Object.create(ErrorData.prototype), data);
+	}
 
-  static fromError(error, context) {
-    new ErrorData(error, context);
-  }
+	static fromError(error, context) {
+		new ErrorData(error, context);
+	}
 
-  get message() {
-    return this.error.message;
-  }
+	parseErrorStack({ node_modules } = {}) {
+		let stack = this.error.stack;
+		try {
+			stack = decodeURI(stack).replaceAll("\\", "/");
+		} catch {
+			stack = `!decodeError of stack\n${stack}`;
+		}
 
-  parseErrorStack({ node_modules } = {}) {
-    let stack = this.error.stack;
-    try {
-      stack = decodeURI(stack).replaceAll("\\", "/");
-    } catch (error) {
-      stack = `!decodeError of stack\n${stack}`;
-    }
+		stack ||= "null";
+		const projectPath = process.cwd().replaceAll("\\", "/");
+		const regular = new RegExp(
+			`(?<fileOfError>${projectPath}/.+?\\.js):(?<strokeOfError>\\d+)`,
+		);
+		const groups = stack.match(regular)?.groups;
+		if (!groups) {
+			return undefined;
+		}
+		const { fileOfError } = groups;
+		if (node_modules === false && fileOfError.includes("node_modules")) {
+			return null;
+		}
+		return { ...groups, stack };
+	}
 
-    stack ||= "null";
-    const projectPath = process.cwd().replaceAll("\\", "/");
-    const regular = new RegExp(
-      `(?<fileOfError>${projectPath}/.+?\\.js):(?<strokeOfError>\\d+)`,
-    );
-    const groups = stack.match(regular)?.groups;
-    if (!groups) {
-      return undefined;
-    }
-    const { fileOfError } = groups;
-    if (node_modules === false && fileOfError.includes("node_modules")) {
-      return null;
-    }
-    return { ...groups, stack };
-  }
+	get message() {
+		return this.error.message;
+	}
 
-  get stack() {
-    return this.error.stack;
-  }
+	get stack() {
+		return this.error.stack;
+	}
 }
 
 class Group {
-  constructor(key) {
-    this.meta = new GroupMetadata();
-    this.errors = [];
-    this.key = key;
-  }
+	constructor(key) {
+		this.meta = new GroupMetadata();
+		this.errors = [];
+		this.key = key;
+	}
 
-  addComment({ responseText, id }) {
-    const meta = this.meta;
-    const comment = { responseText, id };
-    meta.appendComment(comment);
-  }
+	addComment({ responseText, id }) {
+		const meta = this.meta;
+		const comment = { responseText, id };
+		meta.appendComment(comment);
+	}
 
-  onErrorReceive(errorData) {
-    this.pushError(errorData);
+	onErrorReceive(errorData) {
+		this.pushError(errorData);
 
-    this.meta.appendMetadata({
-      tags: errorData.tags,
-      errorsCount: this.errors.length,
-    });
-  }
+		this.meta.appendMetadata({
+			tags: errorData.tags,
+			errorsCount: this.errors.length,
+		});
+	}
 
-  pushError(errorData) {
-    this.errors.push(errorData);
-  }
+	pushError(errorData) {
+		this.errors.push(errorData);
+	}
 }
 
 class SessionsMetadataCache {
-  #cache = new Map();
+	#cache = new Map();
 
-  async _fetchAndSet(key) {
-    const json = await FileUtils.readFile(key);
-    this.#cache.set(key, json?.meta);
-  }
+	async _fetchAndSet(key) {
+		const json = await FileUtils.readFile(key);
+		this.#cache.set(key, json?.meta);
+	}
 
-  async fetch(key) {
-    const cache = this.#cache;
-    !cache.has(key) && (await this._fetchAndSet(key));
+	async fetch(key) {
+		const cache = this.#cache;
+		!cache.has(key) && (await this._fetchAndSet(key));
 
-    return cache.get(key);
-  }
+		return cache.get(key);
+	}
 }
 
 class FileUtils {
-  static directory = `errors`;
+	static directory = `errors`;
 
-  static async keys() {
-    const files = await StorageManager.keys(this.directory);
-    const suffix = ".json";
-    const filtered = files.filter((name) => name.endsWith(suffix));
-    const keys = filtered.map((name) => name.replace(suffix, ""));
-    return keys;
-  }
-  static normalizeName(name) {
-    if (name.endsWith(".json")) {
-      name = name.replace(/\.json$/, "");
-    }
-    return name;
-  }
+	static async keys() {
+		const files = await StorageManager.keys(this.directory);
+		const suffix = ".json";
+		const filtered = files.filter((name) => name.endsWith(suffix));
+		const keys = filtered.map((name) => name.replace(suffix, ""));
+		return keys;
+	}
+	static normalizeName(name) {
+		if (name.endsWith(".json")) {
+			name = name.replace(/\.json$/, "");
+		}
+		return name;
+	}
 
-  static async readFile(file) {
-    const path = `${this.directory}/${file}.json`;
-    const data = await StorageManager.read(path);
-    return parse(data);
-  }
+	static async readFile(file) {
+		const path = `${this.directory}/${file}.json`;
+		const data = await StorageManager.read(path);
+		return parse(data);
+	}
 
-  static async write(fileName, data) {
-    const path = `${this.directory}/${fileName}.json`;
+	static async write(fileName, data) {
+		const path = `${this.directory}/${fileName}.json`;
 
-    data = stringify(data);
-    await StorageManager.write(path, data);
-  }
+		data = stringify(data);
+		await StorageManager.write(path, data);
+	}
 }
 
 class Core {
-  /** 
+	/** 
    @typedef {object} ICoreStore
    @property {Map<string, Group>} errorGroups
    @property { SessionMetadata } meta
   */
-  static cache = new SessionsMetadataCache();
+	static cache = new SessionsMetadataCache();
 
-  static filesList = [];
+	static filesList = [];
 
-  /**@type {ICoreStore} */
-  static session = {
-    errorGroups: new Map(),
-    meta: new SessionMetadata(),
-  };
-  static toJSON() {
-    const { errorGroups, meta } = this.session;
-    const groups = [...errorGroups.values()];
-    return { groups, meta };
-  }
+	/**@type {ICoreStore} */
+	static session = {
+		errorGroups: new Map(),
+		meta: new SessionMetadata(),
+	};
+	static toJSON() {
+		const { errorGroups, meta } = this.session;
+		const groups = [...errorGroups.values()];
+		return { groups, meta };
+	}
 
-  static updateSessionMetadata({ force = false } = {}) {
-    const { errorGroups, meta } = this.session;
-    if (!force && !meta.updateRequested) {
-      return;
-    }
+	static updateSessionMetadata({ force = false } = {}) {
+		const { errorGroups, meta } = this.session;
+		if (!force && !meta.updateRequested) {
+			return;
+		}
 
-    const groups = [...errorGroups.values()];
-    meta.appendMetadata({
-      uniqueTags: groups.reduce(
-        (acc, { meta }) => (acc.push(...meta.uniqueTags), acc),
-        [],
-      ),
-      commentsCount: groups.reduce(
-        (acc, { meta }) => acc + (meta.comments?.length ?? 0),
-        0,
-      ),
-      errorsCount: groups.reduce((acc, { errors }) => acc + errors.length, 0),
-      uniqueErrors: [...errorGroups.keys()],
-    });
-    meta.updateRequested = false;
-  }
+		const groups = [...errorGroups.values()];
+		meta.appendMetadata({
+			uniqueTags: groups.reduce(
+				(acc, { meta }) => (acc.push(...meta.uniqueTags), acc),
+				[],
+			),
+			commentsCount: groups.reduce(
+				(acc, { meta }) => acc + (meta.comments?.length ?? 0),
+				0,
+			),
+			errorsCount: groups.reduce((acc, { errors }) => acc + errors.length, 0),
+			uniqueErrors: [...errorGroups.keys()],
+		});
+		meta.updateRequested = false;
+	}
 }
 
 class Manager {
-  static Core = Core;
-  static File = FileUtils;
+	static Core = Core;
+	static File = FileUtils;
 
-  static actualSessionMetadata() {
-    Core.updateSessionMetadata();
-    return Core.session.meta;
-  }
+	static actualSessionMetadata() {
+		Core.updateSessionMetadata();
+		return Core.session.meta;
+	}
 
-  static errorLogger(error) {
-    console.error(`[${dayjs().format("HH:mm")}]`, error);
-  }
+	static errorLogger(error) {
+		console.error(`[${dayjs().format("HH:mm")}]`, error);
+	}
 
-  static async fetchManyMetadata() {
-    const current = this.actualSessionMetadata();
-    const cache = [
-      ...(
-        await Promise.all(Core.filesList.map((key) => Core.cache.fetch(key)))
-      ).values(),
-    ];
-    return { current, cache };
-  }
+	static async fetchManyMetadata() {
+		const current = this.actualSessionMetadata();
+		const cache = [
+			...(
+				await Promise.all(Core.filesList.map((key) => Core.cache.fetch(key)))
+			).values(),
+		];
+		return { current, cache };
+	}
 
-  /**
-   *
-   * @param {string} key
-   * @returns {Group}
-   */
-  static getErrorsGroupBy(key) {
-    return mapGetOrInsert(Core.session.errorGroups, key, new Group(key));
-  }
+	/**
+	 *
+	 * @param {string} key
+	 * @returns {Group}
+	 */
+	static getErrorsGroupBy(key) {
+		return mapGetOrInsert(Core.session.errorGroups, key, new Group(key));
+	}
 
-  static async importFileErrorsList() {
-    const keys = (await FileUtils.keys()).filter(
-      (key) => !Core.filesList.includes(key),
-    );
-    Core.filesList.push(...keys);
-    return Core.filesList;
-  }
+	static async importFileErrorsList() {
+		const keys = (await FileUtils.keys()).filter(
+			(key) => !Core.filesList.includes(key),
+		);
+		Core.filesList.push(...keys);
+		return Core.filesList;
+	}
 
-  static onErrorReceive(error, context) {
-    try {
-      const errorData = new ErrorData(error, context);
-      this.pushToSessionErrors(errorData, context);
-      this.errorLogger(error);
+	static onErrorReceive(error, context) {
+		try {
+			const errorData = new ErrorData(error, context);
+			this.pushToSessionErrors(errorData, context);
+			this.errorLogger(error);
 
-      const meta = Core.session.meta;
-      meta.requestUpdate();
-    } catch (error) {
-      console.error(error);
-      process.exit();
-    }
-  }
+			const meta = Core.session.meta;
+			meta.requestUpdate();
+		} catch (error) {
+			console.error(error);
+			process.exit();
+		}
+	}
 
-  static pushToSessionErrors(errorData, context) {
-    const { message: key } = errorData;
-    const group = this.getErrorsGroupBy(key);
-    group.onErrorReceive(errorData, context);
-  }
+	static pushToSessionErrors(errorData, context) {
+		const { message: key } = errorData;
+		const group = this.getErrorsGroupBy(key);
+		group.onErrorReceive(errorData, context);
+	}
 
-  static session() {
-    return Core.session;
-  }
+	static session() {
+		return Core.session;
+	}
 
-  static async sessionWriteFile() {
-    Core.updateSessionMetadata();
-    const data = Core.toJSON();
-    const timestamp = Date.now();
-    return await FileUtils.write(timestamp, data);
-  }
+	static async sessionWriteFile() {
+		Core.updateSessionMetadata();
+		const data = Core.toJSON();
+		const timestamp = Date.now();
+		return await FileUtils.write(timestamp, data);
+	}
 }
 
 export function util_store_and_send_audit(context, error) {
-  const { channel, interaction } = context;
-  const primary = context.getSafeJSON?.() || null;
-  Manager.onErrorReceive(error, primary);
-  sendErrorInfo({ channel, interaction, error, primary });
+	const { channel, interaction } = context;
+	const primary = context.getSafeJSON?.() || null;
+	Manager.onErrorReceive(error, primary);
+	sendErrorInfo({ channel, interaction, error, primary });
 }
 
 export default Manager;
--- src/lib/sendErrorInfo.js
+++ src/lib/sendErrorInfo.js
@@ -1,236 +1,236 @@
+import client from "#bot/client.js";
+import config from "#config";
 import { ErrorData, ErrorsHandler } from "#lib/modules/ErrorsHandler.js";
 import {
-  ButtonStyle,
-  ComponentType,
-  TextInputStyle,
-} from "discord-api-types/v10";
+	objectToLocaleDeveloperString,
+	resolveGithubPath,
+	uid,
+} from "#lib/util.js";
 import { CreateModal } from "@zoodogood/utils/discordjs";
 import {
-  objectToLocaleDeveloperString,
-  resolveGithubPath,
-  uid,
-} from "#lib/util.js";
+	ButtonStyle,
+	ComponentType,
+	TextInputStyle,
+} from "discord-api-types/v10";
 import Path from "path";
-import config from "#config";
-import client from "#bot/client.js";
 
 class UserInterfaceUtil {
-  static components = {
-    getErrorInfo({ interaction, context }) {
-      const { stack } = context;
-      interaction.msg({
-        ephemeral: true,
-        content: `\`\`\`js\n${stack}\`\`\``,
-      });
-    },
-    async setAddableInformationForError({ interaction, context }) {
-      const modalId = `review-${uid()}`;
-      const components = [
-        {
-          type: ComponentType.TextInput,
-          customId: `${modalId}-content`,
-          style: TextInputStyle.Paragraph,
-          label: "Предположительно",
-          required: true,
-          maxLength: 1_000,
-          value: "Это бета-функция и она может быть удалена в будущем",
-          placeholder: "Необязательно, отправляет сообщение разработчику",
-        },
-      ];
+	static components = {
+		getErrorInfo({ interaction, context }) {
+			const { stack } = context;
+			interaction.msg({
+				ephemeral: true,
+				content: `\`\`\`js\n${stack}\`\`\``,
+			});
+		},
+		async setAddableInformationForError({ interaction, context }) {
+			const modalId = `review-${uid()}`;
+			const components = [
+				{
+					type: ComponentType.TextInput,
+					customId: `${modalId}-content`,
+					style: TextInputStyle.Paragraph,
+					label: "Предположительно",
+					required: true,
+					maxLength: 1_000,
+					value: "Это бета-функция и она может быть удалена в будущем",
+					placeholder: "Необязательно, отправляет сообщение разработчику",
+				},
+			];
 
-      const modal = CreateModal({
-        components,
-        customId: modalId,
-        title: "Отправить",
-      });
+			const modal = CreateModal({
+				components,
+				customId: modalId,
+				title: "Отправить",
+			});
 
-      interaction.showModal(modal);
+			interaction.showModal(modal);
 
-      const response = await interaction
-        .awaitModalSubmit({
-          filter: (interaction) => interaction.customId === modalId,
-          time: 300_000,
-        })
-        .catch(() => {});
+			const response = await interaction
+				.awaitModalSubmit({
+					filter: (interaction) => interaction.customId === modalId,
+					time: 300_000,
+				})
+				.catch(() => {});
 
-      if (!response) {
-        return;
-      }
+			if (!response) {
+				return;
+			}
 
-      const responseText = response.fields.getField(`${modalId}-content`).value;
+			const responseText = response.fields.getField(`${modalId}-content`).value;
 
-      const StatusEnum = {
-        Dang: "dang",
-        Urge: "urge",
-        Desi: "desi",
-        Quie: "quie",
-      };
+			const StatusEnum = {
+				Dang: "dang",
+				Urge: "urge",
+				Desi: "desi",
+				Quie: "quie",
+			};
 
-      const options = [
-        {
-          label: "Опасно",
-          value: StatusEnum.Dang,
-          description: "Имеет последствия",
-        },
-        {
-          label: "Необходимо",
-          value: StatusEnum.Urge,
-        },
-        {
-          label: "Мешает",
-          value: StatusEnum.Desi,
-          description: "Является преградой",
-        },
-        {
-          label: "Живёт себе спокойно",
-          value: StatusEnum.Quie,
-          description: "Эту ошибку можно обойти",
-        },
-      ];
-      (async () => {
-        const message = await response.msg({
-          ephemeral: true,
-          content: `Текст будет прикреплён как публичный комментарий к ошибке, не сообщайте личную информацию, а также направлен разработчику.\nПодробности можно узнать на основном сервере <${config.guild.url}>\nВыберите важность ошибки из списка, чтобы начать отправку`,
-          fetchReply: true,
-          placeholder: "Необходимо",
-          components: [
-            {
-              type: ComponentType.StringSelect,
-              customId: `${modalId}-select`,
-              options,
-            },
-          ],
-        });
+			const options = [
+				{
+					label: "Опасно",
+					value: StatusEnum.Dang,
+					description: "Имеет последствия",
+				},
+				{
+					label: "Необходимо",
+					value: StatusEnum.Urge,
+				},
+				{
+					label: "Мешает",
+					value: StatusEnum.Desi,
+					description: "Является преградой",
+				},
+				{
+					label: "Живёт себе спокойно",
+					value: StatusEnum.Quie,
+					description: "Эту ошибку можно обойти",
+				},
+			];
+			(async () => {
+				const message = await response.msg({
+					ephemeral: true,
+					content: `Текст будет прикреплён как публичный комментарий к ошибке, не сообщайте личную информацию, а также направлен разработчику.\nПодробности можно узнать на основном сервере <${config.guild.url}>\nВыберите важность ошибки из списка, чтобы начать отправку`,
+					fetchReply: true,
+					placeholder: "Необходимо",
+					components: [
+						{
+							type: ComponentType.StringSelect,
+							customId: `${modalId}-select`,
+							options,
+						},
+					],
+				});
 
-        const selectionInteract = await message
-          .awaitMessageComponent({ time: 120_000 })
-          .catch(() => {});
+				const selectionInteract = await message
+					.awaitMessageComponent({ time: 120_000 })
+					.catch(() => {});
 
-        if (!selectionInteract) {
-          message.delete();
-          return;
-        }
+				if (!selectionInteract) {
+					message.delete();
+					return;
+				}
 
-        const { error } = context;
-        const group = ErrorsHandler.getErrorsGroupBy(error.message);
-        group.addComment({ responseText, id: interaction.user.id });
+				const { error } = context;
+				const group = ErrorsHandler.getErrorsGroupBy(error.message);
+				group.addComment({ responseText, id: interaction.user.id });
 
-        selectionInteract.msg({
-          edit: true,
-          content: "Отправлено",
-          components: [],
-        });
+				selectionInteract.msg({
+					edit: true,
+					content: "Отправлено",
+					components: [],
+				});
 
-        client.channels.cache.get(config.guild.logChannelId).msg({
-          title: `Дан комментарий ошибки с срочностью ${
-            options.find(({ value }) => value === selectionInteract.values[0])
-              .label
-          }\n\`\`\`${error.message}\`\`\``,
-          description: responseText,
-          color: "#d8bb40",
-          footer: { text: interaction.user.id },
-        });
-      })();
-      return;
-    },
+				client.channels.cache.get(config.guild.logChannelId).msg({
+					title: `Дан комментарий ошибки с срочностью ${
+						options.find(({ value }) => value === selectionInteract.values[0])
+							.label
+					}\n\`\`\`${error.message}\`\`\``,
+					description: responseText,
+					color: "#d8bb40",
+					footer: { text: interaction.user.id },
+				});
+			})();
+			return;
+		},
 
-    async readPrimaryContext({ interaction, context }) {
-      const DEEP = 1;
-      const description = objectToLocaleDeveloperString(context.primary, DEEP);
-      interaction.msg({
-        title: "Часть состояния программы:",
-        description,
-      });
-    },
-  };
+		async readPrimaryContext({ interaction, context }) {
+			const DEEP = 1;
+			const description = objectToLocaleDeveloperString(context.primary, DEEP);
+			interaction.msg({
+				title: "Часть состояния программы:",
+				description,
+			});
+		},
+	};
 
-  static onComponent({ interaction, context }) {
-    this.components[interaction.customId].call(this, {
-      interaction,
-      context,
-    });
-  }
+	static onComponent({ interaction, context }) {
+		this.components[interaction.customId].call(this, {
+			interaction,
+			context,
+		});
+	}
 
-  static async sendErrorInfo({
-    channel,
-    error,
-    interaction = {},
-    primary = null,
-    description = "",
-  }) {
-    const { fileOfError, strokeOfError, stack } =
-      ErrorData.prototype.parseErrorStack.call(
-        { error },
-        { node_modules: false },
-      ) ?? {};
+	static async sendErrorInfo({
+		channel,
+		error,
+		interaction = {},
+		primary = null,
+		description = "",
+	}) {
+		const { fileOfError, strokeOfError, stack } =
+			ErrorData.prototype.parseErrorStack.call(
+				{ error },
+				{ node_modules: false },
+			) ?? {};
 
-    if (stack?.length >= 1900) {
-      stack.length = 1900;
-    }
+		if (stack?.length >= 1900) {
+			stack.length = 1900;
+		}
 
-    const components = [
-      {
-        type: ComponentType.Button,
-        style: ButtonStyle.Secondary,
-        label: "Получить отчёт",
-        customId: "getErrorInfo",
-        emoji: "〽️",
-      },
-      {
-        type: ComponentType.Button,
-        style: ButtonStyle.Link,
-        label: "В Github",
-        url: resolveGithubPath(
-          Path.relative(process.cwd(), fileOfError ?? "."),
-          strokeOfError,
-        ),
-        disabled: !fileOfError,
-      },
-      {
-        type: ComponentType.Button,
-        style: ButtonStyle.Success,
-        label: "Сообщение",
-        customId: "setAddableInformationForError",
-      },
-      {
-        type: ComponentType.Button,
-        style: ButtonStyle.Success,
-        label: "Контекст",
-        customId: "readPrimaryContext",
-        disabled: !primary,
-      },
-    ];
-    const embed = {
-      title: "— Данные об панике 🙄",
-      description: `> ${error.message}\n\n${description}`,
-      color: "#d8bb40",
-      components,
-      reference: interaction.message?.id ?? null,
-    };
-    const message = await channel.msg(embed);
+		const components = [
+			{
+				type: ComponentType.Button,
+				style: ButtonStyle.Secondary,
+				label: "Получить отчёт",
+				customId: "getErrorInfo",
+				emoji: "〽️",
+			},
+			{
+				type: ComponentType.Button,
+				style: ButtonStyle.Link,
+				label: "В Github",
+				url: resolveGithubPath(
+					Path.relative(process.cwd(), fileOfError ?? "."),
+					strokeOfError,
+				),
+				disabled: !fileOfError,
+			},
+			{
+				type: ComponentType.Button,
+				style: ButtonStyle.Success,
+				label: "Сообщение",
+				customId: "setAddableInformationForError",
+			},
+			{
+				type: ComponentType.Button,
+				style: ButtonStyle.Success,
+				label: "Контекст",
+				customId: "readPrimaryContext",
+				disabled: !primary,
+			},
+		];
+		const embed = {
+			title: "— Данные об панике 🙄",
+			description: `> ${error.message}\n\n${description}`,
+			color: "#d8bb40",
+			components,
+			reference: interaction.message?.id ?? null,
+		};
+		const message = await channel.msg(embed);
 
-    const context = {
-      error,
-      stack,
-      interaction,
-      channel,
-      description,
-      primary,
-    };
+		const context = {
+			error,
+			stack,
+			interaction,
+			channel,
+			description,
+			primary,
+		};
 
-    const collector = message.createMessageComponentCollector({
-      time: 3_600_000,
-    });
-    collector.on("collect", async (interaction) =>
-      this.onComponent({ interaction, context }),
-    );
-    collector.on("end", () => message.edit({ components: [] }));
-    return { context, message };
-  }
+		const collector = message.createMessageComponentCollector({
+			time: 3_600_000,
+		});
+		collector.on("collect", async (interaction) =>
+			this.onComponent({ interaction, context }),
+		);
+		collector.on("end", () => message.edit({ components: [] }));
+		return { context, message };
+	}
 }
 
 function sendErrorInfo(...params) {
-  return UserInterfaceUtil.sendErrorInfo(...params);
+	return UserInterfaceUtil.sendErrorInfo(...params);
 }
 
 export { UserInterfaceUtil as ErrorUserInterfaceUtil, sendErrorInfo };
