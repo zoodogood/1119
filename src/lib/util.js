54cb9dead23985a6be67a45fcdf2f050275fa96b
zoodogood <zoodogood@gmail.com>
2024-12-07 10:52:35 +0200

    Export assert(), path

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,4 +3,6 @@ export * from "#lib/mini.js";
 export * from "#lib/safe-utils.js";
 export * from "#server/util.js";
 export * from "#src/app/utils.js";
+export { default as assert } from "node:assert";
 export { default as EventEmitter } from "node:events";
+export { default as path } from "node:path";
