6686732c48e49f689c2dcf7bbb6952a0ca31e46a
zoodogood <zoodogood@gmail.com>
2025-02-25 10:20:37 +0200

    Clean a code

--- src/site/src/core/index.js
+++ src/site/src/core/index.js
@@ -1,10 +1,9 @@
-
 import PageWrapper from "#site/components/PageWrapper/mod.svelte";
 import svelteApp from "#site/core/svelte-app_singleton.js";
 import { current_page_by_route } from "#site/lib/page_router_singleton.js";
 import { mount } from "svelte";
 
-
-
-mount(PageWrapper, { target: svelteApp.document.body, props: {page: current_page_by_route()} });
-
+mount(PageWrapper, {
+	target: svelteApp.document.body,
+	props: { page: current_page_by_route() },
+});
