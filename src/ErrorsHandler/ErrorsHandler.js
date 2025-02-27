3055a26444e032ddda4cc4d9dd34bbfe4cd475f2
zoodogood <zoodogood@gmail.com>
2025-02-27 08:01:59 +0200

    Revert "Developer crutch"
    This reverts commit 71e5b299b6040fb24802dfc4201fa7aec6d2d3ac.

--- src/ErrorsHandler/ErrorsHandler.js
+++ src/ErrorsHandler/ErrorsHandler.js
@@ -114,10 +114,10 @@ class ErrorData {
 		try {
 			context &&= stringify(context);
 		} catch (error) {
-			// Manager.onErrorReceive(error, {
-			// 	in_errors_handler_module: true,
-			// });
-			context = undefined;
+			Manager.onErrorReceive(error, {
+				in_errors_handler_module: true,
+			});
+			context = stringify({ _broked: true, keys: Object.keys(context) });
 		}
 		this.error = error;
 		try {
@@ -328,8 +328,6 @@ class Manager {
 	}
 
 	static onErrorReceive(error, context) {
-		console.log({ message: error.message });
-
 		try {
 			const errorData = new ErrorData(error, context);
 			this.pushToSessionErrors(errorData, context);
