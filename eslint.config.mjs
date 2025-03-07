c310df67520f7c65ec496853c030c7524e8da5a4
zoodogood <zoodogood@gmail.com>
2025-03-07 07:44:29 +0200

    Migrate from prettier to stylistic formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,76 +1,71 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import perfectionist from "eslint-plugin-perfectionist";
-import prettier from "eslint-plugin-prettier";
-import svelte from "eslint-plugin-svelte";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import parser from "svelte-eslint-parser";
-
-const configFile = fileURLToPath(import.meta.url);
-const baseDirectory = path.dirname(configFile);
-const compat = new FlatCompat({
-	baseDirectory,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
+import lintjs from "@eslint/js"
+import stylistic from "@stylistic/eslint-plugin"
+import perfectionist from "eslint-plugin-perfectionist"
+import globals from "globals"
 
 export default [
-	...compat.extends(
-		"eslint:recommended",
-		"plugin:svelte/prettier",
-		"prettier",
-		"plugin:prettier/recommended",
-	),
 	{
 		plugins: {
-			prettier,
-			svelte,
-			perfectionist,
-		},
-
+			perfectionist ,
+			"@stylistic": stylistic ,
+		} ,
+		files: [
+			"**/*.js" ,
+			"**/*.cjs" ,
+			"**/*.mjs" ,
+			"**/*.ts" ,
+			"**/*.tsx" ,
+		] ,
 		languageOptions: {
 			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-
-			ecmaVersion: "latest",
-			sourceType: "module",
-		},
+				... globals.browser ,
+				... globals.node ,
+			} ,
+			ecmaVersion: "latest" ,
+			sourceType: "module" ,
+		} ,
 
 		rules: {
-			"prettier/prettier": ["error"],
-			eqeqeq: ["error", "always"],
-			"no-constant-condition": ["off"],
-			"prefer-const": ["error"],
-			"no-fallthrough": ["off"],
-			"no-async-promise-executor": ["off"],
+			... stylistic.configs.recommended.rules ,
+			... lintjs.configs.recommended.rules ,
+			"@stylistic/brace-style": [ "error" , "1tbs" , { allowSingleLine: true } ] ,
+			"@stylistic/comma-dangle": [ "error" , "always-multiline" ] ,
+			"@stylistic/comma-spacing": [ "error" , { before: true , after: true } ] ,
+			"@stylistic/array-bracket-spacing": [ "error" , "always" ] ,
+			"@stylistic/key-spacing": [ "error" , { beforeColon: false , afterColon: true } ] ,
+			"@stylistic/rest-spread-spacing": [ "error" , "always" ] ,
+			"@stylistic/space-in-parens": [ "error" , "always" ] ,
+			"@stylistic/indent": [
+				"error" ,
+				"tab" ,
+				{ tabLength: 2 } ,
+			] ,
+			"@stylistic/no-tabs": [ "off" ] ,
+			"@stylistic/quotes": [ "error" , "double" ] ,
+			"eqeqeq": [
+				"error" ,
+				"always" ,
+			] ,
+			"no-constant-condition": [ "off" ] ,
+			"prefer-const": [ "error" ] ,
+			"no-fallthrough": [ "off" ] ,
+			"no-async-promise-executor": [ "off" ] ,
 
 			"no-unused-vars": [
-				"error",
+				"error" ,
 				{
-					argsIgnorePattern: "^_",
-				},
-			],
+					argsIgnorePattern: "^_" ,
+				} ,
+			] ,
 
-			"perfectionist/sort-classes": ["error"],
-		},
-	},
-	...compat.extends("plugin:svelte/prettier").map((config) => ({
-		...config,
-		files: ["**/*.svelte"],
-	})),
+			"perfectionist/sort-classes": [ "error" ] ,
+		} ,
+	} ,
 	{
-		files: ["**/*.svelte"],
+		files: [ "**/*.svelte" ] ,
 
 		languageOptions: {
-			parser,
-		},
-
-		rules: {
-			"prettier/prettier'": "off",
-		},
-	},
-];
+			parser: ( await import( "svelte-eslint-parser" ) ).default ,
+		} ,
+	} ,
+]
