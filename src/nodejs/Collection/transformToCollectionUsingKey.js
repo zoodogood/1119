368a87eebd7a022aee3c9d45a7e5060fc4b35084
zoodogood <zoodogood@gmail.com>
2025-04-09 09:43:27 +0300

    Declare types

--- src/nodejs/Collection/transformToCollectionUsingKey.js
+++ src/nodejs/Collection/transformToCollectionUsingKey.js
@@ -1,6 +1,14 @@
 import { Collection } from '@discordjs/collection'
 
+/**
+ * @template K, T
+ * @param {(T & {key: K})[]} array
+ * @returns {Collection<K, T>} Collection<K, T>
+ */
 export function transformToCollectionUsingKey( array ) {
+	/**
+	 * @type {[K, T][]}
+	 */
 	const entries = array.map( object => [ object.key , object ] )
 	return new Collection( entries )
 }
