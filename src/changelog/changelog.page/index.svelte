996fd52590754532cf959f043133504584d16e5c
zoodogood <zoodogood@gmail.com>
2025-05-30 22:06:27 +0300

    Clean a code; Improve

--- src/changelog/changelog.page/index.svelte
+++ src/changelog/changelog.page/index.svelte
@@ -1,50 +1,48 @@
 <script>
-	import Layout from "#site-component/Layout";
-	import { group_changes_by_default } from "#src/changelog/ChangelogDaemon/display.js";
-	import { metadata } from "#src/changelog/ChangelogDaemon/metadata.js";
+	import Layout from '#site-component/Layout'
+	import { group_changes_by_default } from '#src/changelog/ChangelogDaemon/display.js'
+	import { metadata } from '#src/changelog/ChangelogDaemon/metadata.js'
 
-	import { fetchFromInnerApi } from "#src/http_requests/fetchFromInnerApi.js";
-	import { season_of_month } from "#src/safe-utils.js";
-	import { ending } from "@zoodogood/utils/primitives";
+	import { dayjs } from '#src/dayjs.js'
+	import { fetchFromInnerApi } from '#src/http_requests/fetchFromInnerApi.js'
+	import { season_of_month } from '#src/safe-utils.js'
 
-	import { dayjs } from "#src/dayjs.js";
-	import ChangeItem from "./change_item.svelte";
+	import { ending } from '@zoodogood/utils/primitives'
+	import ChangeItem from './change_item.svelte'
 
-	const SeasonEmoji = ["⛄", "🌸", "☀️", "🍁"];
+	const SeasonEmoji = [ '⛄' , '🌸' , '☀️' , '🍁' ]
 
-	const _interface_promise = (async () => {
-		const changes = await fetchFromInnerApi("changelog");
-		console.log(changes);
+	const _interface_promise = ( async () => {
+		const changes = await fetchFromInnerApi( 'changelog' )
+		console.log( changes )
 
 		return {
-			data: changes,
-			flat: changes.map(metadata),
-			first_change: changes[0],
-		};
-	})();
+			data: changes ,
+			flat: changes.map( metadata ) ,
+			first_change: changes[ 0 ] ,
+		}
+	} )()
 
-	let filter_by_source_raw = "";
-	let filter_queries = [];
-	let filtered_flat = [];
+	let filter_by_source_raw = ''
+	let filter_queries = []
+	let filtered_flat = []
 
-	let flat = [];
+	let flat = []
 
-	_interface_promise.then(({ flat: value }) => (flat = value));
+	_interface_promise.then( ( { flat: value } ) => ( flat = value ) )
 
-	$: filter_queries = filter_by_source_raw.toLowerCase().split(" ");
-	$: filtered_flat = flat.filter(changes_filter_factory(filter_queries));
+	$: filter_queries = filter_by_source_raw.toLowerCase().split( ' ' )
+	$: filtered_flat = flat.filter( changes_filter_factory( filter_queries ) )
 
-	console.log({ dayjs });
-
-	const changes_filter_factory =
-		(queries) =>
-		({ lowed_change }) =>
-			!queries ||
-			queries.every((query) =>
-				query.startsWith("!")
-					? !lowed_change.includes(query.slice(1))
-					: lowed_change.includes(query),
-			);
+	const changes_filter_factory
+		= queries =>
+			( { lowed_change } ) =>
+				!queries
+					|| queries.every( query =>
+					query.startsWith( '!' )
+						? !lowed_change.includes( query.slice( 1 ) )
+						: lowed_change.includes( query ) ,
+				)
 </script>
 
 <Layout>
@@ -52,43 +50,43 @@
 		Грузимся...
 	{:then _interface}
 		<input
-			type="text"
-			placeholder="Фильтровать по сообщению"
+			type='text'
+			placeholder='Фильтровать по сообщению'
 			bind:value={filter_by_source_raw}
 		/>
 		<small
-			>— используйте «!» при поиске для исключения, например, !bug !fix, —
+		>— используйте «!» при поиске для исключения, например, !bug !fix, —
 			исключает источники, включающие «bug» и «fix»</small
 		>
 		<h2>Список изменений</h2>
 		<h5>
 			Давайте посмотрим, что изменилось за {ending(
 				dayjs
-					.duration(Date.now() - _interface.first_change.createdAt)
-					.get("months") + 1,
-				"месяц",
-				"ев",
-				"",
-				"а",
+					.duration( Date.now() - _interface.first_change.createdAt )
+					.get( 'months' ) + 1 ,
+				'месяц' ,
+				'ев' ,
+				'' ,
+				'а' ,
 			)}..
 			<p>
-				{ending(_interface.flat?.length, "изменени", "й", "е", "я")}:
+				{ending( _interface.flat?.length , 'изменени' , 'й' , 'е' , 'я' )}:
 			</p>
 		</h5>
 
 		{#key filtered_flat}
-			{#each group_changes_by_default(filtered_flat) as [period, byPeriod]}
+			{#each group_changes_by_default( filtered_flat ) as [ period , byPeriod ]}
 				<p>
 					<span period_emoji>
-						{SeasonEmoji[season_of_month(+period.split(".")[0])]}
+						{SeasonEmoji[ season_of_month( +period.split( '.' )[ 0 ] ) ]}
 					</span>{period}
 				</p>
 				<ul>
-					{#each byPeriod as [group_base, byGroupSymbol]}
+					{#each byPeriod as [ group_base , byGroupSymbol ]}
 						<p group_label>
 							{group_base?.label}
 						</p>
-						{#each byGroupSymbol as { short_change, group_symbol, message, commit_id, createdAt, change, uid }}
+						{#each byGroupSymbol as { short_change , group_symbol , message , commit_id , createdAt , change , uid }}
 							<ChangeItem
 								{short_change}
 								{group_symbol}
@@ -101,12 +99,19 @@
 						{/each}
 						<br />
 					{/each}
-					<hr style="width: 100%;" />
+					<hr style='width: 100%;' />
 				</ul>
 			{/each}
 		{/key}
 	{:catch error}
-		{console.info(error) || error}
+		{( () => {
+			const { message } = error
+			if ( message === 'Failed to fetch' ) {
+				return 'Технически сервер недоступен'
+			}
+			console.error( error )
+			return error
+		} )()}
 	{/await}
 </Layout>
 
