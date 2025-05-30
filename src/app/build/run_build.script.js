30dc85dacd5647718ac7bb3a166ddca2f1a84d33
zoodogood <zoodogood@gmail.com>
2025-05-30 13:12:17 +0300

    Alterfix; Clean a code

--- src/app/build/run_build.script.js
+++ src/app/build/run_build.script.js
@@ -1,22 +1,3 @@
-/** .bat */
-// @echo off
-
-// cd ../../
-
-// echo "Node version:"
-// call node -v || echo "ERROR: Need install Node.js" && exit /b
-
-// echo "Install modules:"
-// call npm install || echo "Unknow error" && exit /b
-
-// echo "Check files:"
-// call node ./folder/scripts/checkFiles.js || echo "Unknow error" && exit /b
-
-// echo "Build bundle:"
-// call rollup -c ./src/site/rollup.config.mjs || echo "Unknow error" && exit /b
-
-// echo "Success!"
-
 import process from 'node:process'
 import get from '#src/nodejs/child-process-utils.js'
 
@@ -34,7 +15,7 @@ await info( 'Install modules:' )
 await run( manager , [ 'install' ] )
 
 await info( 'Check files:' )
-await run( runtime , [ './src/app/scripts/checkFiles.js' ] )
+await run( runtime , [ './src/app/build/build_require_files.script.js' ] )
 
 await info( 'Build bundle:' )
 await run( manager , [ 'run' , 'site-build' ] )
